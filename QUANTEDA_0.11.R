78394eb59bcb3339a47b97314a60537bd2993594
Paul Nulty <paul@paul-HP-Compaq-8200-Elite-CMT-PC.(none)>
2012-08-15 14:24:34 +0100

    test

--- QUANTEDA_0.11.R
+++ QUANTEDA_0.11.R
@@ -12,7 +12,7 @@
 #     notes (default is NULL, can be user-supplied)
 
 # testing git again
-# testing Rstudio with git 2 3 4
+# testing Rstudio with git 2 3 4 8
 
 library(austin)
 if(!require(XML)){
