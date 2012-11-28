09770372a70895faa454e8aec9ef2f02364a354a
Paul Nulty <paul@paul-HP-Compaq-8200-Elite-CMT-PC.(none)>
2012-11-28 16:51:42 +0000

    added data object for iebudgets

--- testing/testQuanteda.R
+++ testing/testQuanteda.R
@@ -1,11 +1,55 @@
-#source("/home/paul/Dropbox/code/quanteda/R/quanteda.R")
-#library(austin)
-#library(e1071)
+library(quanteda)
+source("~/Dropbox/code/quanteda/R/corpustools.R")
+source("~/Dropbox/code/quanteda/R/languagetools.R")
+library(austin)
 
+# make a list of text objects from a directory
+print("making texts")
+texts <- getTextDir("~/Dropbox/QUANTESS/corpora/iebudgets/budget_2008")
+parts <- strsplit(getRootFileNames(names(texts)), "_")
+newattribs <-
+  data.frame(matrix(unlist(parts), nrow=length(parts), byrow=TRUE))
+names(newattribs) <- 
+  c("year", "debate", "no", "fname", "speaker", "party")
+newattribs$party <- gsub(".txt", "", newattribs$party)
+iebudgets <- corpus.create(texts, attribs=newattribs)
 
-#require(austin)
-#data(iebudget2009)
-#wfm.ref <- t(iebudget2009)[1:2,]
+texts <- getTextDir("~/Dropbox/QUANTESS/corpora/iebudgets/budget_2009sup")
+parts <- strsplit(getRootFileNames(names(texts)), "_")
+newattribs <-
+  data.frame(matrix(unlist(parts), nrow=length(parts), byrow=TRUE))
+names(newattribs) <- 
+  c("year", "debate", "no", "fname", "speaker", "party")
+newattribs$party <- gsub(".txt", "", newattribs$party)
+iebudgets <- corpus.append(iebudgets, texts, newattribs)
 
-#wsc <- classic.wordscores(wfm.ref, c(-1,1))    # austin wordscores
-#wsc.predicted <- predict(wsc, newdata=iebudget2009)
+texts <- getTextDir("~/Dropbox/QUANTESS/corpora/iebudgets/budget_2010")
+parts <- strsplit(getRootFileNames(names(texts)), "_")
+newattribs <-
+  data.frame(matrix(unlist(parts), nrow=length(parts), byrow=TRUE))
+names(newattribs) <- 
+  c("year", "debate", "no", "fname", "speaker", "party")
+newattribs$party <- gsub(".txt", "", newattribs$party)
+iebudgets <- corpus.append(iebudgets, texts, newattribs)
+
+texts <- getTextDir("~/Dropbox/QUANTESS/corpora/iebudgets/budget_2011")
+parts <- strsplit(getRootFileNames(names(texts)), "_")
+newattribs <-
+  data.frame(matrix(unlist(parts), nrow=length(parts), byrow=TRUE))
+names(newattribs) <- 
+  c("year", "debate", "no", "fname", "speaker", "party")
+newattribs$party <- gsub(".txt", "", newattribs$party)
+iebudgets <- corpus.append(iebudgets, texts, newattribs)
+
+texts <- getTextDir("~/Dropbox/QUANTESS/corpora/iebudgets/budget_2012")
+parts <- strsplit(getRootFileNames(names(texts)), "_")
+newattribs <-
+  data.frame(matrix(unlist(parts), nrow=length(parts), byrow=TRUE))
+names(newattribs) <- 
+  c("year", "debate", "no", "fname", "speaker", "party")
+newattribs$party <- gsub(".txt", "", newattribs$party)
+iebudgets <- corpus.append(iebudgets, texts, newattribs)
+
+fvm <- create.fvm.corpus(iebudgets, group="party")
+#datamat <-  wfm(fvm, word.margin=1)
+#wf <- wordfish(datamat, dir=c(5,1))
