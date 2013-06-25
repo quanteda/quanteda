ecdda30c3b9a0007887e69ebde2f5cfa14603121
Ken Benoit <kbenoit@lse.ac.uk>
2013-06-25 16:36:23 +0100

    minor tweaks to sentence segmenter

--- testing/testSentenceSeg.R
+++ testing/testSentenceSeg.R
@@ -5,7 +5,10 @@ library(XML)
 #string <- paste(readLines('/home/paul/Dropbox/QUANTESS/corpora/UK Manifestos/UK_natl_1997_en_Lab.txt'), collapse="\n")
 strings <- getTextDir('/home/paul/Dropbox/QUANTESS/corpora/UK Manifestos')
 
+strings <- getTextDir('~/Dropbox/QUANTESS/corpora/UK Manifestos')
 strings <- iconv(strings, from="latin1", to="ASCII", sub="byte")
+corpus.ukmanifs <- corpus.create(strings, attribs=names(strings))
+summary(corpus.ukmanifs)
 
 res <- sentenceSeg(strings[11:11])
 ch <- translateChunk('this is a test',  'EN', 'RO', key='AIzaSyDbQj08rNx4i2LAjXPYf5AD4TdfWr1ZEtM', verbose=TRUE)
