3432a260da1e755b1b78b879b59b6635042b3165
Paul Nulty <paul.nulty@gmail.com>
2013-06-27 18:22:27 +0100

    removed hardcoded key

--- testing/testSentenceSeg.R
+++ testing/testSentenceSeg.R
@@ -10,4 +10,4 @@ strings <- iconv(strings[51:60], from="latin1", to="ASCII", sub="byte")
 corpus <- corpus.create(strings)
 result <- corpus.reshape(corpus)
 df <- result$attribs
-resps <- lapply(df$texts[1:3],translate, sourceLanguage='EN', targetLanguage='RO', key='AIzaSyDbQj08rNx4i2LAjXPYf5AD4TdfWr1ZEtM', verbose=TRUE)
+resps <- lapply(df$texts[1:3],translate, sourceLanguage='EN', targetLanguage='RO', key="", verbose=TRUE)
