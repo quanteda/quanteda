5f898edf0619d8b02c0a6a4453e64bf55f21cd01
Paul Nulty <paul.nulty@gmail.com>
2013-11-14 18:06:18 +0000

    testing generic functions for langtools

--- testing/testLangTools.R
+++ testing/testLangTools.R
@@ -1,3 +1,25 @@
-source("~/Dropbox/code/quanteda/R/languagetools.R")
-testSentence <- "Summarise valour banana neighbour."
-print(countSyllables(testSentence))
+# testing and optimization for common language functions
+
+#source("~/Dropbox/code/quanteda/R/languagetools.R")
+
+
+
+data(iebudgets)
+
+tokenize_new.corpus <- function(c){
+  c$attribs$texts <- sapply(c$attribs$texts, scan(what="char",  c$attribs$texts, quiet=TRUE))
+}
+
+tokenize_new <- function(s){
+  tokens <-strsplit(s, "\\s+")[[1]]
+  return(tokens)
+}
+
+
+passby <- function(x){
+  x <- x+1
+}
+
+Rprof(append = TRUE)
+#tokenString <- iebudgets
+Rprof(NULL)
