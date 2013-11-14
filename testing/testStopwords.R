11b072d498a5e55dcd0373ec024c49f7b7036760
Paul Nulty <paul.nulty@gmail.com>
2013-11-14 12:35:42 +0000

    stopwords tester

---
+++ testing/testStopwords.R
@@ -0,0 +1,17 @@
+source('languagetools.R')
+source('corpustools.R')
+
+testString <- "In computing, stop words are words which are filtered out prior to,
+or after, processing of natural language data (text).[1] There is not one definite 
+list of stop words which all tools use and such a filter is not always used. Some 
+tools specifically avoid removing them to support phrase search. Any group of words
+can be chosen as the stop words for a given purpose. For some search machines, these
+are some of the most common, short function words, such as the, is, at, which, and
+on. In this case, stop words can cause problems when searching for phrases that
+include them, particularly in names such as 'The Who', 'The The', or 'Take That'. 
+Other search engines remove some of the most common words—including lexical words, 
+such as want—from a query in order to improve performance."
+
+testCorpus <- corpus.create(testString)
+
+print(removeStopwords(testString))
