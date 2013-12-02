16e73baa1e15e8b44d807ab608941971542dd6b9
Paul Nulty <paul.nulty@gmail.com>
2013-12-02 15:27:09 +0000

    moved and renamed movietests and benchmark

---
+++ testing/benchmark.R
@@ -0,0 +1,44 @@
+library(quanteda)
+library(austin)
+library(ggplot2)
+library(reshape2)
+options(error=dump.frames)
+source("/home/paul/Dropbox/code/quanteda/R/corpustools.R")
+source("/home/paul/Dropbox/code/quanteda/R/languagetools.R")
+texts <- getTextDir("~/Dropbox/QUANTESS/corpora/movieReviews/smaller/neg/")
+
+oldTimes <- vector()
+newTimes <- vector()
+
+testSizes <- c(10, 50, 100, 200, 500, 1000)
+
+for (numDocs in testSizes){
+  vals <-vector()
+  vals[1:(numDocs/2)] <- "neg"
+  atts <- data.frame(vals)
+  names(atts)<-c("lab")
+  negTexts <- texts[1:(numDocs/2)]
+  movies <- corpus.create(negTexts, attribs=atts)
+  
+  texts <- getTextDir("~/Dropbox/QUANTESS/corpora/movieReviews/smaller/pos/")
+  vals <-vector()
+  vals[1:(numDocs/2)] <- "pos"
+  
+  atts <- data.frame(vals)
+  names(atts)<-c("lab")
+  posTexts <- texts[1:(numDocs/2)]
+  movies <- corpus.append(movies, posTexts, atts)
+
+  Rprof(append = FALSE)
+  toks <- create.fvm.corpus(movies)
+  Rprof(NULL)
+  oldTimes <- c(oldTimes, summaryRprof()$sampling.time)
+  
+  Rprof(append = FALSE)
+  toks <- create.fvm.new.corpus(movies)
+  Rprof(NULL)
+  newTimes <- c(newTimes, summaryRprof()$sampling.time)
+}
+df <- data.frame(testSizes, oldTimes, newTimes)
+results <- melt(data = df, id.vars = "testSizes", value.name="seconds")
+ggplot(data = results, aes(x = testSizes , y = seconds, colour = variable)) + geom_line()
--- testing/testMovieReviews.R
+++ testing/testMovieReviews.R
@@ -1,44 +1,11 @@
 library(quanteda)
-library(austin)
-library(ggplot2)
-library(reshape2)
-options(error=dump.frames)
-source("/home/paul/Dropbox/code/quanteda/R/corpustools.R")
-source("/home/paul/Dropbox/code/quanteda/R/languagetools.R")
-texts <- getTextDir("~/Dropbox/QUANTESS/corpora/movieReviews/smaller/neg/")
-
-oldTimes <- vector()
-newTimes <- vector()
-
-testSizes <- c(10, 50, 100, 200, 500, 1000)
-
-for (numDocs in testSizes){
-  vals <-vector()
-  vals[1:(numDocs/2)] <- "neg"
-  atts <- data.frame(vals)
-  names(atts)<-c("lab")
-  negTexts <- texts[1:(numDocs/2)]
-  movies <- corpus.create(negTexts, attribs=atts)
-  
-  texts <- getTextDir("~/Dropbox/QUANTESS/corpora/movieReviews/smaller/pos/")
-  vals <-vector()
-  vals[1:(numDocs/2)] <- "pos"
-  
-  atts <- data.frame(vals)
-  names(atts)<-c("lab")
-  posTexts <- texts[1:(numDocs/2)]
-  movies <- corpus.append(movies, posTexts, atts)
-
-  Rprof(append = FALSE)
-  toks <- create.fvm.corpus(movies)
-  Rprof(NULL)
-  oldTimes <- c(oldTimes, summaryRprof()$sampling.time)
-  
-  Rprof(append = FALSE)
-  toks <- create.fvm.new.corpus(movies)
-  Rprof(NULL)
-  newTimes <- c(newTimes, summaryRprof()$sampling.time)
-}
-df <- data.frame(testSizes, oldTimes, newTimes)
-results <- melt(data = df, id.vars = "testSizes", value.name="seconds")
-ggplot(data = results, aes(x = testSizes , y = seconds, colour = variable)) + geom_line()
+neg_texts <- getTextDir("~/Dropbox/QUANTESS/corpora/movieReviews/smaller/neg/")
+pos_texts <- getTextDir("~/Dropbox/QUANTESS/corpora/movieReviews/smaller/pos/")
+texts <- c(neg_texts,pos_texts)
+vals <-vector()
+vals[1:1000] <- "neg"
+vals[1001:2000] <- "pos"
+atts <- data.frame(vals)
+names(atts)<-c("label")
+movies <- corpus.create(texts, attribs=atts)
+fvm <- create.fvm.corpus(movies, group="label")
