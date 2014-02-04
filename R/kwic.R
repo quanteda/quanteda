b61a3862db4564a32de8df34ead488053e0dddc0
Ken Benoit <kbenoit@lse.ac.uk>
2014-02-04 10:34:25 +0000

    Updated kwic.R
    now returns the result as a formatted data frame. Still needs work on
    the corpus method.

--- R/kwic.R
+++ R/kwic.R
@@ -1,73 +1,43 @@
-
-#' return the context in which 'word' appears
-
-#' Keyword-in-context function to return the 
-#' context in which 'word' appears
-#' 
-#' 
-#' @param corpus The corpus to be searched
-#' @param word The keyword to look for
-#' @param window The size of the context window (the
-#'  number of words each side of keyword to return)
-#'  the default is 5. 
-#' @export
-#' @examples
-#' contexts <- kwic(corpus, keyword, window=6)
+#library(quanteda)
 
 kwic <- function(text, word, window=5){
   UseMethod("kwic")
 }
 
-
-#' return the context in which 'word' appears
-
-#' Keyword-in-context function to return the 
-#' context in which 'word' appears
-#' 
-#' 
-#' @param corpus The corpus to be searched
-#' @param word The keyword to look for
-#' @param window The size of the context window (the
-#'  number of words each side of keyword to return)
-#'  the default is 5. 
-#' @export
-#' @examples
-#' contexts <- kwic(corpus, keyword, window=6)
-kwic.character <- function(text, word, window=5){
-  toks <- tokenize(text)
-  matches = which(toks == word )
-  contexts = vector()
-  if(length(matches) == 0){return(NA)}
-  contexts <- rep(NA, length(matches))
-  for(m in 1:length(matches)){
-    start <- matches[m] - window
-    end <- matches[m] + window
-    if(start < 0) start <- 0
-    if(end > length(toks)) end <- length(toks)
-    
-    contexts[m] <- paste(toks[start:end], collapse=' ' )
-    print(paste(toks[start:end], collapse=' ' ))
-  }
-  return(contexts)
+kwic.character <- function(text, word, window=5) {
+    # don't use tokenize since we want to preserve case and punctuation here
+    # grep needed to get words that end in punctuation mark or in quotes
+    tokens <- strsplit(text, " ")[[1]]
+    matches <- grep(paste("?", tolower(word), "?", sep=""), tolower(tokens))
+    if (length(matches) == 0) return(NA)
+    result <- data.frame(source = matches,
+                         preword = NA,
+                         word = tokens[matches],
+                         postword = NA)
+    for (m in 1:length(matches)) {
+        wordpos  <- matches[m]
+        startpos <- ifelse(((matches[m] - window) < 1), 1, matches[m] - window)
+        endpos   <- ifelse(((matches[m] + window) > length(tokens)), length(tokens), matches[m] + window)
+        result$preword[m]  <-  ifelse(wordpos==startpos, "", paste(tokens[startpos : (wordpos - 1)], collapse = " "))
+        result$postword[m] <-  ifelse(wordpos==endpos, "", paste(tokens[(wordpos + 1) : endpos], collapse = " "))
+    }
+    # left-justify the post-word part
+    result$postword <- format(result$postword, justify="left")
+    # centre the post-word part
+    result$word <- format(result$word, justify="centre")
+    class(result) <- c("kwic", class(result))
+    return(result)
 }
 
-
-#' return the context in which 'word' appears
-
-#' Keyword-in-context function to return the 
-#' context in which 'word' appears
-#' 
-#' 
-#' @param corpus The corpus to be searched
-#' @param word The keyword to look for
-#' @param window The size of the context window (the
-#'  number of words each side of keyword to return)
-#'  the default is 5.
-#' @export 
-#' @examples
-#' contexts <- kwic(corpus, keyword, window=6)
 kwic.corpus <- function(corpus, word, window=5){
-  contexts <- lapply(corpus$attribs$texts, kwic.character, word=word, window=window)
-  names(contexts) <- row.names(corpus$attribs)
-  return(contexts)
+    # need to modify this so that it returns a data frame, and
+    # puts "[filename, line XX]" in the "source" column
+    contexts <- sapply(corpus$attribs$texts, kwic, word=word)
+    names(contexts) <- row.names(corpus$attribs)
+    return(contexts)
 }
+
+#text <- "Keanu is really \"great\" in this movie, and Arnold is great too.  I think R is a GREAT programming tool for statistics, while Matlab just is not that great."
+
+#t <- kwic(text, "great")
+
