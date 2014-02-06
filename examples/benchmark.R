1922e2f07ceceb3c439ef6400007a339f7cc2876
Paul Nulty <paul.nulty@gmail.com>
2014-02-06 22:52:02 +0000

    benchmarking example and namespace fix

--- examples/benchmark.R
+++ examples/benchmark.R
@@ -3,22 +3,21 @@ library(austin)
 library(ggplot2)
 library(reshape2)
 
-source("~/Dropbox/code/quanteda/R/languagetools.R")
-source("~/Dropbox/code/quanteda/R/corpustools.R")
 
 options(error=dump.frames)
 
 texts <- getTextDir("~/Dropbox/QUANTESS/corpora/movieReviews/smaller/neg/")
 oldTimes <- vector()
 newTimes <- vector()
-testSizes <- c(500, 800)
+testSizes <- c(100,200,300,400,500)
 for (numDocs in testSizes){
+  print(numDocs)
   vals <-vector()
   vals[1:(numDocs/2)] <- "neg"
   atts <- data.frame(vals)
   names(atts)<-c("lab")
   negTexts <- texts[1:(numDocs/2)]
-  movies <- corpus.create(negTexts, attribs=atts)
+  movies <- createCorpus(negTexts, attribs=atts)
   
   texts <- getTextDir("~/Dropbox/QUANTESS/corpora/movieReviews/smaller/pos/")
   vals <-vector()
@@ -27,17 +26,19 @@ for (numDocs in testSizes){
   atts <- data.frame(vals)
   names(atts)<-c("lab")
   posTexts <- texts[1:(numDocs/2)]
-  movies <- corpus.append(movies, posTexts, atts)
+  movies <- corpusAppend(movies, posTexts, atts)
+  
+  Rprof(append = FALSE)
+  toks <- dfm.corpus(movies)
+  Rprof(NULL)
+  newTimes <- c(newTimes, summaryRprof()$sampling.time)
 
   Rprof(append = FALSE)
   toks <- create.fvm.corpus(movies)
   Rprof(NULL)
   oldTimes <- c(oldTimes, summaryRprof()$sampling.time)
   
-  Rprof(append = FALSE)
-  toks <- create.fvm.corpus(movies)
-  Rprof(NULL)
-  newTimes <- c(newTimes, summaryRprof()$sampling.time)
+
 }
 df <- data.frame(testSizes, oldTimes, newTimes)
 results <- melt(data = df, id.vars = "testSizes", value.name="seconds")
