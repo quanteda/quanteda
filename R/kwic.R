4bd4efe1eaba5e38f293d59bade7a3cea63684c8
Paul Nulty <paul.nulty@gmail.com>
2014-03-06 14:15:03 +0000

    removed kwic2 and collocate and features (retained in dev)

--- R/kwic.R
+++ R/kwic.R
@@ -18,7 +18,7 @@
 ##' data(iebudgets)
 ##' kwic(subset(iebudgets, year==2010), "Christmas", window=4)
 kwic <- function(text, word, window=5) {
-    UseMethod("kwic")
+  UseMethod("kwic")
 }
 
 
@@ -26,28 +26,28 @@ kwic <- function(text, word, window=5) {
 #' @method kwic character
 #' @S3method kwic character
 kwic.character <- function(text, word, window=5) {
-    # don't use tokenize since we want to preserve case and punctuation here
-    # grep needed to get words that end in punctuation mark or in quotes
-    tokens <- strsplit(text, " ")[[1]]
-    matches <- grep(paste("^", tolower(word), "$", sep=""), tolower(tokens))
-    if (length(matches) == 0) return(NA)
-    result <- data.frame(source = matches, 
-                         preword = NA,
-                         word = tokens[matches],
-                         postword = NA)
-    for (m in 1:length(matches)) {
-        wordpos  <- matches[m]
-        startpos <- ifelse(((matches[m] - window) < 1), 1, matches[m] - window)
-        endpos   <- ifelse(((matches[m] + window) > length(tokens)), length(tokens), matches[m] + window)
-        result$preword[m]  <-  ifelse(wordpos==startpos, "", paste(tokens[startpos : (wordpos - 1)], collapse = " "))
-        result$postword[m] <-  ifelse(wordpos==endpos, "", paste(tokens[(wordpos + 1) : endpos], collapse = " "))
-    }
-    # left-justify the post-word part
-    result$postword <- format(result$postword, justify="left")
-    # centre the post-word part
-    result$word <- format(result$word, justify="centre")
-    class(result) <- c("kwic", class(result))
-    return(result)
+  # don't use tokenize since we want to preserve case and punctuation here
+  # grep needed to get words that end in punctuation mark or in quotes
+  tokens <- strsplit(text, " ")[[1]]
+  matches <- grep(paste("^", tolower(word), "$", sep=""), tolower(tokens))
+  if (length(matches) == 0) return(NA)
+  result <- data.frame(source = matches, 
+                       preword = NA,
+                       word = tokens[matches],
+                       postword = NA)
+  for (m in 1:length(matches)) {
+    wordpos  <- matches[m]
+    startpos <- ifelse(((matches[m] - window) < 1), 1, matches[m] - window)
+    endpos   <- ifelse(((matches[m] + window) > length(tokens)), length(tokens), matches[m] + window)
+    result$preword[m]  <-  ifelse(wordpos==startpos, "", paste(tokens[startpos : (wordpos - 1)], collapse = " "))
+    result$postword[m] <-  ifelse(wordpos==endpos, "", paste(tokens[(wordpos + 1) : endpos], collapse = " "))
+  }
+  # left-justify the post-word part
+  result$postword <- format(result$postword, justify="left")
+  # centre the post-word part
+  result$word <- format(result$word, justify="centre")
+  class(result) <- c("kwic", class(result))
+  return(result)
 }
 
 
@@ -55,18 +55,17 @@ kwic.character <- function(text, word, window=5) {
 #' @method kwic corpus
 #' @S3method kwic corpus
 kwic.corpus <- function(corpus, word, window=5){
-    contexts <- sapply(corpus$attribs$texts, kwic, word=word, window=window, USE.NAMES=FALSE)
-    if (length(contexts)==1 && is.na(contexts)) return(NA)
-    names(contexts) <- row.names(corpus$attribs)
-    contexts <- contexts[!is.na(contexts)]
-    for (l in 1:length(contexts)) {
-        contexts[[l]]$source <- paste("[", names(contexts[l]), ", ",  contexts[[l]]$source, "]", sep="")
-    }
-    contexts <- do.call("rbind", contexts)
-    contexts$source <- format(contexts$source, justify="right")
-    contexts$postword <- format(contexts$postword, justify="left")
-    row.names(contexts) <- contexts$source
-    contexts <- contexts[,-1]   
-    return(contexts)
+  contexts <- sapply(corpus$attribs$texts, kwic, word=word, window=window, USE.NAMES=FALSE)
+  if (length(contexts)==1 && is.na(contexts)) return(NA)
+  names(contexts) <- row.names(corpus$attribs)
+  contexts <- contexts[!is.na(contexts)]
+  for (l in 1:length(contexts)) {
+    contexts[[l]]$source <- paste("[", names(contexts[l]), ", ",  contexts[[l]]$source, "]", sep="")
+  }
+  contexts <- do.call("rbind", contexts)
+  contexts$source <- format(contexts$source, justify="right")
+  contexts$postword <- format(contexts$postword, justify="left")
+  row.names(contexts) <- contexts$source
+  contexts <- contexts[,-1]   
+  return(contexts)
 }
-
