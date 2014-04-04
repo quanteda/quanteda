99f96c8f4f575cd740cba7353b5e6310fadd6a63
Ken Benoit <kbenoit@lse.ac.uk>
2014-04-04 10:01:19 -0500

    Notes and slight changes to ngrams.R

--- R/ngrams.R
+++ R/ngrams.R
@@ -1,3 +1,16 @@
+## Ken's notes
+# should use tokeniser instead of just splitting on space delimiters
+# help function needs arguments explained, and returns
+# we should generalize this to ngrams(text, n=2, window=1, unordered=FALSE)
+# -- meaning we need a new, more general version to supercede this
+# -- Note the suggested defaults above - default window should be 1, not 2
+# -- we probably never really want to make unordered=TRUE since these are not
+#    naturally occurring pairs, just like "savings bank" is not the same as 
+#    "bank savings"
+# -- possibly define a version for a corpus
+# -- right now it treats a text vector as a single text, which is fine, 
+#    but we will want to note this in the help file.  To apply to a vector
+#    of texts we will need to apply() it or define a corpus method.
 
 #' Create bigrams
 #' 
@@ -7,26 +20,30 @@
 #' @export
 #' @examples
 #' bigrams("aa bb cc dd ee ff")
-bigrams <- function(text, window = 2, sort = FALSE){
-  t <- unlist(strsplit(text, ' '))
-  bigrams <- c()
-  w <- 1
-  for(w in c(1:window)){
+#' bigrams(")
+bigrams <- function(text, window = 2, unordered = FALSE){
+    ## should use our tokenizer instead - KB
+    t <- unlist(strsplit(text, ' '))
 
-    m1 <- c(rep('', w), t)
-    m2 <- c(t, rep('', w))
-    b <- paste(m1, m2, sep='-')
-    #print(b)
-    l <- length(b)
-    bigrams <- c(bigrams, b[(w+1):(l-w)])
-    
-  }
-  #Sort words in bigrams alphabetically
-  if(sort){
-    bigrams <- unlist(lapply(bigrams, function(x) paste(sort(unlist(strsplit(x, '-'))), collapse = '-' )))
-  }
-  #bigramText <- paste(bigrams, collapse = ' ')
-  return(bigrams)
+    # initialize bigrams vector
+    bigrams <- c()
+
+    # w <- 1  - not nececssary - for() initializes the w using the range of in
+    for (w in (1:window)) {
+        m1 <- c(rep('', w), t)
+        m2 <- c(t, rep('', w))
+        b <- paste(m1, m2, sep='_')
+        #print(b)
+        l <- length(b)
+        bigrams <- c(bigrams, b[(w+1):(l-w)])
+    }
+
+    # Sort words in bigrams alphabetically - but not really the bigrams since it reorders them!
+    if (unordered) {
+        bigrams <- unlist(lapply(bigrams, function(x) paste(sort(unlist(strsplit(x, '_'))), collapse = '_' )))
+    }
+    #bigramText <- paste(bigrams, collapse = ' ')
+    return(bigrams)
 }
 #bigrams("aa bb cc dd ee ff", 5)
 
