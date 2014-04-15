56977d848f7adcbbf49fc0dcafa7ab800c33dbda
Ken Benoit <kbenoit@lse.ac.uk>
2014-04-15 11:08:45 +0100

    Fixed some issues in ngrams.

--- R/ngrams.R
+++ R/ngrams.R
@@ -1,13 +1,14 @@
-
 #' Create bigrams
 #' 
 #' @param tokens
-#' @author kohei Watanabe
+#' @author Kohei Watanabe and Ken Benoit
+#' @param text character vector containing the texts from which ngrams will be extracted
+#' @param window how many words to be counted for adjacency.  Default is 1 for only immediately 
+#' neighbouring words.
 #' @return a character of bigrams vector
 #' @export
 #' @examples 
 #' data(iebudgets)
-#' wfm <- dfm(iebudgets, group="party", bigram=TRUE)
 #' bigrams("The quick brown fox jumped over the lazy dog.")
 #' bigrams("The quick brown fox jumped over the lazy dog.", window=2)
 bigrams <- function(text, window = 1) {
@@ -32,28 +33,33 @@ bigrams <- function(text, window = 1) {
 
 #' Create ngrams
 #' 
-#' @param tokens
+#' Create a set of ngrams (words in sequence) from a text.
+#' 
 #' @author Kohei Watanabe, Ken Benoit, Paul Nulty
 #' @return a character of bigrams vector
+#' @param text character vector containing the texts from which ngrams will be extracted
+#' @param n the number of tokens to concatenate. Default is 2 for bigrams.
+#' @param window how many words to be counted for adjacency.  Default is 1 for only immediately 
+#' neighbouring words.
+#' @param concatenator character for combining words, default is \code{_} (underscore) character
 #' @export
 #' @examples 
 #' data(iebudgets)
 #' ngrams("The quick brown fox jumped over the lazy dog.", n=2)
 #' ngrams("The quick brown fox jumped over the lazy dog.", n=3)
-ngrams <- function(text, n=2, window=1) {
+ngrams <- function(text, n=2, window=1, concatenator="_") {
     ## GENERALIZE to NGRAMS
     t <- tokenize(text)
-    # initialize bigrams vector
-    bigrams <- c()
-    # w <- 1  - not nececssary - for() initializes the w using the range of in
+    # initialize ngrams vector
+    ngrams <- c()
     for (w in (1:window)) {
         m1 <- c(rep('', w), t)
         m2 <- c(t, rep('', w))
-        b <- paste(m1, m2, sep='_')
+        b <- paste(m1, m2, sep=concatenator)
         #print(b)
+        # trim the ends
         l <- length(b)
-        bigrams <- c(bigrams, b[(w+1):(l-w)])
+        ngrams <- c(ngrams, b[(w+1+(n-2)):(l-w-(n-2))])
     }
-    
     return(ngrams)
 }
