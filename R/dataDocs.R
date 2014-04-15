723730babf756e2f9728b5414d46afab2e8d9257
Paul Nulty <paul.nulty@gmail.com>
2014-04-15 17:20:27 +0100

    documentation for the datasets

---
+++ R/dataDocs.R
@@ -0,0 +1,49 @@
+#' @name ieAttribs
+#' @title A vector of attributes to match ieBudget documents
+#' @description This is a small vector of attributes for use in examples with ieBudgets
+#' @docType data
+NULL
+
+#' @name ieTexts
+#' @title A vector of texts from Irish budget debates for use with examples
+#' @description This is a small vector of texts from the ieBudget corpus for use with testing examples
+#' @docType data
+NULL
+
+#' @name ieTextsHeaders
+#' @title A version of ieTexts with attributes contained in JSON headers
+#' @description This is a small vector of texts for use in examples with corpusFromHeaders
+#' @docType data
+NULL
+
+#' @name ieTexts
+#' @title A vector of texts from Irish budget debates for use with examples
+#' @description This is a small vector of texts from the ieBudget corpus for use with testing examples
+#' @docType data
+NULL
+
+
+#' @name movies
+#' @docType data
+#' @description A corpus object containing 2000 movie reviews classified by positive or negative sentiment
+#' @references \url{http://dl.acm.org/citation.cfm?id=1118704}
+NULL
+
+#' @name swissdebates
+#' @docType data
+#' @description A corpus object containing 770 texts from debates in the Swiss parliament
+#' @references \url{http://dl.acm.org/citation.cfm?id=1118704}
+NULL
+
+#' @name stopwords_EN
+#' @docType data
+#' @description A list containing common English stopwords
+NULL
+
+#' @name sylCounts
+#' @docType data
+#' @description A named list mapping words to counts of their syllables, generated from
+#' the CMU pronunciation dictionary
+#' @references \url{http://www.speech.cs.cmu.edu/cgi-bin/cmudict}
+#' 
+NULL
