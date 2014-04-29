fb55363480ac0d63235c07faaae5102356d9b236
Paul Nulty <paul.nulty@gmail.com>
2014-04-29 14:07:20 +0100

    example for new twitter.R

--- examples/twitterExample.R
+++ examples/twitterExample.R
@@ -2,4 +2,8 @@ library(twitteR)
 
 library(RCurl)
 load("~/Dropbox/QUANTESS/social media/QUANTESS_oauth.Rdata")
-x <- twitterTerms('rstudio',  my_oauth, 17)
+key='ZOHJIRAwnw23FhvFWyUg'
+cons_secret='HTfEcEmRRDcx0ZsJ5CHOcmPc84AfDOp5VvIXwt0oY'
+token='778251283-ZkDTfl3IbIFZFXlVokA6Gpc19TZPyov3wucZ0XaB'
+access_secret='8vgPnpEWP3qhvILmTLXVb9RslwcEwVVeKOo4KCYHOY'
+x <- twitterTerms('rstudio', numResults=50, key, cons_secret, token, access_secret)
