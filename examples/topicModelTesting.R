b920cc43bbacfc71669113f5efc97a383b0f2e02
Paul Nulty <paul.nulty@gmail.com>
2014-05-19 16:26:51 +0100

    example for stm package

--- examples/topicModelTesting.R
+++ examples/topicModelTesting.R
@@ -1,1 +1,9 @@
-libra
+library(tm)
+library(stm)
+library(quanteda)
+data(iebudgets)
+d <- dfm(iebudgets, stopwords=TRUE)
+docs <- readCorpus(d, type=c("dtm"))
+vocab <- colnames(d)
+
+x <- stm(docs$documents, docs$vocab, 15)
