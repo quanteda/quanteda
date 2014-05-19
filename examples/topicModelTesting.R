6cda1f8a0a620948cc64ce6f9e3d33604945c907
Paul Nulty <paul.nulty@gmail.com>
2014-05-19 17:24:09 +0100

    label topics

--- examples/topicModelTesting.R
+++ examples/topicModelTesting.R
@@ -1,9 +1,10 @@
 library(tm)
 library(stm)
 library(quanteda)
-data(iebudgets)
-d <- dfm(iebudgets, stopwords=TRUE)
+data(movies)
+d <- dfm(movies, stopwords=TRUE)
 docs <- readCorpus(d, type=c("dtm"))
 vocab <- colnames(d)
 
-x <- stm(docs$documents, docs$vocab, 15)
+x <- stm(docs$documents, docs$vocab, 5, max.em.its=20)
+labelTopics(x)
