e5b3ce0c5dd8fa532d42b79e536855eea423b09c
Paul Nulty <paul.nulty@gmail.com>
2014-06-03 15:40:42 +0100

    documentation for stopwords dataset and expanded twitter guide

--- R/dataDocs.R
+++ R/dataDocs.R
@@ -37,10 +37,19 @@ NULL
 #' @references \url{http://dl.acm.org/citation.cfm?id=1118704}
 NULL
 
-#' @name stopwords_EN
-#' @title A list containing common English stopwords
+#' @name stopwords
+#' @title A named list containing common stopwords in 14 languages
 #' @docType data
-#' @description A list containing common English stopwords
+#' @description SMART English stopwords from the SMART information retrieval
+#' system (obtained from
+#' http://jmlr.csail.mit.edu/papers/volume5/lewis04a/a11-smart-stop-list/english.stop)
+#' and a set of stopword lists from the Snowball stemmer project in
+#' different languages (obtained from
+#' http://svn.tartarus.org/snowball/trunk/website/algorithms/*/stop.txt). Supported
+#' languages are danish, dutch, english, finnish, french, german,
+#' hungarian, italian, norwegian, portuguese, russian, spanish, and
+#' swedish. Language names are case sensitive. Alternatively, their IETF
+#' language tags may be used.
 NULL
 
 #' @name sylCounts
