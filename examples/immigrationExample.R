ca19e80558c1dd005df06b46a4331623ef7e40cf
Paul Nulty <paul.nulty@gmail.com>
2014-06-10 13:24:20 +0100

    news article analysis example

--- examples/immigrationExample.R
+++ examples/immigrationExample.R
@@ -1,26 +1,21 @@
 library(quanteda)
 library(topicmodels)
 library(lda)
+library(ca)
 path = '/home/paul/Dropbox/LSETextMining/code/articles'
 attNames = c("paperName", "id")
 newsCorpus <- corpusFromFilenames(path, attNames, sep = "_")
 paperCount <- table(newsCorpus$attribs$paperName)
 topPapers<- names(sort(paperCount, decreasing = TRUE)[1:21])
-reducedCorpus <- subset(newsCorpus, paperName%in%topPapers)
+reducedCorpus <- subset(newsCorpus, paperName %in% topPapers)
 data(stopwords)
-fulld <- dfm(newsCorpus, stopwords=stopwords$english)
-fullByPaper <- dfm(newsCorpus, group = "paperName", stopwords=stopwords$english)
-dtmReduced <- dfmTrim(fulld, minCount=1, minDoc=10) 
-td <- dfm2tmformat(dtmReduced)
-tdByPaper <- - dfm2tmformat(dtmByPaper)
-ldaModel <- LDA(td, control = list(alpha = 0.1), k = 10)
+reducedDfm <- dfm(reducedCorpus, stopwords=stopwords$english)
+reducedByPaper <- dfm(newsCorpus, group = "paperName", stopwords=stopwords$english)
+trimByPaper <- dfmTrim(fulld, minCount=1, minDoc=2) 
+trimByPaperTm<- dfm2tmformat(trimByPaper)
+ldaModel <- LDA(trimByPaperTm, control = list(alpha = 0.1), k = 10)
 get_terms(ldaModel, k=5)
-
-ldaModelbyPaper <- LDA(fullByPaper, control = list(alpha = 0.1), k = 10)
-get_terms(ldaModelbyPaper, k=5)
-
-library(ca)
-model <- ca(t(fullByPaper), nd = 1)
+model <- ca(t(trimByPaper), nd = 2)
 
 
 ldaResult <- lda.collapsed.gibbs.sampler(documents=td$documents, 
