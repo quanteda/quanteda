53cef6906b825ea67968efc9932534d1a97f9d68
Kenneth Benoit <kbenoit@lse.ac.uk>
2015-11-23 14:23:45 +0000

    Started rewrite of tagPOS()

--- R/pos.R
+++ R/pos.R
@@ -1,8 +1,5 @@
-require(openNLP)
-require(NLP)
-
-pos.env <- new.env()
-pos.env$model <- Maxent_POS_Tag_Annotator()
+# pos.env <- new.env()
+# pos.env$model <- Maxent_POS_Tag_Annotator()
 
 anno_word <- Simple_Word_Token_Annotator(function(s){
   return(NLP::Span(pos.env$from, pos.env$to))
@@ -11,12 +8,24 @@ anno_sent <- Simple_Sent_Token_Annotator(function(s){
   return(NLP::Span(head(pos.env$from, 1), tail(pos.env$to, 1)))
 })
 
-# @examples
-# tokens <- tokenize(inaugTexts[1], simplify=TRUE)
-# pos <- tagPOS(tokens) # tokens should be sentence
-# 
-# @export
-tagPOS <- function(x, ...){
+
+
+#' tag parts of speech in a tokenized list
+#'
+#' Add tags to a tokenized list of parts of speech
+#' @param x list of tokens to tag
+#' @param ... not currently used
+#' @export
+tagPOS <- function(x, ...)
+  UseMethod("tagPOS")
+
+
+#' @rdname tagPOS
+#' @examples
+#' tokens <- tokenize(inaugTexts[1], simplify=TRUE)
+#' pos <- tagPOS(tokens) # tokens should be sentence
+#' @export
+tagPOS.tokenizedTexts <- function(x, ...){
   length <- stringi::stri_length(x)
   to <- as.integer(cumsum(length))
   from <- as.integer(to - (length - 1))
