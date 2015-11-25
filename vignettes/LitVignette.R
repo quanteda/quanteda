ec0c80295ab42f4ec6c160d5b38736861e95b732
Ken Benoit <kbenoit@lse.ac.uk>
2015-11-25 18:00:42 +0000

    Ken's minor syntax's edits to Jockers vignette

--- vignettes/LitVignette.R
+++ vignettes/LitVignette.R
@@ -7,7 +7,7 @@ require(quanteda)
 # read the text as a single file
 # mobydicktf <- textfile("http://www.gutenberg.org/cache/epub/2701/pg2701.txt")
 mobydicktf <- textfile("~/Dropbox/QUANTESS/corpora/project_gutenberg/pg2701.txt")
-mobydicktf
+#mobydicktf
 
 ## ------------------------------------------------------------------------
 substring(texts(mobydicktf), 1, 75)
@@ -18,6 +18,8 @@ mobydickText <- texts(mobydicktf)
 endMetadataIndex <- regexec("CHAPTER 1. Loomings.", mobydickText)[[1]]
 metadata.v <- substring(texts(mobydicktf), 1, endMetadataIndex - 1)
 
+
+## ------------------------------------------------------------------------
 # verify that "orphan" is the end of the novel
 kwic(mobydickText, "orphan")
 
@@ -28,28 +30,37 @@ novel.v <- substring(mobydickText, endMetadataIndex,
 ## ------------------------------------------------------------------------
 # lowercase
 novel.lower.v <- toLower(novel.v)
+
+## ------------------------------------------------------------------------
 # tokenize
 moby.word.v <- tokenize(novel.lower.v, removePunct = TRUE, simplify = TRUE)
+length(moby.word.v)
+total.length <- length(moby.word.v)
 str(moby.word.v)
 moby.word.v[1:10]
-moby.word.v[99986]  # different because we removed punctiation
+moby.word.v[99986] 
 
 moby.word.v[c(4,5,6)]
 
 head(which(moby.word.v=="whale"))
 
 ## ------------------------------------------------------------------------
-length(moby.word.v[which(moby.word.v=="whale")])
-length(moby.word.v)
-ntoken(novel.v, removePunct = TRUE)
 
-whale.hits.v <- length(moby.word.v[which(moby.word.v=="whale")])
+moby.word.v <- tokenize(novel.lower.v, simplify = TRUE)
+# count of the word 'whale'
+length(moby.word.v[which(moby.word.v == "whale")])
 
-# Put a count of total words into total.words.v 
-total.words.v <- length(moby.word.v)
-# now divide
-whale.hits.v/total.words.v
+# total occurrences of 'whale' including possessive
+length(moby.word.v[which(moby.word.v == "whale")]) + length(moby.word.v[which(moby.word.v == "whale's")])
 
+nrow(kwic(novel.lower.v, "whale"))
+nrow(kwic(novel.lower.v, "whale*")) # includes words like 'whalemen'
+total.whale.hits <- nrow(kwic(novel.lower.v, "^whale('s)?\\b", valuetype = 'regex'))
+
+## ------------------------------------------------------------------------
+total.whale.hits/ntoken(novel.lower.v, removePunct=TRUE)  
+
+## ------------------------------------------------------------------------
 # total unique words
 length(unique(moby.word.v))
 ntype(toLower(novel.v), removePunct = TRUE)
@@ -62,13 +73,10 @@ mobyDfm[, "whale"]
 topfeatures(mobyDfm)
 plot(topfeatures(mobyDfm, 100), log = "y", cex = .6, ylab = "Term frequency")
 
-# whale:token ratio
-length(which(moby.word.v == "whale")) / ntoken(mobyDfm)
-
 ## ----eval=TRUE-----------------------------------------------------------
 # frequencies of 'he' and 'she' - these are matrixes, not numerics
 mobyDfm[, c("he", "she", "him", "her")]
-
+mobyDfm[, "her"]
 mobyDfm[, "him"]/mobyDfm[, "her"]
 mobyDfm[, "he"]/mobyDfm[, "she"]
 
@@ -86,17 +94,28 @@ plot(kwic(novel.v, "whale"))
 plot(kwic(novel.v, "Ahab"))
 
 ## ----eval = FALSE--------------------------------------------------------
-#  require('ggplot2')
-#  qplot(whales, geom="histogram", binwidth=1000)
-#  qplot(whales, geom="density", adjust=0.1)
+#  # identify the chapter break locations
+#  (chap.positions.v <- kwic(novel.v, "CHAPTER \\d", valuetype = "regex")$position)
 
-## ----eval=FALSE----------------------------------------------------------
-#  kwic(mobyCorp, 'chapter')
-#  chapters <- segment(mobyCorp, what='other', delimiter="CHAPTER\\s\\d", perl=TRUE)
-#  chapDfm <- dfm(chapters)
-#  barplot(as.numeric(chapDfm[, 'whale']))
-#  barplot(as.numeric(chapDfm[, 'ahab']))
-#  
+## ------------------------------------------------------------------------
+mobyCorp <- corpus(mobydickText)
+kwic(mobydickText, 'chapter')
+chaptersCorp <- segment(mobyCorp, what='other', delimiter="CHAPTER\\s\\d", perl=TRUE)
+
+## ----eval=TRUE-----------------------------------------------------------
+chapDfm <- dfm(chaptersCorp)
+barplot(as.numeric(chapDfm[, 'whale']))
+barplot(as.numeric(chapDfm[, 'ahab']))
+
+## ----eval=TRUE-----------------------------------------------------------
+relDfm <- weight(chapDfm, type='relFreq')
+barplot(as.numeric(relDfm[, 'whale']))
+barplot(as.numeric(relDfm[, 'ahab']))
+
+## ------------------------------------------------------------------------
+require('ggplot2')	
+qplot(as.numeric(relDfm[, 'whale']), geom="histogram", binwidth=0.001)
+qplot(as.numeric(relDfm[, 'whale']), geom="density", adjust=0.1)
 
 ## ----eval=TRUE-----------------------------------------------------------
 #ttr <- statLexdiv(chapDfm)
