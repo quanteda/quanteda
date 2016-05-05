0881952a6b4bfd0c9a301c4266a5313eb34f2468
koheiw <watanabe.kohei@gmail.com>
2016-05-05 13:28:40 +0100

    Use RCPP types for Windows

--- src/tokens.cpp
+++ src/tokens.cpp
@@ -11,11 +11,11 @@ List select_tokens_cpp(SEXP x,  std::vector< std::string > types, bool remove, b
   
   int len_texts = texts.size();
   for (int h=0; h < len_texts; h++){
-    std::vector<std::string> text = texts[h];
-    std::vector<std::string> text_temp;
+    Rcpp::StringVector text = texts[h];
+    Rcpp::StringVector text_temp;
     int len_text = text.size();
     for (int i=0; i < len_text; i++){
-      std::string token = text[i];
+      Rcpp::String token = text[i];
       bool is_in = set_types.find(token) != set_types.end();
       if(is_in == remove){
         //Rcout << "Match " << i << ' ' << token << "\n";
