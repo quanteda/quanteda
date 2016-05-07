49909eca38110fd6d43ff918ca6e3a21d40e3ade
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-05-07 18:53:45 +0100

    This is the fastest in C++, but stil slower than current dfm indexing

--- src/index.cpp
+++ src/index.cpp
@@ -11,90 +11,35 @@ using namespace std;
 
 
 // [[Rcpp::export]]
-std::vector<int> index_cpp_std(std::vector< std::string > tokens, 
-                                    std::vector< std::string > types){
+Rcpp::List index_cpp(SEXP x, 
+                      const std::vector< std::string > &types,
+                      const int &n){
   
-  
-  std::map<string, int> index;
-  
-  // Assign integer IDs
-  for (int h = 0; h < types.size(); h++){
-    std::string type = types[h];
-    index[type] = h + 1;
-    //index["aaaaa"] = h + 1;
-    //index.insert(std::pair<string,int>(type, h));
-  }
-  //Rcout << "Types " <<  index.size() << "\n";
-  
-  // Convert tokens to IDs
-  std::vector<int> indices;
-  for (int i = 0; i < tokens.size(); i++){
-    std::string token = tokens[i];
-    //Rcout << "Token " <<  token << " " << index[token] << "\n";
-    indices.push_back(index[token]);
-  }
-  return indices;
-}
-
-
-// [[Rcpp::export]]
-NumericVector index_cpp_std_umap(const std::vector< std::string > &tokens, 
-                                 const std::vector< std::string > &types){
-  
-  std::unordered_map<string, int> index;
+  Rcpp::List texts(x);
+  std::unordered_map<String, int> index;
   
   // Assign integer IDs
-  for (int h = 0; h < types.size(); h++){
-    index[types[h]] = h + 1;
+  for (int g = 0; g < types.size(); g++){
+    index[types[g]] = g + 1;
   }
 
   // Convert tokens to IDs
-  std::vector<int> indices(tokens.size());
-  for (int i = 0; i < tokens.size(); i++){
-    //Rcout << "Token " <<  token << " " << index[token] << "\n";
-    indices[i] = index[tokens[i]];
-  }
-  return wrap(indices);
-}
-
-// [[Rcpp::export]]
-std::vector<int> index_cpp_boost(std::vector<std::string> tokens, std::vector<std::string> types) {
-  
-  typedef unordered_map<std::string, int> unordered_map;
-  unordered_map map;
-
-  for (int h = 0; h < types.size(); h++){
-    std::string type = types[h];
-    map.emplace(type, h + 1);
-  }
-  std::vector<int> indices;
-  for (int i = 0; i < tokens.size(); i++){
-    std::string token = tokens[i];
-    //Rcout << "Token " <<  map[token] << "\n";
-    indices.push_back(map[token]);
-  }
-  return indices;
-
-}
-
-
-// [[Rcpp::export]]
-NumericVector index_cpp_r(StringVector tokens, 
-                         StringVector types){
-
-  // Convert tokens to IDs
-  NumericVector indices;
-  for (int i = 0; i < tokens.size(); i++){
-    for (int j = 0; j < types.size(); j++){
-      if(tokens[i] == types[j]){
-        indices.push_back(j);
-        break;
+  NumericVector idx_doc(n);
+  NumericVector idx_feat(n);
+  int k = 0;
+  for (int h = 0; h < texts.size(); h++){
+    StringVector tokens = texts[h];
+    //Rcout << "Text " <<  h << "\n";
+    for (int i = 0; i < tokens.size(); i++){
+      //Rcout << "Token " <<  tokens[i] << "\n";
+      if (index.find(tokens[i]) != index.end()){
+        idx_doc[k] = h + 1;
+        idx_feat[k] = index[tokens[i]];
+        k++;
       }
     }
   }
-  return indices;
+  return List::create(Named("doc") = idx_doc , Named("feature") = idx_feat);
 }
-
-
 
 
