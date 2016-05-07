326b0753dfe7db39653d840104bb43e931b13c24
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-05-07 15:20:18 +0100

    Fast but keeps character encoding

--- src/tokens.cpp
+++ src/tokens.cpp
@@ -5,43 +5,41 @@
 #include <unordered_set>
 using namespace Rcpp;
 
+
 // [[Rcpp::export]]
 Rcpp::List select_tokens_cppl(SEXP x,
                                const std::vector< std::string > &types,
                                const bool &remove,
                                const bool &spacer){
-
+  
   Rcpp::List texts(x);
   Rcpp::List texts_temp(texts.size());
   std::unordered_set<std::string> set_types (types.begin(), types.end());
   //const std::string space = "";
   int len_texts = texts.size();
   for (int h=0; h < len_texts; h++){
-    std::vector< std::string > text = texts[h];
-    std::vector< std::string > text_temp(text.size()); // make vector in the same length as original
+    CharacterVector text = texts[h];
+    CharacterVector text_temp(text.size()); // make vector in the same length as original
     //text_temp.reserve(text.size());
     int j = 0;
     //int omit = 0;
     for (int i=0; i < text.size(); i++){
-      //Rcpp::String token = text[i];
-      std::string token = text[i];
+      Rcpp::String token = text[i];
+      //std::string token = text[i];
       bool is_in = set_types.find(token) != set_types.end();
       if(is_in == remove){
         //Rcout << "Match " << i << ' ' << text_temp[i] << "\n";
         if(spacer){
           text_temp[j] = "";
           j++;
-        }else{
-          text_temp.pop_back(); // shorten the vector
         }
       }else{
         text_temp[j] = token;
         j++;
       }
     }
-    texts_temp[h] = text_temp;
+    texts_temp[h] = text_temp[seq(0, j-1)];
   }
   return texts_temp;
 }
-
 
