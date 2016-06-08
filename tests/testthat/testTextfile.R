0c0d71dc0371d523d224689819f033b39e926fbc
Adam Obeng <github@binaryeagle.com>
2016-06-08 14:03:06 +0100

    Add debugging output for travis-ci
    Signed-off-by: Adam Obeng <github@binaryeagle.com>

--- tests/testthat/testTextfile.R
+++ tests/testthat/testTextfile.R
@@ -378,18 +378,18 @@ test_that("test docvars.corpusSource warning with field!=NULL", {
      )
 })
 
-  # Currently, these encodings don't work for reasons that seem unrelated 
-  # to quanteda, and are either a problem in base R or on travis-ci
-  broken_encodings <- c(
-      "437", "850", "852", "855", "857", "860", "861", "862", "863", "865", 
-      "869", "BIG5-HKSCS", "CHINESE", "CP1251", "CP1255", "CP1256", "CP1361",
-      "CP154", "CP737", "CP858", "CP864", "CP856", "CP932", "CP950", "EUC-JISX0213", 
-      "EUC-JP", "EUC-KR", "GB18030", "HEBREW", "HZ","ISO-2022-JP-1", "ISO-2022-JP-2", 
-      "ISO-2022-JP-3", "ISO-8859-11", "ISO-IR-166", "KOI8-R",
-      "UNICODE-1-1-UTF-7",
-      "MACCENTRALEUROPE", "MACCYRILLIC", "MACGREEK", "MACICELAND", "MACTURKISH",
-      "MS_KANJI", "SHIFT_JISX0213"
-  )
+# Currently, these encodings don't work for reasons that seem unrelated 
+# to quanteda, and are either a problem in base R or on travis-ci
+broken_encodings <- c(
+    "437", "850", "852", "855", "857", "860", "861", "862", "863", "865", 
+    "869", "BIG5-HKSCS", "CHINESE", "CP1251", "CP1255", "CP1256", "CP1361",
+    "CP154", "CP737", "CP858", "CP864", "CP856", "CP932", "CP950", "EUC-JISX0213", 
+    "EUC-JP", "EUC-KR", "GB18030", "HEBREW", "HZ","ISO-2022-JP-1", "ISO-2022-JP-2", 
+    "ISO-2022-JP-3", "ISO-8859-11", "ISO-IR-166", "KOI8-R",
+    "UNICODE-1-1-UTF-7",
+    "MACCENTRALEUROPE", "MACCYRILLIC", "MACGREEK", "MACICELAND", "MACTURKISH",
+    "MS_KANJI", "SHIFT_JISX0213"
+)
 
 
 FILEDIR <- quanteda:::mktemp(directory=T)
@@ -424,6 +424,8 @@ test_that("Test loading all these files at once with different encodings", {
 
 
 test_that("test textfile encoding parameter: UTF-8 encoded file, read as UTF-16 (should not work)", {
+     print(file.path(FILEDIR, 'UTF-8__characters.txt'))
+     print(file.exists(file.path(FILEDIR, 'UTF-8__characters.txt')))
      expect_warning(
        misread_texts <- texts(textfile(file.path(FILEDIR, 'UTF-8__characters.txt'), encoding='utf-16'))
      )
