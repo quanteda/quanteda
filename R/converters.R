d3d7a3aefd6cb07d1d06e65ebab2498cdf882eb0
Kenneth Benoit <kbenoit@lse.ac.uk>
2016-06-14 23:20:47 +0100

    Fix #209 inefficient convert(x, to = "stm) Change NEWS to NEWS.md

--- R/converters.R
+++ R/converters.R
@@ -235,30 +235,31 @@ ldaformat2dtm <- function (documents, vocab, omit_empty = TRUE)
 # mydfmStm <- dfm2stmformat(mydfm)
 # str(mydfmStm)
 # @export
-dfm2stmformatOLD <- function(data) {
-    sortedData <- data[, order(features(data))]
-    vocab <- features(sortedData)
-    stmdocs <- list()
-    length(stmdocs) <- ndoc(data)
-    names(stmdocs) <- docnames(data)
-    for (d in docnames(data)) {
-        temp <- as.matrix(rbind(1:length(vocab), as.integer(as.matrix(sortedData[d, ]))))
-        stmdocs[[d]] <- temp[, which(temp[2, ] > 0), drop=FALSE]
-    }
-    list(documents=stmdocs, vocab = vocab, meta = NULL)
-}
 
 dfm2stmformat <- function(data) {
-    sortedData <- data[, order(features(data))]
-    sortedData <- as(sortedData, "dgTMatrix")
-    stmdocs <- list()
-    length(stmdocs) <- ndoc(data)
-    names(stmdocs) <- docnames(data)
-    for (d in seq_len(nrow(sortedData))-1) {
-        temp <- rbind(sortedData@j[sortedData@i==d] + 1, sortedData@x[sortedData@i==d])
-        stmdocs[[d+1]] <- temp
-    }
-    list(documents = stmdocs, vocab = colnames(sortedData), meta = NULL)
+    # sort features into alphabetical order
+    data <- data[, order(features(data))]
+    data <- as(data, "dgTMatrix")
+    
+    # find out which documents are not empty
+    non_empty_docs <- which(rowSums(data) != 0)
+    
+    # convert counts to STM documents format
+    documents <- ijv.to.doc(data[non_empty_docs, ]@i, data[non_empty_docs, ]@j, data[non_empty_docs, ]@x) 
+    names(documents) <- rownames(data)[non_empty_docs]
+    
+    # return the object
+    list(documents = documents, vocab = colnames(data), meta = NULL)
+}
+
+
+# borrowed from the STM package
+ijv.to.doc <- function(i, j, v) {
+    index <- split(j, i)
+    index <- lapply(index, as.integer)
+    count <- split(v, i)
+    count <- lapply(count, as.integer)
+    mapply(rbind, index, count)
 }
 
 
