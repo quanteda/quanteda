3e87b791124e3e5adaeeb77a39bd0790941af1cc
Adam Obeng <github@binaryeagle.com>
2016-06-22 11:01:02 +0100

    Add tests for collocation behaviour related to bug report
    Signed-off-by: Adam Obeng <github@binaryeagle.com>

---
+++ tests/testthat/testCollocation.R
@@ -0,0 +1,44 @@
+context('test collocation.R')
+
+row_in_df <- function(df, row) {
+    nrow(merge(df, row)) > 0
+}
+
+test_that("test that collocations do not span texts by default", {
+    actual_collocations <- data.frame(
+            collocations(
+                tokenize(c('this is a test', 'this is also a test'))
+            )
+        )
+    expect_false(
+        row_in_df(
+            actual_collocations,
+            data.frame(word1='test', word2='this')
+        )
+    )
+})
+
+test_that("test that collocations do not span punctuation by default", {
+    actual_collocations <- data.frame(
+        collocations(
+        tokenize(c('this is a test.', 'this is also a test'))
+        )
+    )
+    expect_false(
+        row_in_df(
+            actual_collocations,
+            data.frame(word1='test', word2='this')
+        )
+    )
+})
+
+test_that("test that collocations which span punctuation do not include punctuation", {
+
+    actual_collocations <- data.frame(
+        collocations(
+            tokenize(c('This is a test. This is also a test')), spanPunct=T
+        )
+    )
+    expect_false('.' %in% actual_collocations$word1)
+    expect_false('.' %in% actual_collocations$word2)
+})
