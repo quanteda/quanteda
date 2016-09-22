957218225c0f6dc49c4c22bc3c380006efe5918b
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-09-22 18:40:08 +0100

    tokenizeHashed seems to have small overhead, while memory consumption is constant

--- R/hashTokens.R
+++ R/hashTokens.R
@@ -23,24 +23,24 @@
 #' as.tokenizedTexts(toksHashed)
 
 hashTokens <- function(x, ...) {
-    UseMethod("hashTokens")
+  UseMethod("hashTokens")
 }
 
 #' @rdname hashTokens
 #' @export
 hashTokens.tokenizedTexts <- function(x, vocabulary, ...){
-    if (!is.tokenizedTexts(x)) 
-        stop("Input must be tokenizedTexts")
-    types <- unique(unlist(x, use.names = FALSE))
-    if(missing(vocabulary)){
-      vocabulary <- types  
-    }else{
-      vocabulary <- c(vocabulary, types[!types %in% vocabulary]) 
-    }
-    xNumeric <- lapply(x, function(x,y) fmatch(x,y), vocabulary)
-    class(xNumeric) <- c("tokenizedTextsHashed", class(xNumeric))
-    attr(xNumeric, "vocabulary") <- vocabulary
-    return(xNumeric)
+  if (!is.tokenizedTexts(x)) 
+    stop("Input must be tokenizedTexts")
+  types <- unique(unlist(x, use.names = FALSE))
+  if(missing(vocabulary)){
+    vocabulary <- types  
+  }else{
+    vocabulary <- c(vocabulary, types[!types %in% vocabulary]) 
+  }
+  xNumeric <- lapply(x, function(x,y) fmatch(x,y), vocabulary)
+  class(xNumeric) <- c("tokenizedTextsHashed", class(xNumeric))
+  attr(xNumeric, "vocabulary") <- vocabulary
+  return(xNumeric)
 }
 
 #' @rdname hashTokens
@@ -48,28 +48,28 @@ hashTokens.tokenizedTexts <- function(x, vocabulary, ...){
 #' making the methods available for this object type available to this object.
 #' @export
 as.tokenizedTexts.tokenizedTextsHashed <- function(x, ...) {
-     if (!is.tokenizedTextsHashed(x))
-        stop("input must be a tokenizedTextsHashed types")
-    
-      types <- attr(x, "vocabulary")
-      xTt <- lapply(x, function(x,y) y[x], types)
-      xTt
+  if (!is.tokenizedTextsHashed(x))
+    stop("input must be a tokenizedTextsHashed types")
+  
+  types <- attr(x, "vocabulary")
+  xTt <- lapply(x, function(x,y) y[x], types)
+  xTt
 }
 
 #' @rdname hashTokens
 #' @export
 as.list.tokenizedTextsHashed <- function(x, ...){
-    if (!is.tokenizedTextsHashed(x))
-        stop("input must be a tokenizedTextsHashed types")
-    attr(x,"vocabulary") <- NULL
-    xList <- unclass(x)
-    xList
+  if (!is.tokenizedTextsHashed(x))
+    stop("input must be a tokenizedTextsHashed types")
+  attr(x,"vocabulary") <- NULL
+  xList <- unclass(x)
+  xList
 }
 
 #' @rdname hashTokens
 #' @export
 is.tokenizedTextsHashed <- function(x){
-    ifelse("tokenizedTextsHashed" %in% class(x), TRUE, FALSE)
+  ifelse("tokenizedTextsHashed" %in% class(x), TRUE, FALSE)
 }
 
 #' print a tokenizedTextsHashed objects
@@ -79,43 +79,39 @@ is.tokenizedTextsHashed <- function(x){
 #' @export
 #' @method print tokenizedTextsHashed
 print.tokenizedTextsHashed <- function(x, ...) {
-    ndocuments <- ifelse(is.list(x), length(x), 1)
-    cat("tokenizedTextHashed object from ", ndocuments, " document", 
-        ifelse(ndocuments > 1, "s", ""), ".\n", sep = "")
-    if (is.list(x)) { 
-        class(x) <- "listof"
-        print(x, ...)
-    } else {
-        x <- as.character(x)
-        print(x, ...)
-    }
+  ndocuments <- ifelse(is.list(x), length(x), 1)
+  cat("tokenizedTextHashed object from ", ndocuments, " document", 
+      ifelse(ndocuments > 1, "s", ""), ".\n", sep = "")
+  if (is.list(x)) { 
+    class(x) <- "listof"
+    print(x, ...)
+  } else {
+    x <- as.character(x)
+    print(x, ...)
+  }
 }
 
 #' @examples 
 #' txt <- c('a b c d e', 'd e f g h', 'f g h i j', 'i j k l m')
 #' tokenizeHashed(txt, size_chunk=2)
-#' 
+#'
 #' data(SOTUCorpus, package = "quantedaData")
-#' toks <- hashTokens(tokenize(texts(SOTUCorpus)))
-#' gc()
-#' toks2 <- tokenizeHashed(texts(SOTUCorpus))
-#' gc()
-#' tail(toks[[1]], 20)
-#' tail(toks2[[1]], 20)
-#' 
+#' txt <- rep(unlist(tokenize(SOTUCorpus, what='sentence')), 20)
+#' system.time(toks <- hashTokens(tokenize(txt)))
+#' system.time(toks2 <- tokenizeHashed(txt))
 #' 
 #' @rdname hashTokens
 #' @export
-tokenizeHashed <- function(x, size_chunk=100, ...){
+tokenizeHashed <- function(x, size_chunk=1000, ...){
   
   xSubs <- split(x, ceiling(seq_along(x)/size_chunk))
   xTokSubs <- list()
   for(i in 1:length(xSubs)){
-    cat('Tokenizing and hashing ...\n')
+    #cat('Tokenizing and hashing ...\n')
     if(i == 1){
-      xTokSubs[[i]] <- hashTokens(tokenize(xSubs[[i]]))
+      xTokSubs[[i]] <- hashTokens(tokenize(xSubs[[i]], ...))
     }else{
-      xTokSubs[[i]] <- hashTokens(tokenize(xSubs[[i]]), attr(xTokSubs[[i-1]], "vocabulary"))
+      xTokSubs[[i]] <- hashTokens(tokenize(xSubs[[i]], ...), attr(xTokSubs[[i-1]], "vocabulary"))
     }
   }
   xTok <- unlist(xTokSubs, recursive = FALSE)
