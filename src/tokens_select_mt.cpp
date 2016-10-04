57d73572c7c45db757972c30ef55697244d744af
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-10-04 09:22:50 +0100

    Moved from dev_selectFeatures_mt but tokens_select_mt.cpp is not working.

--- src/tokens_select_mt.cpp
+++ src/tokens_select_mt.cpp
@@ -1,44 +1,38 @@
 #include <Rcpp.h>
 #include <vector>
-#include <mutex>
+
 // [[Rcpp::depends(RcppParallel)]]
 #include <RcppParallel.h>
 // [[Rcpp::plugins(cpp11)]]
 #include <unordered_set>
 using namespace Rcpp;
 using namespace RcppParallel;
-std::mutex lock_input3;
-std::mutex lock_output3;
 
-struct select_tokens3 : public Worker
+struct selecttokens : public Worker
 {
   std::vector<CharacterVector> &input;
   std::vector<CharacterVector> &output;
   const std::unordered_set<String> set_types;
   const bool remove;
   const bool spacer;
+  tthread::mutex lock_input;
+  tthread::mutex lock_output;
   
   // Constructor
-  select_tokens3(std::vector<CharacterVector> &input, std::vector<CharacterVector> &output, const std::unordered_set<String> set_types, bool remove, bool spacer): 
+  selecttokens(std::vector<CharacterVector> &input, std::vector<CharacterVector> &output, const std::unordered_set<String> set_types, bool remove, bool spacer): 
                 input(input), output(output), set_types(set_types), remove(remove), spacer(spacer){}
   
   // parallelFor calles this function with size_t
   void operator()(std::size_t begin, std::size_t end){
-    
-    std::vector<CharacterVector> texts(end - begin + 1);
-    int f = 0;
-    lock_input3.lock();
-    //Rcout << "Range " << begin << " " << end << "\n";
-    for (int g = begin; g < end + 1; g++){ 
-      texts[f] = input[g];
-      f++;
-    }
-    lock_input3.unlock();
-    
-    return;
-    for (int h = 0; h < texts.size(); h++){
+    Rcout << "Range " << begin << " " << end << "\n";
+    for (int h = begin; h < end; h++){
+      
+      lock_input.lock();
       //Rcout << "H " << h << "\n";
-      CharacterVector text = texts[h];
+      CharacterVector text = clone(input[h]);
+      //Rcout << "Size " << text.size() << "\n";
+      lock_input.unlock();
+      
       CharacterVector text_temp(text.size()); // make vector in the same length as original
       int j = 0;
       for (int i = 0; i < text.size(); i++){
@@ -55,18 +49,19 @@ struct select_tokens3 : public Worker
           j++;
         }
       }
-      
+      lock_output.lock();
       if(j == 0){
         output[h] = CharacterVector(); // nothing left
       }else{
         output[h] = text_temp[seq(0, j - 1)];
       }
+      lock_output.unlock();
     }
   }
 };
 
 // [[Rcpp::export]]
-List select_tokens_cppl_mt3(SEXP x, 
+List qatd_cpp_selecttokens_mt(SEXP x, 
                              CharacterVector &types,
                              const bool &remove,
                              const bool &spacer) {
@@ -78,12 +73,12 @@ List select_tokens_cppl_mt3(SEXP x,
   for (int g = 0; g < types.size(); g++){
     set_types.insert(types[g]);
   }
-  
-  // SquareRoot functor (pass input and output matrixes)
-  select_tokens3 select_tokens3(input, output, set_types, remove, spacer);
+  Rcout << "here\n";
+  // call parallelFor to do the work
+  selecttokens selecttokens(input, output, set_types, remove, spacer);
   
   // call parallelFor to do the work
-  parallelFor(0, input.size(), select_tokens3);
+  parallelFor(0, input.size(), selecttokens);
   
   return wrap(output);
 }
@@ -92,7 +87,6 @@ List select_tokens_cppl_mt3(SEXP x,
 /*** R
 
 toks <- list(letters, LETTERS)
-#select_tokens_cppl_mt(toks, c('b', 'O', 'g', 'N'), TRUE, TRUE)
-#select_tokens_cppl_mt(rep(toks, 1000), c('b', 'O', 'g', 'N'), FALSE, TRUE)
+qatd_cpp_selecttokens_mt(rep(toks, 10), c('b', 'O', 'g', 'N'), FALSE, TRUE)
 
 */
--- src/tokens_select_mt_hashed.cpp
+++ src/tokens_select_mt_hashed.cpp
@@ -1,7 +1,5 @@
 #include <Rcpp.h>
 #include <vector>
-#include <mutex>
-//#include <algorithm>    // std::transform
 
 // [[Rcpp::depends(RcppParallel)]]
 #include <RcppParallel.h>
@@ -10,36 +8,32 @@
 using namespace Rcpp;
 using namespace RcppParallel;
 
-struct select_tokens5 : public Worker
+struct selecttokens_hashed : public Worker
 {
   std::vector< std::vector<int> > &input;
   std::vector< std::vector<int> > &output;
   const std::unordered_set<int> set_types;
   const bool remove;
   const bool spacer;
-  //std::mutex lock_input5;
-  //std::mutex lock_output5;
-  
-  tthread::mutex lock_input5;
-  tthread::mutex lock_output5;
+  tthread::mutex lock_input;
+  tthread::mutex lock_output;
   
   // Constructor
-  select_tokens5(std::vector< std::vector<int> > &input, std::vector< std::vector<int> > &output, const std::unordered_set<int> set_types, bool remove, bool spacer): 
-                input(input), output(output), set_types(set_types), remove(remove), spacer(spacer){}
+  selecttokens_hashed(std::vector< std::vector<int> > &input, std::vector< std::vector<int> > &output, const std::unordered_set<int> set_types, bool remove, bool spacer): 
+                      input(input), output(output), set_types(set_types), remove(remove), spacer(spacer){}
   
   // parallelFor calles this function with size_t
   void operator()(std::size_t begin, std::size_t end){
     //Rcout << "Range " << begin << " " << end << "\n";
     for (int h = begin; h < end; h++){
       
-      //lock_input5.lock();
+      lock_input.lock();
       //Rcout << "H " << h << "\n";
       std::vector<int> text = input[h];
       //Rcout << "Size " << text.size() << "\n";
-      //lock_input5.unlock();
+      lock_input.unlock();
       
       std::vector<int> text_temp(text.size());// make vector in the same length as original
-      
       int j = 0;
       for (int i = 0; i < text.size(); i++){
         int token = text[i];
@@ -56,7 +50,7 @@ struct select_tokens5 : public Worker
         }
       }
 
-      //lock_output5.lock();
+      lock_output.lock();
       if(j == 0){
         output[h] = std::vector<int>(); // nothing left
       }else{
@@ -64,30 +58,26 @@ struct select_tokens5 : public Worker
         output[h] = text_temp;
       }
       //Rcout << "J " << j << "\n";
-      //lock_output5.unlock();
+      lock_output.unlock();
 
     }
   }
 };
 
 // [[Rcpp::export]]
-List select_tokens_cppl_mt5(SEXP x, 
-                           std::vector<int> &types,
-                           const bool &remove,
-                           const bool &spacer) {
+List qatd_cpp_selecttokens_mt_hashed(SEXP x, 
+                                     std::vector<int> &types,
+                                     const bool &remove,
+                                     const bool &spacer) {
   
   std::vector< std::vector<int> > input = Rcpp::as< std::vector< std::vector<int> > >(x);
   std::vector< std::vector<int> > output(input.size());
-  
-   //std::vector<int> types_hash(types.size());
-  //std::transform (types.begin(), types.end(), types_hash.begin(), hash);
-  //std::unordered_set<int> set_types (types_hash.begin(), types_hash.end());
-  
+
   std::unordered_set<int> set_types (types.begin(), types.end());
-  select_tokens5 select_tokens5(input, output, set_types, remove, spacer);
+  selecttokens_hashed selecttokens_hashed(input, output, set_types, remove, spacer);
   
   // call parallelFor to do the work
-  parallelFor(0, input.size(), select_tokens5);
+  parallelFor(0, input.size(), selecttokens_hashed);
   
   // return the output matrix
   return wrap(output);
@@ -99,6 +89,6 @@ List select_tokens_cppl_mt5(SEXP x,
 toks <- list(1:50, 200:250)
 targets <- c(10, 50, 220, 230)
 microbenchmark::microbenchmark(
-out <-  select_tokens_cppl_mt5(rep(toks, 1000000), targets, FALSE, TRUE),
+out <-  qatd_cpp_selecttokens_mt_hashed(rep(toks, 10000), targets, FALSE, TRUE),
 times=10)
 */
