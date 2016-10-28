eae2a603c406c34d9d5874899ea0fa08f986a489
Kenneth Benoit <kbenoit@lse.ac.uk>
2016-10-28 13:51:04 -0400

    Add CITATION file

---
+++ inst/CITATION.R
@@ -0,0 +1,20 @@
+citHeader("To cite package 'quanteda' in publications please use the following.")
+
+if (!exists("meta") || is.null(meta)) 
+    meta <- packageDescription("quanteda")
+year <- sub("-.*", "", meta$Date)
+note <- sprintf("R package version %s", meta$Version)
+
+citEntry(entry = "Manual",
+         title = "quanteda: Quantitative Analysis of Textual Data",
+         author = personList(as.person("Kenneth Benoit"),
+                             as.person("Paul Nulty")),                            
+         year = year,
+         note = note,
+         url = "https://github.com/kbenoit/quanteda",
+         
+         textVersion = 
+             sprintf("Benoit, Kenneth and Paul Nulty.  (%s).  \"quanteda: Quantitative Analysis of 
+                     Textual Data\".  R package version: %s.  URL https://github.com/kbenoit/quanteda", 
+                     year, meta$Version)
+)
