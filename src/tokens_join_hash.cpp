78262fe66d72055769b118cc4e96be87e9040092
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-11-06 15:38:54 +0000

    Underlying function of join_tokens for hashed toknes and some benchmarking

--- src/tokens_join_hash.cpp
+++ src/tokens_join_hash.cpp
@@ -1,5 +1,6 @@
 #include <Rcpp.h>
 #include <vector>
+#include <algorithm>  
 #include "quanteda.h"
 
 using namespace Rcpp;
@@ -8,57 +9,60 @@ using namespace quanteda;
 
 
 // [[Rcpp::export]]
-NumericVector join_tokens_hash(NumericVector tokens_, 
-                     NumericVector seq_,
-                     int id){
+NumericVector qatd_cpp_replace_hash_vector(NumericVector tokens_, 
+                                        NumericVector seq_,
+                                        int id){
   
   std::vector<int> tokens = Rcpp::as< std::vector<int> >(tokens_);
   std::vector<int> seq = Rcpp::as< std::vector<int> >(seq_);
-  //std::vector<int> seq_fill(seq.size(), 0);
-  //seq_fill[0] = id;
+  int len_seq = seq.size();
   std::vector<int>::iterator it;
   it = std::search(tokens.begin(), tokens.end(), seq.begin(), seq.end());
   //Rcout << std::distance(tokens.begin(), it) << "\n";
+  bool match = false;
   while(it != tokens.end()){
-    std::advance(it, seq.size());
-    //std::advance(it, 1);
-    it = std::search(it, tokens.end(), seq.begin(), seq.end());
-    //std::copy(seq_fill.begin(), seq_fill.end(), it);
-    
-    std::fill(it, it + seq.size(), 0);
+    match = true;
+    std::fill(it, std::min(it + len_seq, tokens.end()), 0);
     *it = id;
+    std::advance(it, seq.size());
+    it = std::search(it, tokens.end(), seq.begin(), seq.end());
     //Rcout << std::distance(tokens.begin(), it) << "\n";
   }
-  
+  if(match) tokens.erase(std::remove(tokens.begin(), tokens.end(), 0), tokens.end());
   return wrap(tokens);
 
 }
 
 // [[Rcpp::export]]
-void join_tokens_cppl(List texts, 
-                      const std::vector<bool> &flags,
-                      const NumericVector &seq,
-                      const int id){
-
+List qatd_cpp_replace_hash_list(List texts_, 
+                                const std::vector<bool> flags,
+                                const NumericVector seq,
+                                const int id){
+  List texts = clone(texts_);
   int len = texts.size();
   if(flags.size() != len){
     Rcout << "Invalid flag is given\n";
-    return;
+    return(texts);
   }
   for (int h = 0; h < len; h++){
     if(flags[h]){
       //Rcout << "Text " << h << "\n";
-      join_tokens_hash(texts[h], seq, id);
+      texts[h] = qatd_cpp_replace_hash_vector(texts[h], seq, id);
     }
   }
+  return(texts);
 }
 
 /***R
 toks <- rep(letters, 1000)
 toks_hash <- rep(1:26, 1000)
-microbenchmark::microbenchmark(
-  join_tokens_hash(toks_hash, c(3, 4), 1000),
-  quanteda:::matchSequence(c(3, 4), toks_hash), times=1
-)
+
+qatd_cpp_replace_hash_vector(toks_hash, c(3, 4, 5), 9999)
+
+# microbenchmark::microbenchmark(
+#   qatd_cpp_replace_hash_vector(toks_hash, c(3, 4), 9999),
+#   join_tokens_cpp(toks, c('c', 'd'), '_'),
+#   quanteda:::matchSequence(c(3, 4), toks_hash), times=100
+# )
 
 */
