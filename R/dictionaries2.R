06cf055dcc445fc4a626ec1a99ab1bb8a4af47b0
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-11-14 11:36:08 +0000

    Move benchmarks to benchmarks/benchmarks_lookup/tokens.R

--- R/dictionaries2.R
+++ R/dictionaries2.R
@@ -23,7 +23,7 @@ applyDictionary2 <- function(x, dictionary, ...) {
 #' @param concatenator a charactor that connect words in multi-words entries
 #' @examples
 #' 
-#' dict_liwc <- dictionary(file='/home/kohei/Documents/Dictonary/LIWC/LIWC2007_English.dic')
+#' 
 #' toks <- tokens(inaugCorpus)
 #' dict <- dictionary(list(country = "united states", 
 #'                    law=c('law*', 'constitution'), 
@@ -37,30 +37,6 @@ applyDictionary2 <- function(x, dictionary, ...) {
 #' head(dfm(applyDictionary(toks, dict_fix, valuetype='fixed', verbose=TRUE)))
 #' head(dfm(applyDictionary2(toks, dict_fix, valuetype='fixed', verbose=TRUE)))
 #' 
-#' microbenchmark::microbenchmark(
-#'   r=applyDictionary(toks, dict, valuetype='fixed', verbose=FALSE),
-#'   cpp=applyDictionary2(toks, dict, valuetype='fixed', verbose=FALSE)
-#' )
-#' 
-#' microbenchmark::microbenchmark(
-#'   r=applyDictionary(toks, dict_liwc, valuetype='fixed', verbose=FALSE),
-#'   cpp=applyDictionary2(toks, dict_liwc, valuetype='fixed', verbose=FALSE),
-#'   times=1
-#' )
-#' 
-#' toks_short <- tokens(tokenize(inaugCorpus, what='sentence', simplify=TRUE))
-#' microbenchmark::microbenchmark(
-#'   r=applyDictionary(toks_short, dict, valuetype='fixed', verbose=FALSE),
-#'   cpp=applyDictionary2(toks_short, dict, valuetype='fixed', verbose=FALSE)
-#' )
-#' 
-#' microbenchmark::microbenchmark(
-#'   dfm=applyDictionary(dfm(toks), dict_liwc, valuetype='glob', verbose=FALSE),
-#'   tokens=applyDictionary2(toks, dict_liwc, valuetype='glob', verbose=FALSE),
-#'   times=1
-#' )
-#' 
-#' profvis::profvis(applyDictionary2(toks, dict_liwc[1], valuetype='glob', verbose=FALSE))
 #' 
 #' @export 
 applyDictionary2.tokens <- function(x, dictionary,
