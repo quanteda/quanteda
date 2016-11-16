6fa7a51a6e4bafbb70c131d4c5cbacb2f50f0f4d
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-11-16 16:35:47 +0000

    Add more realistic examples in benchmark

--- benchmarks/benchmark_join/tokens.R
+++ benchmarks/benchmark_join/tokens.R
@@ -2,24 +2,35 @@ toks <- tokens(inaugCorpus)
 
 seqs <- list(c('united', 'states'))
 microbenchmark::microbenchmark(
-    #old=joinTokens(toks, seqs, valuetype='fixed', verbose=TRUE),
-    new=joinTokens2(toks, seqs, valuetype='fixed', verbose=TRUE),
+    #joinTokens(toks, seqs, valuetype='fixed', verbose=TRUE), # too unstable
+    phrasetotoken(inaugCorpus, list('united states')),
+    joinTokens2(toks, seqs, valuetype='fixed', verbose=TRUE),
     times=10
 )
 
 seqs_not <- list(c('not', '*'))
 microbenchmark::microbenchmark(
-    #old=joinTokens(toks, seqs_not, valuetype='glob', verbose=TRUE),
-    new=joinTokens2(toks, seqs_not, valuetype='glob', verbose=TRUE),
+    #joinTokens(toks, seqs_not, valuetype='glob', verbose=TRUE), # too unstable
+    joinTokens2(toks, seqs_not, valuetype='glob', verbose=TRUE),
     times=10
 )
 
 seqs_will <- list(c('will', '*'))
 microbenchmark::microbenchmark(
-    #old=joinTokens(toks, seqs_will, valuetype='glob', verbose=TRUE),
-    new=joinTokens2(toks, seqs_will, valuetype='glob', verbose=TRUE),
+    #joinTokens(toks, seqs_will, valuetype='glob', verbose=TRUE), # too unstable
+    joinTokens2(toks, seqs_will, valuetype='glob', verbose=TRUE),
     times=10
 )
 
+dict_lex <- dictionary(file='/home/kohei/Documents/Dictonary/Lexicoder/LSDaug2015/LSD2015_NEG.lc3')
+seq_lex <- tokens(unlist(dict_lex, use.names = FALSE), hash=FALSE, what='fastest')
+
+# microbenchmark::microbenchmark(
+#     tokens(phrasetotoken(texts(inaugCorpus), dict_lex, valuetype='glob')),
+#     joinTokens2(tokens(inaugCorpus), seq_lex, valuetype='glob', verbose=TRUE),
+#     times=1
+# )
+
+profvis::profvis(joinTokens2(tokens(inaugCorpus), seq_lex, valuetype='glob', verbose=TRUE))
 profvis::profvis(joinTokens2(toks, seqs_neg, valuetype='glob', verbose=FALSE))
 
