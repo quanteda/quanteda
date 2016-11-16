16c56faa761232501cf8dc252ae7eac8ae93077b
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-11-16 14:45:53 +0000

    Tidy up

--- R/joinTokens2.R
+++ R/joinTokens2.R
@@ -35,6 +35,11 @@ joinTokens2.tokenizedTexts <- function(x, ...) {
 #' joinTokens(toks, seqs, valuetype = "glob", case_insensitive = TRUE)
 #' joinTokens2(toks, seqs, valuetype = "glob", case_insensitive = TRUE)
 #' 
+#' microbenchmark::microbenchmark(
+#' joinTokens(toks, seqs, valuetype = "glob", case_insensitive = TRUE),
+#' joinTokens2(toks, seqs, valuetype = "glob", case_insensitive = TRUE))
+
+
 #' joinTokens(toks, seqs, valuetype = "glob", case_insensitive = FALSE)
 #' 
 #' seqs <- tokens(c("a b", "C D", "aa bb", "eEE FFf", "d_e e_f"), 
@@ -48,10 +53,10 @@ joinTokens2.tokens <- function(x, sequences, concatenator = "_",
                               verbose = FALSE, case_insensitive = TRUE, ...) {
     
     valuetype <- match.arg(valuetype)
-    seqs <- as.list(sequences)
     
     # Initialize
-    tokens <- qatd_cpp_deepcopy(x) # create empty tokens object
+    seqs <- as.list(sequences)
+    seqs <- seqs[lengths(seqs) > 1] # drop single words
     types <- types(x)
     
     # Convert to regular expressions, then to fixed
@@ -63,13 +68,21 @@ joinTokens2.tokens <- function(x, sequences, concatenator = "_",
     } else {
         seqs_fixed <- seqs
     }
-    if(length(seqs_fixed) == 0) next
-    seqs_id <- lapply(seqs_fixed, function(x) fmatch(x, types))
+    if(verbose) message(sprintf('Join %d sequences', length(seqs_fixed)))
+    if(length(seqs_fixed) == 0) return(x) # do nothing
+    
+    seqs_id <- lapply(seqs_fixed, fmatch, table=types)
     seqs_type <- sapply(seqs_fixed, paste0, collapse = concatenator)
-    res <- qatd_cpp_replace_int_list(x, seqs_id, fmatch(seqs_type, types), length(types) + 1)
-    tokens <- res$text
-    types <- c(types, seqs_type[res$id_new > 0])
+    ids <- fmatch(seqs_type, types)
+    res <- qatd_cpp_replace_int_list(x, seqs_id, ids, length(types) + 1)
 
+    # Select and types to add
+    ids_new <- res$id_new
+    names(ids_new) <- seqs_type
+    ids_new <- sort(ids_new)
+    types <- c(types, names(ids_new[length(types) < ids_new]))
+    
+    tokens <- res$text
     attributes(tokens) <- attributes(x)
     types(tokens) <- types
     
