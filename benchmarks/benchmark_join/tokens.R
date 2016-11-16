ea0d7286c85cb3a95f760aeb59e83f754bb612d8
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-11-16 14:58:11 +0000

    Exclude old joinTokens from banchmakr as it is too unstable

--- benchmarks/benchmark_join/tokens.R
+++ benchmarks/benchmark_join/tokens.R
@@ -2,16 +2,16 @@ toks <- tokens(inaugCorpus)
 
 seqs <- list(c('united', 'states'))
 microbenchmark::microbenchmark(
-    old=joinTokens(toks, seqs, valuetype='fixed', verbose=FALSE),
-    new=joinTokens2(toks, seqs, valuetype='fixed', verbose=FALSE),
+    #old=joinTokens(toks, seqs, valuetype='fixed', verbose=TRUE),
+    new=joinTokens2(toks, seqs, valuetype='fixed', verbose=TRUE),
     times=10
 )
 
 seqs_neg <- list(c('not', '*'))
 microbenchmark::microbenchmark(
-    old=joinTokens(toks, seqs_neg, valuetype='glob', verbose=FALSE),
-    new=joinTokens2(toks, seqs_neg, valuetype='glob', verbose=FALSE),
-    times=1
+    #old=joinTokens(toks, seqs_neg, valuetype='glob', verbose=TRUE),
+    new=joinTokens2(toks, seqs_neg, valuetype='glob', verbose=TRUE),
+    times=10
 )
 
 profvis::profvis(joinTokens2(toks, dict_liwc[1], valuetype='glob', verbose=FALSE))
