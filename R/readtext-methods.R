f18e61cdb6c398523269668ab93e07e7c8fa815a
Kenneth Benoit <kbenoit@lse.ac.uk>
2016-11-17 15:43:21 -0500

    Add methods for readtext objects

---
+++ R/readtext-methods.R
@@ -0,0 +1,24 @@
+###
+### Methods to extend the "readtext" package, by defining methods for 
+### readtext objects
+###
+
+#' @noRd
+#' @export
+texts.readtext <- function(x, groups = NULL, ...) {
+    if (!is.null(groups))
+        stop("groups argument not supported for texts() on a readtext object")
+    result <- x[["text"]]
+    names(result) <- row.names(x)
+    result
+}
+
+#' @noRd
+#' @export
+docvars.readtext <- function(x, field = NULL) {
+    if (!is.null(field))
+        warning("field argument not used for docvars on a readtext object", noBreaks. = TRUE)
+    as.data.frame(x[, -which(names(x)=="text"), drop = FALSE])
+}
+
+
