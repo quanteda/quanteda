701498faaa8958ef0e7a6bbf7099154512c8aa24
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-11-18 19:35:47 +0000

    Fix case sensitive mode

--- R/regex2fixed3.R
+++ R/regex2fixed3.R
@@ -13,10 +13,13 @@
 regex2fixed3 <- function(regex, types, valuetype, case_insensitive = FALSE) {
     
     # Initialize
-    exact <- FALSE
     fixed <- list()
     types_lower <- toLower(types)
-    exact <- (valuetype == 'fixed')
+    if(valuetype == 'fixed'){
+        exact <- TRUE
+    }else{
+        exact <- FALSE
+    }
     
     # Construct tree to reduce search time
     tree <- list(head=split(types, stri_sub(types_lower, 1, 1)),
@@ -25,8 +28,12 @@ regex2fixed3 <- function(regex, types, valuetype, case_insensitive = FALSE) {
     # Make case insensitive
     if(case_insensitive){
         regex <- lapply(regex, toLower)
-        tree_lower <- list(head=lapply(tree$head, toLower),
-                           tail=lapply(tree$tail, toLower))
+        types_search <- types_lower
+        tree_search <- list(head=lapply(tree$head, toLower),
+                            tail=lapply(tree$tail, toLower))
+    }else{
+        types_search <- types
+        tree_search <- tree
     }
     
     # Separate multi and single-entry patterns
@@ -36,11 +43,8 @@ regex2fixed3 <- function(regex, types, valuetype, case_insensitive = FALSE) {
     
     # Process multi-entry patterns
     for(pat_multi in pats_multi) {
-        if(case_insensitive){
-            fixed_multi <- subset_types2(pat_multi, types, types_lower, tree, tree_lower, exact)
-        }else{
-            fixed_multi <- subset_types2(pat_multi, types, types, tree, tree, exact)
-        }
+        print(pat_multi)
+        fixed_multi <- subset_types2(pat_multi, types, types_search, tree, tree_search, exact)
         fixed_comb <- as.matrix(do.call(expand.grid, c(fixed_multi, stringsAsFactors = FALSE))) # create all possible combinations
         fixed <- c(fixed, unname(split(fixed_comb, row(fixed_comb))))
     }
@@ -48,11 +52,7 @@ regex2fixed3 <- function(regex, types, valuetype, case_insensitive = FALSE) {
     # Process single-entry patterns
     if(length(pats_single) > 0){
         pats_single <- unlist(pats_single, use.names = FALSE)
-        if(case_insensitive){
-            fixed_single <- unlist(subset_types2(pats_single, types, types_lower, tree, tree_lower, exact), use.names = FALSE)
-        }else{
-            fixed_single <- unlist(subset_types2(pats_single, types, types, tree, tree, exact), use.names = FALSE)
-        }
+        fixed_single <- unlist(subset_types2(pats_single, types, types_search, tree, tree_search, exact), use.names = FALSE)
         fixed <- c(fixed, fixed_single)
     }
     return(unique(fixed))
@@ -65,16 +65,21 @@ subset_types2 <- function (regex, types, types_search, tree, tree_search, exact)
                 if(exact){
                     subset_types_exact(regex, tree, tree_search)
                 }else{
+                    #cat('Search', regex, '\n')
                     head <- (stri_sub(regex, 1, 1) == '^')
                     tail <- (stri_sub(regex, -1, -1) == '$')
-                    if(head & tail & !is_regex((body <- stri_sub(regex, 2, -2)))){
+                    if(head & stri_length(regex) == 1){
+                        #cat('Any\n')
+                        types # return all types when glob is *
+                    }else if(head & tail & !is_regex((body <- stri_sub(regex, 2, -2)))){
+                        #cat('Exact', body, '\n')
                         subset_types_exact(body, tree, tree_search)
                     }else if(head & !is_regex((headless <- stri_sub(regex, 2, -1)))){
+                        #cat('Starts with', headless, '\n')
                         subset_types_startswith(headless, tree, tree_search)
                     }else if(head & !is_regex((tailless <- stri_sub(regex, 1, -2)))){
+                        #cat('End with', tailless, '\n')
                         subset_types_endswith(tailless, tree, tree_search)
-                    }else if(head & stri_length(regex) == 1){
-                        types # return all types when glob is *
                     }else if(regex == ''){
                         NULL # retun nothing for empty pattern
                     }else{
@@ -86,18 +91,19 @@ subset_types2 <- function (regex, types, types_search, tree, tree_search, exact)
 }
 
 subset_types_exact <- function(str, tree, tree_search){
-    i <- stri_sub(str, 1, 1)
-    return(tree$head[[i]][!is.na(fmatch(tree_search$head[[i]], str))])
+    i <- toLower(stri_sub(str, 1, 1))
+    print(i)
+    tree$head[[i]][!is.na(fmatch(tree_search$head[[i]], str))]
 }
 
 subset_types_startswith <- function(str, tree, tree_search){
-    i <- stri_sub(str, 1, 1)
-    return(tree$head[[i]][stri_startswith_fixed(tree_search$head[[i]], str)])
+    i <- toLower(stri_sub(str, 1, 1))
+    tree$head[[i]][stri_startswith_fixed(tree_search$head[[i]], str)]
 }
 
 subset_types_endswith <- function(str, tree, tree_search){
-    i <- stri_sub(str, -1, -1)
-    return(tree$tail[[i]][stri_startswith_fixed(tree_search$head[[i]], str)])
+    i <- toLower(stri_sub(str, -1, -1))
+    tree$tail[[i]][stri_startswith_fixed(tree_search$head[[i]], str)]
 }
 
 # This function checks if a string is regular expression
