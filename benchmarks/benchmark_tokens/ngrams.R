9eaa1e2ffefa82d06ad95fab12c36cc5d0725cb7
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-12-01 10:57:56 +0000

    Update benchmark

--- benchmarks/benchmark_tokens/ngrams.R
+++ benchmarks/benchmark_tokens/ngrams.R
@@ -8,31 +8,27 @@ tok <- tokens(txt, removeSymbols = TRUE, removeNumbers = TRUE)
 
 microbenchmark::microbenchmark(
     tokenizers = tokenize_ngrams(txt, n = 2),
-    quanteda_unhashed = tokenize(txt, what='fastestword') %>% quanteda::ngrams(n = 2),
-    quanteda_hashed = tokens(txt, what='fastestword') %>% quanteda::ngrams(n = 2),
-    quanteda_hashed_mt = tokens(txt, what='fastestword') %>% quanteda::ngrams(n = 2, thread=4),
+    quanteda_t1 = tokens(txt, what='fastestword') %>% quanteda::ngrams(n = 2, thread=1),
+    quanteda_t4 = tokens(txt, what='fastestword') %>% quanteda::ngrams(n = 2, thread=4),
     unit='relative', times = 1
 )
-
 
 microbenchmark::microbenchmark(
     tokenizers = tokenize_skip_ngrams(txt, n = 2, k = 1),
-    quanteda_unhashed = tokenize(txt, what='fastestword') %>% quanteda::ngrams(n = 2, skip = 1),
-    quanteda_hashed = tokens(txt, what='fastestword') %>% quanteda::ngrams(n = 2, skip = 1),
-    quanteda_hashed_mt = tokens(txt, what='fastestword') %>% quanteda::ngrams(n = 2, skip = 1, thread=4),
+    quanteda_t1 = tokens(txt, what='fastestword') %>% quanteda::ngrams(n = 2, skip = 1, thread=1),
+    quanteda_t4 = tokens(txt, what='fastestword') %>% quanteda::ngrams(n = 2, skip = 1, thread=4),
     unit='relative', times = 1
 )
-
-
 
 microbenchmark::microbenchmark(
     ngrams(tok, n=2, thread=1),
     ngrams(tok, n=2, thread=2),
     ngrams(tok, n=2, thread=3),
     ngrams(tok, n=2, thread=4),
+    ngrams(tok, n=2, thread=8),
     times=1
 )
 
 
-profvis::profvis(ngrams(tok, 2))
+profvis::profvis(ngrams(tok, 2, thread=1))
 profvis::profvis(quanteda::ngrams(tokens(txt, what='fastestword'), n = 2, thread=4))
