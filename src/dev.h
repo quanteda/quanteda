a5a20760d03a1cc8c681c0eac93e274f6dd7fb2b
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-12-01 10:05:18 +0000

    Add timter for development

--- src/dev.h
+++ src/dev.h
@@ -1,10 +1,15 @@
+#include <chrono>
+#include <string>
+#include <map>
+
 #ifndef __DEV__
 #define __DEV__
 
 using namespace Rcpp;
-using namespace std;
 
 namespace dev{
+
+    std::map<std::string, std::chrono::time_point<std::chrono::high_resolution_clock> > timer;
 
     inline void print_ngram(std::vector<int> vec){
         for(auto elm : vec){
@@ -18,6 +23,24 @@ namespace dev{
           Rcout << elm << " ";
         }
         Rcout << "\n";
+    }
+    
+    inline void start_timer(std::string label){
+        auto now = std::chrono::high_resolution_clock::now();
+        timer[label] = now;
+    }
+    
+    inline void stop_timer(std::string label){
+        if (timer.find(label) == timer.end()){
+            Rcout << std::left << std::setw(20) << "'" + label + "'";
+            Rcout << " is not started\n";
+        }else{
+            Rcout << std::left << std::setw(20) <<  "'" + label + "'";
+            Rcout << " ";
+            auto now = std::chrono::high_resolution_clock::now();
+            Rcout << std::chrono::duration<double, std::milli>(now - timer[label]).count();
+            Rcout << " millsec\n";
+        }
     }
 
 }
