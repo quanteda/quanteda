0250dd3018b1e43ef5e3dfa351fb013adbf26715
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-12-01 12:34:51 +0000

    Update test

--- tests/testthat/test_ngrams.R
+++ tests/testthat/test_ngrams.R
@@ -3,18 +3,6 @@ library(testthat)
 
 context('test ngrams.R')
 
-test_that("test that ngrams warns that skip!=0 with n=1 does not skip", {
-      toks <- c('The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog')
-      expect_that(
-          unskipped_ngrams <- ngrams(toks, n=1, skip=42),
-          gives_warning('skip argument ignored for n = 1')
-      )
-      expect_identical(
-          unskipped_ngrams,
-          ngrams(toks, n=1, skip=0)
-      )
-})
-
 test_that("test that ngrams produces the results from Guthrie 2006", {
       toks <- tokens(c('insurgents killed in ongoing fighting'))
 
