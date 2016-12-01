5752c061b67db49d9d15e29273b50081d5b32aba
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-12-01 10:44:51 +0000

    Fix timer

--- src/dev.h
+++ src/dev.h
@@ -8,32 +8,21 @@
 using namespace Rcpp;
 
 namespace dev{
-
-    std::map<std::string, std::chrono::time_point<std::chrono::high_resolution_clock> > timer;
-
-    inline void print_ngram(std::vector<int> vec){
-        for(auto elm : vec){
-          Rcout << std::to_string(elm) << " ";
-        }
-          Rcout << "\n";
-    }
     
-    inline void print_ngram(std::vector<std::string> vec){
-        for(auto elm : vec){
-          Rcout << elm << " ";
-        }
-        Rcout << "\n";
-    }
     
-    inline void start_timer(std::string label){
+    /* ---- Profiling tools ------------------------- */
+    
+    typedef std::map<std::string, std::chrono::time_point<std::chrono::high_resolution_clock> > Timer;
+    
+    inline void start_timer(std::string label, Timer &timer){
         auto now = std::chrono::high_resolution_clock::now();
         timer[label] = now;
     }
     
-    inline void stop_timer(std::string label){
+    inline void stop_timer(std::string label, Timer &timer){
         if (timer.find(label) == timer.end()){
             Rcout << std::left << std::setw(20) << "'" + label + "'";
-            Rcout << " is not started\n";
+            Rcout << " is not timed\n";
         }else{
             Rcout << std::left << std::setw(20) <<  "'" + label + "'";
             Rcout << " ";
@@ -42,7 +31,22 @@ namespace dev{
             Rcout << " millsec\n";
         }
     }
-
+    
+    /* ---- Debugging tools ------------------------- */
+    
+    inline void print_ngram(std::vector<int> vec){
+        for(auto elm : vec){
+            Rcout << std::to_string(elm) << " ";
+        }
+        Rcout << "\n";
+    }
+    
+    inline void print_ngram(std::vector<std::string> vec){
+        for(auto elm : vec){
+            Rcout << elm << " ";
+        }
+        Rcout << "\n";
+    }
 }
 
 #endif
