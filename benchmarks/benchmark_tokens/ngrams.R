fb6618b43755856feca2bb26b492c5d48381fc51
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-12-03 08:45:49 +0000

    Update benchmark

--- benchmarks/benchmark_tokens/ngrams.R
+++ benchmarks/benchmark_tokens/ngrams.R
@@ -1,25 +1,24 @@
 library(profvis) # for profiling
 library(tokenizers)
-library(testthat)
-library(magrittr)
 
 txt <- readLines('~/Documents/Brexit/Analysis/all_bbc_2015.txt') # 80MB
 
 microbenchmark::microbenchmark(
-    tokenizers = tokenize_ngrams(txt, n = 2),
-    quanteda_t1 = as.tokens(tokenize_words(txt)) %>% quanteda::ngrams(n = 2, thread=1),
-    quanteda_t4 = as.tokens(tokenize_words(txt)) %>% quanteda::ngrams(n = 2, thread=4),
+    tokenizers = as.tokens(tokenize_ngrams(txt, n = 2)),
+    quanteda_t1 = quanteda::ngrams(as.tokens(tokenize_words(txt)), n = 2, thread=1),
+    quanteda_t4 = quanteda::ngrams(as.tokens(tokenize_words(txt)), n = 2, thread=4),
     unit='relative', times = 1
 )
 
 microbenchmark::microbenchmark(
-    tokenizers = tokenize_skip_ngrams(txt, n = 2, k = 1),
-    quanteda_t1 = as.tokens(tokenize_words(txt)) %>% quanteda::ngrams(n = 2, skip = 1, thread=1),
-    quanteda_t4 = as.tokens(tokenize_words(txt)) %>% quanteda::ngrams(n = 2, skip = 1, thread=4),
+    tokenizers = as.tokens(tokenize_skip_ngrams(txt, n = 2, k = 1)),
+    quanteda_t1 = quanteda::ngrams(as.tokens(tokenize_words(txt)), n = 2, skip = 1, thread=1),
+    quanteda_t4 = quanteda::ngrams(as.tokens(tokenize_words(txt)), n = 2, skip = 1, thread=4),
     unit='relative', times = 1
 )
 
 tok <- tokens(txt, removeSymbols = TRUE, removeNumbers = TRUE)
+
 
 microbenchmark::microbenchmark(
     ngrams(tok, n=2, thread=1),
@@ -40,6 +39,7 @@ sum(lengths(tok))
 length(attr(tok, 'types'))
 mx <- dfm(tok)
 
+#ngm <- as.tokens(tokenize_ngrams(txt, n = 2))
 ngm <- ngrams(tok, 2, thread=4)
 sum(lengths(ngm))
 length(attr(ngm, 'types'))
