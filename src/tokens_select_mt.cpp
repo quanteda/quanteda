9ac127e9e146e4b6e11d26d3c205bb49dc2b6eaa
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-12-16 20:37:43 +0000

    Integrate keep and remove

--- src/tokens_select_mt.cpp
+++ src/tokens_select_mt.cpp
@@ -12,26 +12,46 @@ using namespace quanteda;
 using namespace ngrams;
 
 
-Text select(Text tokens, 
+Text keep(Text tokens, 
             int span_max,
             SetNgrams &set_words,
             bool padding){
     
     Text tokens_copy(tokens.size(), 0);
-    bool match = false;
     for(int span = span_max; span >= 1; span--){ // substitution starts from the longest sequences
-        //for(int span = 1; span <= span_max; span++){ // substitution starts from the shortest sequences
         for(int i = 0; i < tokens.size() - (span - 1); i++){
             Ngram ngram(tokens.begin() + i, tokens.begin() + i + span);
             bool is_in = set_words.find(ngram) != set_words.end();
             if(is_in){
-                match = true;
                 std::copy(ngram.begin(), ngram.end(), tokens_copy.begin() + i);
             }
         }
     }
     if(!padding) tokens_copy.erase(std::remove(tokens_copy.begin(), tokens_copy.end(), 0), tokens_copy.end());
     return tokens_copy;
+}
+
+Text remove(Text tokens, 
+            int span_max,
+            SetNgrams &set_words,
+            bool padding){
+    
+    Text tokens_copy(tokens.size(), 0);
+    unsigned int filler = std::numeric_limits<unsigned int>::max(); // use upper limit as a filler
+    bool match = false;
+    for(int span = span_max; span >= 1; span--){ // substitution starts from the longest sequences
+        for(int i = 0; i < tokens.size() - (span - 1); i++){
+            Ngram ngram(tokens.begin() + i, tokens.begin() + i + span);
+            bool is_in = set_words.find(ngram) != set_words.end();
+            if(is_in){
+                match = true;
+                std::fill(tokens.begin() + i, tokens.begin() + i + span, filler); // fill subsequent containers
+                if(padding) tokens[i] = 0;
+            }
+        }
+    }
+    if(match) tokens.erase(std::remove(tokens.begin(), tokens.end(), filler), tokens.end());
+    return tokens;
 }
 
 struct select_mt : public Worker{
@@ -41,17 +61,23 @@ struct select_mt : public Worker{
     int span_max;
     SetNgrams &set_words;
     bool padding;
-    
+    int mode;
     
     // Constructor
-    select_mt(Texts &input_, Texts &output_, int span_max_, SetNgrams &set_words_, bool padding_):
-              input(input_), output(output_), span_max(span_max_), set_words(set_words_), padding(padding_) {}
+    select_mt(Texts &input_, Texts &output_, int span_max_, SetNgrams &set_words_, int mode_, bool padding_):
+              input(input_), output(output_), span_max(span_max_), set_words(set_words_), mode(mode_), padding(padding_) {}
     
     // parallelFor calles this function with size_t
     void operator()(std::size_t begin, std::size_t end){
         //Rcout << "Range " << begin << " " << end << "\n";
         for (int h = begin; h < end; h++){
-            output[h] = select(input[h], span_max, set_words, padding);
+            if(mode == 1){
+                output[h] = keep(input[h], span_max, set_words, padding);
+            }else if(mode == 2){
+                output[h] = remove(input[h], span_max, set_words, padding);
+            }else{
+                output[h] = input[h];
+            }
         }
     }
 };
@@ -59,8 +85,9 @@ struct select_mt : public Worker{
 
 // [[Rcpp::export]]
 List qatd_cpp_tokens_select(List texts_, 
-                             List words,
-                             bool padding){
+                            List words,
+                            int mode,
+                            bool padding){
     
     Texts input = Rcpp::as<Texts>(texts_);
 
@@ -75,7 +102,7 @@ List qatd_cpp_tokens_select(List texts_,
     //Rcout << "Span max " << span_max << "\n";
     
     Texts output(input.size());
-    select_mt select_mt(input, output, span_max, set_words, padding);
+    select_mt select_mt(input, output, span_max, set_words, mode, padding);
     
     dev::Timer timer;
     dev::start_timer("Token select", timer);
@@ -91,8 +118,8 @@ List qatd_cpp_tokens_select(List texts_,
 
 toks <- list(rep(1:10, 10), rep(5:15, 10))
 dict <- list(c(1, 2), c(5, 6), 10, 15, 20)
-qatd_cpp_tokens_select(toks, dict, TRUE)
-qatd_cpp_tokens_select(toks, dict, FALSE)
+qatd_cpp_tokens_select(toks, dict, TRUE, 1)
+qatd_cpp_tokens_select(toks, dict, FALSE, 1)
 
 
 
