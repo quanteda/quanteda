6eb441b5b72bfd035c819df6420fe886214390d4
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-12-17 12:01:44 +0000

    Add comparison with dfm_select

--- tests/benchmarks/benchmark_tokens/select.R
+++ tests/benchmarks/benchmark_tokens/select.R
@@ -3,8 +3,8 @@ library(quanteda)
 load("/home/kohei/Documents/Brexit/Analysis/data_corpus_guardian.RData")
 docvars(data_corpus_guardian, 'year') <- format(docvars(data_corpus_guardian, 'date'), '%Y')
 
-#tok <- tokens(data_corpus_guardian)
-tok <- tokens(corpus_subset(data_corpus_guardian, year == 2012))
+tok <- tokens(data_corpus_guardian)
+#tok <- tokens(corpus_subset(data_corpus_guardian, year == 2012))
 tok[[1]]
 
 tok2 <- tokens_select(tok, stopwords())
@@ -20,3 +20,8 @@ system.time(tokens_select(tok, '*'))
 system.time(tokens_select(tok, list(c('President', '*'))))
 system.time(tokens_remove(tok, list(c('President', '*'))))
 
+microbenchmark::microbenchmark(
+    dfm(tokens_remove(tok, stopwords(), valuetype='fixed')),
+    dfm_remove(dfm(tok), stopwords(), valuetype='fixed'),
+    times=1
+)
