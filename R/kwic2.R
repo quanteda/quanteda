cf4088e2533d895c0e2e09bc01e916abf9734710
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-01-02 15:49:08 +0000

    First attempt to reimplement kwic for tokens objects

---
+++ R/kwic2.R
@@ -0,0 +1,139 @@
+#' locate keywords-in-context
+#' 
+#' For a text or a collection of texts (in a quanteda corpus object), return a 
+#' list of a keyword supplied by the user in its immediate context, identifying 
+#' the source text and the word index number within the source text.  (Not the 
+#' line number, since the text may or may not be segmented using end-of-line 
+#' delimiters.)
+#' 
+#' @param x a character, \link{corpus}, or \link{tokens} object
+#' @param keywords a keyword pattern or phrase consisting of multiple keyword 
+#'   patterns, possibly including punctuation.  If a phrase, \code{keywords} 
+#'   will be tokenized using the \code{...} options.
+#' @param window the number of context words to be displayed around the keyword.
+#' @inheritParams valuetype
+#' @param case_insensitive match without respect to case if \code{TRUE}
+#' @param ... additional arguments passed to \link{tokens}, for applicable 
+#'   object types
+#' @return A kwic object classed data.frame, with the document name 
+#'   (\code{docname}), the token index position (\code{position}), the context
+#'   before (\code{contextPre}), the keyword in its original format
+#'   (\code{keyword}, preserving case and attached punctuation), and the context
+#'   after (\code{contextPost}).
+#' @author Kenneth Benoit
+#' @export
+#' @examples
+#' head(kwic(data_char_inaugural, "secure*", window = 3, valuetype = "glob"))
+#' head(kwic(data_char_inaugural, "secur", window = 3, valuetype = "regex"))
+#' head(kwic(data_char_inaugural, "security", window = 3, valuetype = "fixed"))
+#' 
+#' kwic(data_corpus_inaugural, "war against")
+#' kwic(data_corpus_inaugural, "war against", valuetype = "regex")
+#' 
+kwic2 <- function(x, keywords, window = 5, valuetype = c("glob", "regex", "fixed"), case_insensitive = TRUE, ...) {
+    UseMethod("kwic2")
+}
+
+#' @rdname kwic
+#' @noRd
+#' @export
+kwic2.character <- function(x, keywords, window = 5, valuetype = c("glob", "regex", "fixed"), case_insensitive = TRUE, ...) {
+    kwic2(tokens(x, ...), keywords, window, valuetype, case_insensitive)
+}
+
+#' @rdname kwic
+#' @noRd
+#' @export 
+kwic2.corpus <- function(x, keywords, window = 5, valuetype = c("glob", "regex", "fixed"), case_insensitive = TRUE, ...) {
+    kwic2(texts(x), keywords, window, valuetype, case_insensitive, ...)
+}
+
+#' @rdname kwic
+#' @noRd
+#' @examples 
+#' mycorpus <- corpus(c("This is a test",
+#' "This is it.",
+#' "What is in a train?",
+#' "Is it a question?",
+#' "Sometimes you don't know if this is it.",
+#' "Is it a bird or a plane or is it a train?"))
+#' 
+#' kwic2(mycorpus, c("is", "a"), valuetype = "fixed")
+#' kwic2(mycorpus, list("is", "a", c("is", "it")), valuetype = "fixed")
+#' @export 
+kwic2.tokens <- function(x, keywords, window = 5, valuetype = c("glob", "regex", "fixed"), case_insensitive = TRUE, ...) {
+    
+    if (!is.tokens(x)) stop("x must be a tokens class object")
+    
+    valuetype <- match.arg(valuetype)
+    
+    names_org <- names(x)
+    attrs_org <- attributes(x)
+    
+    # Generate all combinations of type IDs
+    entries_id <- list()
+    keys_id <- c()
+    types <- types(x)
+    index <- index_regex(types, valuetype, case_insensitive) # index types before the loop
+    #if (verbose) 
+    #    message('Registering ', length(unlist(keywords)), ' entiries in the keywords...');
+    for (h in 1:length(keywords)) {
+        entries <- keywords[h]
+        entries_fixed <- regex2fixed5(entries, types, valuetype, case_insensitive, index) # convert glob or regex to fixed
+        if (length(entries_fixed) == 0) next
+        entries_id <- c(entries_id, lapply(entries_fixed, function(x) fmatch(x, types)))
+    }
+    #if (verbose) 
+    #    message('Searching ', length(entries_id), ' types of features...')
+    mask <- qatd_cpp_tokens_detect(x, entries_id)
+    match <- which(lapply(mask, sum) > 0)
+    for(i in 1:length(match)){
+        df_temp <- kwic_split(x[[i]], mask[[i]], window)
+        rownames(df_temp) <- stri_c(names_org[i], rownames(df_temp), sep=':')
+        if(i == 1){
+            df <- df_temp
+        }else{
+            df <- rbind(df, df_temp, stringsAsFactors=FALSE)
+        }
+    }
+    return(df)
+}
+
+#kwic_split(letters[1:5], c(0, 1, 1, 0, 1), window=1)
+#kwic_split(letters[1:5], c(1, 1, 1, 0, 1), window=1)
+
+kwic_split <- function(char, mask, window){
+    
+    # Expand vector for matches at the top or end
+    char <- c('', char, '')
+    mask <- c(0, mask, 0)
+    len <- length(char)
+    start <- which(diff(c(0, mask))==1)
+    end <- which(diff(c(mask, 0))==-1)
+    
+    pre <- target <- post <- c()
+    for(i in 1:length(start)){
+        pre <- c(pre, stri_c(char[max(0, start[i] - window):max(0, start[i] - 1)], collapse = ' '))
+        target <- c(target, stri_c(char[start[i]:end[i]], collapse = ' '))
+        post <- c(post, stri_c(char[min(len + 1, end[i] + 1):min(len, end[i] + window)], collapse = ' '))
+    }
+    #pre <- char[pmax(0, start - window):pmax(0, start - 1)]
+    #target <- char[start:end]
+    #post <- char[pmin(len, end + 1):pmin(len, end + window)]
+    return(data.frame(before = format(pre, justify="right"),
+                      keyword = stri_c('[', target, ']', sep = ''), 
+                      after = format(post, justify="left"), 
+                      stringsAsFactors = FALSE))
+    
+
+    
+}
+
+#' @rdname kwic
+#' @noRd
+#' @export 
+kwic2.tokenizedTexts <- function(x, keywords, window = 5, valuetype = c("glob", "regex", "fixed"), case_insensitive = TRUE, ...) {
+    kwic2(as.tokens(x), keywords, window, valuetype, case_insensitive, ...)
+}
+
+
---
+++ src/tokens_detect_mt.cpp
@@ -0,0 +1,104 @@
+#include <Rcpp.h>
+#include "dev.h"
+#include "quanteda.h"
+
+// [[Rcpp::depends(RcppParallel)]]
+#include <RcppParallel.h>
+
+// [[Rcpp::plugins(cpp11)]]
+using namespace Rcpp;
+using namespace RcppParallel;
+using namespace quanteda;
+using namespace ngrams;
+
+
+Text detect(Text tokens, 
+            int span_max,
+            SetNgrams &set_words){
+    
+    if(tokens.size() == 0) return {}; // return empty vector for empty text
+    
+    Text tokens_pos(tokens.size(), 0);
+    for(int span = span_max; span >= 1; span--){ // substitution starts from the longest sequences
+        for(int i = 0; i < tokens.size() - (span - 1); i++){
+            Ngram ngram(tokens.begin() + i, tokens.begin() + i + span);
+            bool is_in = set_words.find(ngram) != set_words.end();
+            if(is_in){
+                std::fill(tokens_pos.begin() + i, tokens_pos.begin() + i + span, 1);
+            }
+        }
+    }
+    return tokens_pos;
+}
+
+
+struct detect_mt : public Worker{
+    
+    Texts &input;
+    Texts &output;
+    int span_max;
+    SetNgrams &set_words;
+    
+    // Constructor
+    detect_mt(Texts &input_, Texts &output_, int span_max_, SetNgrams &set_words_):
+              input(input_), output(output_), span_max(span_max_), set_words(set_words_){}
+    
+    // parallelFor calles this function with size_t
+    void operator()(std::size_t begin, std::size_t end){
+        //Rcout << "Range " << begin << " " << end << "\n";
+        for (int h = begin; h < end; h++){
+            output[h] = detect(input[h], span_max, set_words);
+        }
+    }
+};
+
+/* 
+ * This funciton finds features in tokens object. This is similar to tokens_tookup, 
+ * but returns a tokens object filled with zero or one.
+ * The number of threads is set by RcppParallel::setThreadOptions()
+ * @used tokens_detect()
+ * @creator Kohei Watanabe
+ * @param texts_ tokens ojbect
+ * @param words_ list of features to find
+ * 
+ */
+
+
+// [[Rcpp::export]]
+List qatd_cpp_tokens_detect(List texts_, 
+                            List words_){
+    
+    Texts input = Rcpp::as<Texts>(texts_);
+    List words = words_;
+
+    SetNgrams set_words;
+    int span_max = 0;
+    for (int g = 0; g < words.size(); g++) {
+        if (has_na(words[g])) continue;
+        Ngram word = words[g];
+        set_words.insert(word);
+        if (span_max < word.size()) span_max = word.size();
+    }
+    
+    Texts output(input.size());
+    detect_mt detect_mt(input, output, span_max, set_words);
+    
+    // dev::Timer timer;
+    // dev::start_timer("Dictionary detect", timer);
+    parallelFor(0, input.size(), detect_mt);
+    // dev::stop_timer("Dictionary detect", timer);
+    
+    //ListOf<IntegerVector> texts_key = Rcpp::wrap(output);
+    return as_list(output);
+}
+
+/***R
+
+toks <- list(rep(1:10, 1), rep(5:15, 1))
+dict <- list(c(1, 2), c(5, 6), 10, 15, 20)
+
+qatd_cpp_tokens_detect(toks, dict)
+
+lapply(toks)
+
+*/
