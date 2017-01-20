4890eb1ab3c62a321f52b767eef0b759012afde6
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-01-20 08:46:19 +0000

    Update man and test

--- tests/testthat/test-textstat_keyness.R
+++ tests/testthat/test-textstat_keyness.R
@@ -2,10 +2,10 @@
 # correlation
 test_that("test textstat_simil method = \"correlation\" against proxy simil(): documents", {
     period <- as.factor(ifelse(docvars(data_corpus_inaugural)$Year < 1945, 'post-war', 'pre-war'))
-    mydfm <- dfm(data_corpus_inaugural, groups = period)
-    mydfm <- dfm_remove(mydfm, stopwords('english'))
-    mydfm <- dfm_select(mydfm, min_count = 2, min_nchar = 2)
-    mydfm <- dfm_smooth(mydfm, smoothing = 1)
-    expect_equal(head(names(textstat_keyness(mydfm)), 5),
+    testdfm <- dfm(data_corpus_inaugural, groups = period)
+    testdfm <- dfm_remove(testdfm, stopwords('english'))
+    testdfm <- dfm_select(testdfm, min_count = 2, min_nchar = 2)
+    testdfm <- dfm_smooth(testdfm, smoothing = 1)
+    expect_equal(head(names(textstat_keyness(testdfm)), 5),
                  c('states', 'constitution', 'government', 'upon', 'public'))
 })
