a8104c5d18414bb64c36e768f4005f00c4bb562d
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-02-07 12:41:45 +0000

    Add overflow detection

--- src/quanteda.h
+++ src/quanteda.h
@@ -80,16 +80,14 @@ namespace ngrams {
     
     struct hash_ngram {
             std::size_t operator() (const Ngram &vec) const {
+            unsigned int add = 0;
             unsigned int seed = 0;
             for (std::size_t i = 0; i < vec.size(); i++) {
-                if(i < 6) {
-                    #if __WORDSIZE == 64 // change size of bit shift in 64 bit systems
-                    seed += (vec[i] << (8 * i));
-                    #else
-                    seed += (vec[i] << (4 * i));
-                    #endif
+                add = vec[i] << (8 * i);
+                if (seed < UINT_MAX - add) { // check if addition will overflow seed
+                    seed += add;
                 } else {
-                    seed += vec[i];
+                    return std::hash<unsigned int>()(seed);
                 }
             }
             return std::hash<unsigned int>()(seed);
