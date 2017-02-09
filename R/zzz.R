3bcc4a624edabf03876017a18132d28d7e8aef3b
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-02-09 12:45:06 +0000

    Modify cores message on load

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(...) {
     packageStartupMessage("quanteda version ", as.character(utils::packageVersion("quanteda")))
     threads <- RcppParallel::defaultNumThreads()
-    packageStartupMessage("Use ", threads - 1, " of ", threads, " cores in parallel computing")
+    packageStartupMessage("Using ", threads - 1, " of ", threads, " cores for parallel computing")
     RcppParallel::setThreadOptions(threads - 1)
 }
