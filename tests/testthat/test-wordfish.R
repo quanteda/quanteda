13b8bdec574959d7caf1f3db79ce0f14a63e5234
HaiyanLW <wangh1651@gmail.com>
2017-02-17 20:58:56 +0000

    Disable the call to library 'austin' Compare the test result of the two wordfish versions.

--- tests/testthat/test-wordfish.R
+++ tests/testthat/test-wordfish.R
@@ -3,7 +3,7 @@ require(testthat)
 context('Testing textmodel-wordfish.R')
 
 # load the austin library
-library(austin)
+# library(austin)
 test_that("textmodel-wordfish works as expected as austin::wordfish", {
     skip_if_not_installed("austin")
     ie2010dfm <- dfm(data_corpus_irishbudget2010, verbose = FALSE)
@@ -13,11 +13,10 @@ test_that("textmodel-wordfish works as expected as austin::wordfish", {
     expect_gt(cc, 0.99)
 })
      
-test_that("textmodel-wordfish works as expected as austin::wordfish", {
-    skip_if_not_installed("austin")
+test_that("textmodel-wordfish Parallel version works as expected as serial version", {
     ie2010dfm <- dfm(data_corpus_irishbudget2010, verbose = FALSE)
-    wfm <- textmodel_wordfish(ie2010dfm, dir = c(6,5))
-    wfmodelAustin <- austin::wordfish(quanteda::as.wfm(ie2010dfm), dir = c(6,5))
-    cc<-cor(wfm@theta, wfmodelAustin$theta)
+    wfm_s <- textmodel_wordfish(ie2010dfm, dir = c(6,5))
+    wfm_p <- textmodel_wordfish(ie2010dfm, dir = c(6,5), version = "Parallel")
+    cc<-cor(wfm_s@theta, wfm_p@theta)
     expect_gt(cc, 0.99)
 })
