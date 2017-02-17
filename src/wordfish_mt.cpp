b702d33dc1b0a462abd49c5e69debbc0b3836fd7
HaiyanLW <wangh1651@gmail.com>
2017-02-17 12:05:02 +0000

    Parallelize computing log-posterior

--- src/wordfish_mt.cpp
+++ src/wordfish_mt.cpp
@@ -1,7 +1,9 @@
 
 // includes from the plugin
 #include <RcppArmadillo.h>
+#include <RcppParallel.h>
 
+using namespace RcppParallel;
 using namespace Rcpp;
 using namespace arma;
 # define RESIDUALS_LIM 0.5
@@ -14,6 +16,151 @@ using namespace arma;
 #define ARMA_64BIT_WORD
 #endif
 
+
+//Compute LOG-POSTERIOR 
+struct LogPos : public Worker {
+    const arma::colvec& alpha; 
+    const arma::rowvec& psi;
+    const arma::rowvec& beta;
+    const arma::colvec& theta;
+    const arma::sp_mat& wfm;
+    const std::size_t K;
+    
+    // accumulated value
+    double lp;
+    
+    // constructors
+    LogPos(const arma::colvec& alpha, const arma::rowvec& psi, const arma::rowvec& beta, 
+           const arma::colvec& theta, const arma::sp_mat& wfm, const std::size_t K) 
+        : alpha(alpha), psi(psi), beta(beta), theta(theta), wfm(wfm), K(K), lp(0) {}
+    LogPos(const LogPos& sum, Split) : alpha(sum.alpha), psi(sum.psi), beta(sum.beta),
+    theta(sum.theta), wfm(sum.wfm), K(sum.K), lp(0) {}
+    
+    
+    void operator() (std::size_t begin, std::size_t end) {
+        for (std::size_t i = begin; i < end; i++) {
+            float temp = lp;
+            for (std::size_t k = 0; k < K; k++){
+                double loglambdaik = alpha(i) + psi(k) + beta(k) * theta(i);
+                temp += loglambdaik * wfm(i,k) - exp(loglambdaik);
+            }
+            lp = temp;
+        }
+    }
+    
+    // join lp with that of another LogPos
+    void join(const LogPos& rhs) { 
+        lp += rhs.lp; 
+    }
+};
+
+// UPDATE WORD PARAMETERS
+struct WordPar : public Worker {
+    const arma::colvec& alpha; 
+    const arma::colvec& theta; 
+    RVector<double> psi; 
+    RVector<double> beta; 
+    const arma::rowvec& phi;
+    const arma::sp_mat& wfm;
+    const NumericVector& tolvec;
+    const int outeriter;
+    const double priorprecpsi;
+    const double priorprecbeta;
+    double stepsize;
+    
+    WordPar(const arma::colvec& alpha,  NumericVector& psi,  NumericVector& beta, 
+            const arma::colvec& theta, const arma::rowvec& phi, const arma::sp_mat& wfm,
+            const NumericVector& tolvec, const int outeriter, 
+            const double priorprecpsi, const double priorprecbeta, double stepsize) 
+        : alpha(alpha), psi(psi), beta(beta), theta(theta), phi(phi), wfm(wfm), tolvec(tolvec), 
+          outeriter(outeriter), priorprecpsi(priorprecpsi), priorprecbeta(priorprecbeta), stepsize(stepsize) {}
+    
+    void operator() (std::size_t begin, std::size_t end) {
+        for (std::size_t k = begin; k < end; k++) {
+            double cc = 1;
+            int inneriter = 0;
+            if (outeriter == 1) stepsize = 0.5;
+            arma::colvec col_k(wfm.col(k));
+            arma::colvec lambdak(alpha.size());
+            arma::mat pars(2,1);
+            arma::mat newpars(2,1);
+            arma::mat G(2,1);
+            arma::mat H(2,2);
+            while ((cc > tolvec(1)) && inneriter < INNERITER){
+                inneriter++;
+                lambdak = exp(alpha + psi[k] + beta[k] * theta);
+                G(0,0) = accu(col_k - lambdak) / phi(k) - psi[k] * priorprecpsi;
+                G(1,0) = accu(theta % (col_k - lambdak)) / phi(k) - beta[k] * priorprecbeta;
+                H(0,0) = -accu(lambdak) / phi(k) - priorprecpsi;
+                H(1,0) = -accu(theta % lambdak) / phi(k);
+                H(0,1) = H(1,0);
+                H(1,1) = -accu((theta % theta) % lambdak) / phi(k) - priorprecbeta;
+                pars(0,0) = psi[k];
+                pars(1,0) = beta[k];
+                newpars(0,0) = pars(0,0) - stepsize * (H(1,1) * G(0,0) - H(0,1) * G(1,0)) / (H(0,0) * H(1,1) - H(0,1) * H(1,0));
+                newpars(1,0) = pars(1,0) - stepsize * (H(0,0) * G(1,0) - H(1,0) * G(0,0)) / (H(0,0) * H(1,1) - H(0,1) * H(1,0));
+                psi[k] = newpars(0,0);
+                beta[k] = newpars(1,0);
+                cc = abs(newpars - pars).max();
+                stepsize = 1.0;
+            }
+        }
+    } //End of operator
+};
+
+// UPDATE DOCUMENT PARAMETERS
+struct DocPar : public Worker {
+    RVector<double> alpha; 
+    RVector<double> theta; 
+    const arma::rowvec& psi;
+    const arma::rowvec& beta; 
+    const arma::rowvec& phi;
+    const arma::sp_mat& wfm;
+    const NumericVector& tolvec;
+    const int outeriter;
+    const double priorprecalpha;
+    const double priorprectheta;
+    double stepsize;
+    
+    DocPar(NumericVector& alpha,  const arma::rowvec& psi,  const arma::rowvec& beta, 
+           NumericVector& theta, const arma::rowvec& phi, const arma::sp_mat& wfm,
+            const NumericVector& tolvec, const int outeriter, 
+            const double priorprecalpha, const double priorprectheta, double stepsize) 
+        : alpha(alpha), psi(psi), beta(beta), theta(theta), phi(phi), wfm(wfm), tolvec(tolvec), 
+          outeriter(outeriter), priorprecalpha(priorprecalpha), priorprectheta(priorprectheta), stepsize(stepsize) {}
+    
+    void operator() (std::size_t begin, std::size_t end) {
+        for (std::size_t i = begin; i < end; i++) {
+            double cc = 1;
+            int inneriter = 0;
+            if (outeriter == 1) stepsize = 0.5;
+            arma::rowvec row_i(wfm.row(i));
+            arma::rowvec lambdai(alpha.size());
+            arma::mat pars(2,1);
+            arma::mat newpars(2,1);
+            arma::mat G(2,1);
+            arma::mat H(2,2);
+            while ((cc > tolvec(1)) && inneriter < INNERITER){
+                inneriter++;
+                lambdai = exp(alpha[i] + psi + beta * theta[i]);
+                G(0,0) = accu((row_i - lambdai) / phi) - alpha[i] * priorprecalpha;
+                G(1,0) = accu((beta % (row_i - lambdai)) / phi) - theta[i] * priorprectheta;		
+                H(0,0) = -accu(lambdai / phi) - priorprecalpha;
+                H(1,0) = -accu((beta % lambdai) / phi);
+                H(0,1) = H(1,0);
+                H(1,1) = -accu(((beta % beta) % lambdai) / phi) - priorprectheta;
+                pars(0,0) = alpha[i];
+                pars(1,0) = theta[i];
+                newpars(0,0) = pars(0,0) - stepsize*(H(1,1)*G(0,0) - H(0,1)*G(1,0))/(H(0,0)*H(1,1) - H(0,1)*H(1,0));
+                newpars(1,0) = pars(1,0) - stepsize*(H(0,0)*G(1,0) - H(1,0)*G(0,0))/(H(0,0)*H(1,1) - H(0,1)*H(1,0));
+                alpha[i] = newpars(0,0);
+                theta[i] = newpars(1,0);
+                cc = abs(newpars - pars).max();	
+                stepsize = 1.0;
+            }
+        }
+    } //End of operator
+};
 // [[Rcpp::export]]
 
 Rcpp::List wordfish_cpp(arma::sp_mat &wfm, IntegerVector& dirvec, NumericVector& priorvec, NumericVector& tolvec, IntegerVector& disptype, NumericVector& dispmin){
@@ -24,8 +171,8 @@ Rcpp::List wordfish_cpp(arma::sp_mat &wfm, IntegerVector& dirvec, NumericVector&
     double priorprecbeta = priorvec(2);
     double priorprectheta = priorvec(3);		
     
-    unsigned int N = wfm.n_rows;
-    unsigned int K = wfm.n_cols;
+    std::size_t N = wfm.n_rows;
+    std::size_t K = wfm.n_cols;
     
     // SET INITIAL VALUES
     
@@ -33,17 +180,17 @@ Rcpp::List wordfish_cpp(arma::sp_mat &wfm, IntegerVector& dirvec, NumericVector&
     arma::rowvec psi(K); 
     arma::rowvec beta(K); 
     arma::colvec theta(N); 
-    
     arma::colvec thetaSE(N); // document position standard errors
     arma::rowvec phi(K); // word-level dispersion parameters
     phi.fill(1.0);
+ 
     // Construct Chi-Sq Residuals	
     arma::sp_mat C(wfm); 
     arma::colvec rsum(sum(C,1));
     arma::rowvec csum(sum(C,0));
     double asum = accu(C);		
-    for (unsigned int i=0; i < N; i++){
-        for (unsigned int k=0; k < K; k++){
+    for (std::size_t i=0; i < N; i++){
+        for (std::size_t k=0; k < K; k++){
             double residual = (wfm(i,k) - rsum(i) * csum(k) / asum) / sqrt(rsum(i) * csum(k) / asum);	
             //Rprintf("%d: %f2\\n",k,residual);
             if (fabs(residual) > RESIDUALS_LIM) C(i,k) = residual;
@@ -56,9 +203,10 @@ Rcpp::List wordfish_cpp(arma::sp_mat &wfm, IntegerVector& dirvec, NumericVector&
     arma::vec s(svdk);
     arma::mat V(K, svdk);
     arma::svds(U,s,V,C, svdk);
+    Rcout<<"svd done"<<endl;
     
     // Load initial values
-    for (unsigned int i=0; i < N; i++) theta(i) = pow(rsum(i)/asum, -0.5) * U(i, 0);
+    for (std::size_t i=0; i < N; i++) theta(i) = pow(rsum(i)/asum, -0.5) * U(i, 0);
     //for (int k=0; k < K; k++) beta(k) = 0; // pow(csum(k)/asum,-0.5) * V(k,0);
     beta.fill(0.0);
     alpha = log(rsum);
@@ -72,7 +220,7 @@ Rcpp::List wordfish_cpp(arma::sp_mat &wfm, IntegerVector& dirvec, NumericVector&
     arma::mat newpars(2,1);		
     arma::mat G(2,1);
     arma::mat H(2,2);
-    double loglambdaik;
+    //double loglambdaik;
     double mutmp;		
     double phitmp;
     arma::rowvec lambdai(K);
@@ -81,50 +229,39 @@ Rcpp::List wordfish_cpp(arma::sp_mat &wfm, IntegerVector& dirvec, NumericVector&
     double cc = 0.0;
     int inneriter = 0;
     int outeriter = 0;
-    
+
+    //Initialize LOG-POSTERIOR 
     double lastlp = LASTLP;
     double lp = -1.0*(accu(0.5 * ((alpha % alpha) * priorprecalpha)) + accu(0.5 * ((psi  % psi) * priorprecpsi)) 
                           + accu(0.5 * ((beta  % beta) * priorprecbeta)) + accu(0.5 * ((theta % theta) * priorprectheta)));
-    for (unsigned int i=0; i < N; i++){
-        for (unsigned int k=0; k < K; k++){
-            loglambdaik = alpha(i) + psi(k) + beta(k)*theta(i);
-            lp = lp + loglambdaik * wfm(i,k)-exp(loglambdaik);
-        }
-    }
+    
+    // compute LOG posterior
+    LogPos logPos(alpha, psi, beta, theta, wfm, K);
+    parallelReduce(0, N, logPos);
+    lp = logPos.lp;
+    
     
     // BEGIN WHILE LOOP
     while(((lp - lastlp) > tolvec(0)) && outeriter < OUTERITER){	
         outeriter++;
         
         // UPDATE WORD PARAMETERS
-        for (unsigned int k=0; k < K; k++){
-            cc = 1;
-            inneriter = 0;
-            if (outeriter == 1) stepsize = 0.5;
-            while ((cc > tolvec(1)) && inneriter < INNERITER){
-                inneriter++;
-                lambdak = exp(alpha + psi(k) + beta(k) * theta);
-                arma::vec col_k(wfm.col(k));
-                G(0,0) = accu(col_k - lambdak) / phi(k) - psi(k) * priorprecpsi;
-                G(1,0) = accu(theta % (col_k - lambdak)) / phi(k) - beta(k) * priorprecbeta;
-                H(0,0) = -accu(lambdak) / phi(k) - priorprecpsi;
-                H(1,0) = -accu(theta % lambdak) / phi(k);
-                H(0,1) = H(1,0);
-                H(1,1) = -accu((theta % theta) % lambdak) / phi(k) - priorprecbeta;
-                pars(0,0) = psi(k);
-                pars(1,0) = beta(k);
-                newpars(0,0) = pars(0,0) - stepsize * (H(1,1) * G(0,0) - H(0,1) * G(1,0)) / (H(0,0) * H(1,1) - H(0,1) * H(1,0));
-                newpars(1,0) = pars(1,0) - stepsize * (H(0,0) * G(1,0) - H(1,0) * G(0,0)) / (H(0,0) * H(1,1) - H(0,1) * H(1,0));
-                psi(k) = newpars(0,0);
-                beta(k) = newpars(1,0);
-                cc = abs(newpars - pars).max();
-                stepsize = 1.0;
-            }	
-        }	
+        NumericVector psi_N(psi.begin(),psi.end());
+        NumericVector beta_N(beta.begin(),beta.end());
+        WordPar wordPar(alpha, psi_N, beta_N, theta, phi, wfm, tolvec,
+                        outeriter, priorprecpsi, priorprecbeta, stepsize);
+        parallelFor(0, K, wordPar);
         
-        
+        psi = as<arma::rowvec>(psi_N);
+        beta = as<arma::rowvec>(beta_N);
+        for(unsigned int i=0; i<N; i++){
+            if (std::isnan(alpha(i)) || std::isnan(theta(i))) Rcout<<outeriter<<"alpha or theta is nan"<<i<<std::endl;
+        }
+        for(unsigned int i=0; i<K; i++){
+            if (std::isnan(beta(i)) || std::isnan(psi(i))) Rcout<<outeriter<<"beta or psi is nan"<<i<<std::endl;
+        }
         // UPDATE DOCUMENT PARAMETERS
-        for (unsigned int i=0; i < N; i++){
+        for (std::size_t i=0; i < N; i++){
             cc = 1;
             inneriter = 0;
             if (outeriter == 1) stepsize = 0.5;
@@ -153,20 +290,20 @@ Rcpp::List wordfish_cpp(arma::sp_mat &wfm, IntegerVector& dirvec, NumericVector&
         
         if (disptype(0) == 2) { // single dispersion parameter for all words
             phitmp = 0.0;
-            for (unsigned int k=0; k < K; k++){
-                for (unsigned int i=0; i < N; i++){
+            for (std::size_t k=0; k < K; k++){
+                for (std::size_t i=0; i < N; i++){
                     mutmp = exp(alpha(i) + psi(k) + beta(k)*theta(i));
                     phitmp = phitmp + (wfm(i,k) - mutmp) * (wfm(i,k) - mutmp)/mutmp;
                 }   
             }	    
             phitmp = phitmp/(N*K - 2*N - 2*K);
-            for (unsigned int k=0; k < K; k++) phi(k) = phitmp;
+            for (std::size_t k=0; k < K; k++) phi(k) = phitmp;
         }
         
         if (disptype(0) >= 3) { // individual dispersion parameter for each word
-            for (unsigned int k=0; k < K; k++){
+            for (std::size_t k=0; k < K; k++){
                 phitmp = 0.0;
-                for (unsigned int i=0; i < N; i++){
+                for (std::size_t i=0; i < N; i++){
                     mutmp = exp(alpha(i) + psi(k) + beta(k) * theta(i));
                     phitmp = phitmp + (wfm(i,k) - mutmp) * (wfm(i,k) - mutmp)/mutmp;	        
                 }   
@@ -184,12 +321,16 @@ Rcpp::List wordfish_cpp(arma::sp_mat &wfm, IntegerVector& dirvec, NumericVector&
         lastlp = lp;
         lp = -1.0*(accu(0.5 * ((alpha % alpha) * priorprecalpha)) + accu(0.5 * ((psi % psi) * priorprecpsi)) 
                        + accu(0.5 * ((beta % beta) * priorprecbeta)) + accu(0.5 * ((theta % theta) * priorprectheta)));
-        for (unsigned int i=0; i < N; i++){
-            for (unsigned int k=0; k < K; k++){
-                loglambdaik = alpha(i) + psi(k) + beta(k) * theta(i);
-                lp = lp + loglambdaik * wfm(i,k) - exp(loglambdaik);
-            }
-        }
+        // for (std::size_t i=0; i < N; i++){
+        //     for (std::size_t k=0; k < K; k++){
+        //         loglambdaik = alpha(i) + psi(k) + beta(k) * theta(i);
+        //         lp = lp + loglambdaik * wfm(i,k) - exp(loglambdaik);
+        //     }
+        // }
+        
+        LogPos logPos2(alpha, psi, beta, theta, wfm, K);
+        parallelReduce(0, N, logPos2);
+        lp = logPos2.lp;
         // Rprintf("%d: %f2\\n",outeriter,lp);
         
         // END WHILE LOOP		
@@ -205,7 +346,7 @@ Rcpp::List wordfish_cpp(arma::sp_mat &wfm, IntegerVector& dirvec, NumericVector&
     }
     
     // COMPUTE DOCUMENT STANDARD ERRORS
-    for (unsigned int i=0; i < N; i++) {
+    for (std::size_t i=0; i < N; i++) {
         lambdai = exp(alpha(i) + psi + beta * theta(i));
         H(0,0) = -accu(lambdai / phi) - priorprecalpha;
         H(1,0) = -accu((beta % lambdai) / phi);
