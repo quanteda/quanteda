4bb3864264753fcd72b96052286704ff1d622ea6
HaiyanLW <wangh1651@gmail.com>
2017-02-23 16:54:42 +0000

    Add more wordfish tests.

--- tests/testthat/test-wordfish.R
+++ tests/testthat/test-wordfish.R
@@ -13,10 +13,31 @@ test_that("textmodel-wordfish works as expected as austin::wordfish", {
     expect_gt(cc, 0.99)
 })
      
-test_that("textmodel-wordfish Parallel version works as expected as serial version", {
+test_that("textmodel-wordfish Parallel version works as expected as serial version: poission", {
     ie2010dfm <- dfm(data_corpus_irishbudget2010, verbose = FALSE)
     wfm_s <- textmodel_wordfish(ie2010dfm, dir = c(6,5), version = "serial")
-    wfm_p <- textmodel_wordfish(ie2010dfm, dir = c(6,5), version = "parallel")
+
+    wfm_p <- textmodel_wordfish(ie2010dfm, dir = c(6,5), version = "parallel", residual_floor = 0.5)
     cc <- cor(wfm_s@theta, wfm_p@theta)
     expect_gt(cc, 0.99)
+})
+
+test_that("textmodel-wordfish Parallel version works as expected as serial version: poisson-feature", {
+    ie2010dfm <- dfm(data_corpus_irishbudget2010, verbose = FALSE)
+    wfm_s <- textmodel_wordfish(ie2010dfm, dir = c(6,5), dispersion = "quasipoisson",dispersion_level = "feature")
+    
+    wfm_p <- textmodel_wordfish(ie2010dfm, dir = c(6,5), dispersion = "quasipoisson",dispersion_level = "feature",version = "parallel")
+    cc <- cor(wfm_s@theta, wfm_p@theta)
+    expect_gt(cc, 0.99)
+
+})
+
+test_that("textmodel-wordfish Parallel version works as expected as serial version: quasipoisson-overall", {
+    ie2010dfm <- dfm(data_corpus_irishbudget2010, verbose = FALSE)
+    wfm_s <- textmodel_wordfish(ie2010dfm, dir = c(6,5), dispersion = "quasipoisson",dispersion_level = "overall")
+    
+    wfm_p <- textmodel_wordfish(ie2010dfm, dir = c(6,5), dispersion = "quasipoisson",dispersion_level = "overall",version = "parallel")
+    cc <- cor(wfm_s@theta, wfm_p@theta)
+    expect_gt(cc, 0.99)
+    
 })
