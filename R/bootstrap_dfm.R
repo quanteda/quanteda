1ceadfd3c6ca70d3d6e9da3ade7a74ced4592eae
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-02-24 09:43:10 +0000

    Bootstrap dfm over sentence-resampled list

--- R/bootstrap_dfm.R
+++ R/bootstrap_dfm.R
@@ -4,27 +4,43 @@
 #' @param x the dfm
 #' @param n number of resamples
 #' @param ... additional arguments passed to \link{\code{dfm}}
+#' @param verbose if \code{TRUE} print status messages
 #' @export
-bootstrap_dfm <- function(x, n = 10, ...) {
+bootstrap_dfm <- function(x, n = 10, ..., verbose = TRUE) {
     UseMethod("dfm_bootstrap")
 }
 
 #' @noRd
 #' @export
-bootstrap_dfm.corpus <- function(x, n = 10, ...) {
-    
-    for (i in 1:n) {
-        corp_sentences <- corpus_reshape(x, to = "sentences")
-        corp_sentences <- corpus_resample(corp_senteces, replace = TRUE)
-        tmp <- corpus_reshape(x, to = "documents")
-    }
+bootstrap_dfm.corpus <- function(x, n = 10, ..., verbose = TRUE) {
 
+    if (verbose) 
+        message("Bootstrapping the corpus to create multiple dfm objects...")
+    
+    if (verbose) 
+        message("   ...segmenting the corpus into sentences")
+    corp_sentences <- corpus_reshape(x, to = "sentences")
+    
+    if (verbose)
+        message("   ...resampling and forming dfms: 0", appendLF = FALSE)
+    result <- list(dfm_0 = dfm(x, ...))
+    
+    # could be parallelized
+    for (i in 1:n) {
+        if (verbose) message(", ", i, appendLF = FALSE)
+        result <- c(result, 
+                    as.list(setNames(dfm(corpus_resample(corp_sentences, replace = TRUE), groups = "_document", ...)), 
+                            paste0("dfm_", i)))
+    }
+    if (verbose) message("") # adds LF
+    
+    result
 }
 
 #' @noRd
 #' @export
-bootstrap_dfm.character <- function(x, n = 10, ...) {
-    dfm_bootstrap(corpus(x), ...)
+bootstrap_dfm.character <- function(x, n = 10, ..., verbose = TRUE) {
+    dfm_bootstrap(corpus(x), ..., verbose = verbose)
 }
 
 
