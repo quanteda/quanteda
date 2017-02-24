26e6e84954fdfe113c287e877b0abb6c773059f1
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-02-24 10:21:26 +0000

    Update tests for bootstrap_dfm

---
+++ tests/testthat/test-bootstrap.R
@@ -0,0 +1,18 @@
+context("test bootstrapping functions")
+
+test_that("bootstrap_dfm works as planned", {
+    txt <- c(textone = "This is a sentence.  Another sentence.  Yet another.", 
+             texttwo = "Premiere phrase.  Deuxieme phrase.")
+    mycorpus <- corpus(txt, 
+                       docvars = data.frame(country=c("UK", "USA"), year=c(1990, 2000)),
+                       metacorpus = list(notes = "Example showing how corpus_reshape() works."))
+    set.seed(10)
+    dfmresamp <- bootstrap_dfm(mycorpus, n = 3, verbose = FALSE)
+    expect_identical(dfmresamp[[1]], 
+                     dfm(mycorpus))
+    
+    dfmresamp <- bootstrap_dfm(txt, n = 3, verbose = FALSE)
+    expect_identical(dfmresamp[[1]], 
+                     dfm(txt))
+
+})
