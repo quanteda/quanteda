5d15bb867fef8126aa0c4c950e70238637b8d53a
HaiyanLW <wangh1651@gmail.com>
2017-03-06 18:05:02 +0000

    Add more tests on test-tolower

--- tests/testthat/test-tolower.R
+++ tests/testthat/test-tolower.R
@@ -10,13 +10,35 @@ test_that("toLower keeps acronyms.", {
     expect_equal(suppressWarnings(toLower("According to NATO", keep_acronyms = TRUE)), "according to NATO")
 })
 
-test_that("char_tolower works.", {
+test_that("char_tolower/char_toUpper works.", {
     expect_equal(suppressWarnings(toLower("According to NATO")), "according to nato")
+    expect_equal(suppressWarnings(toUpper("According to NATO")), "ACCORDING TO NATO")
 })
 
 test_that("char_tolower keeps acronyms.", {
     expect_equal(suppressWarnings(toLower("According to NATO", keep_acronyms = TRUE)), "according to NATO")
 })
 
+test_that("tokens_tolower/tokens_toupper works", {
+    toksh <- tokens("According to NATO")
+    expect_equal(suppressWarnings(as.list(toLower(toksh))[[1]]),
+                      c("according", "to", "nato"))
+    expect_equal(suppressWarnings(as.list(toUpper(toksh))[[1]]),
+                 c("ACCORDING", "TO", "NATO"))
+})
 
+test_that("toLower/toUpper.tokenizedTexts works as expected", {
+    toksh <- tokenize("According to NATO")
+    expect_equal(suppressWarnings(as.list(toLower(toksh))[[1]]),
+                 c("according", "to", "nato"))
+    expect_equal(suppressWarnings(as.list(toUpper(toksh))[[1]]),
+                 c("ACCORDING", "TO", "NATO"))
+})
 
+test_that("toLower/toUpper.corpus works as expected", {
+    corp <- corpus("According to NATO")
+    expect_equal(suppressWarnings(toLower(corp))[[1]],
+                 c("according to nato"))
+    expect_equal(suppressWarnings(toUpper(corp))[[1]],
+                 c("ACCORDING TO NATO"))
+})
