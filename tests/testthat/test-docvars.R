7caf7772929dd30ea0d4a9682fdc29d95b718033
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-03-07 10:18:29 +0000

    Update tests
    Documentation

--- tests/testthat/test-docvars.R
+++ tests/testthat/test-docvars.R
@@ -160,3 +160,13 @@ test_that("metadoc for dfm works", {
     
 }) 
 
+test_that("creating tokens and dfms with empty docvars", {
+    expect_true(
+        is.null(docvars(tokens(data_corpus_irishbudget2010, include_docvars = FALSE)))
+    )
+    expect_true(
+        is.null(docvars(dfm(data_corpus_irishbudget2010, include_docvars = FALSE)))
+    )
+    
+})
+
