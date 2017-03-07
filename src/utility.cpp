9f076896f69ba311e12afb8d0861e414c4b68416
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-03-07 18:44:13 +0000

    Clean up

--- src/utility.cpp
+++ src/utility.cpp
@@ -25,15 +25,7 @@ List qatd_cpp_chars_remove(List input_, String char_remove){
     return output_;
 }
 
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically
-// run after the compilation.
-//
 
 /*** R
-list <- list(c(1:10), c(1:20))
-qatd_cpp_structcopy_int_list(list)
 
-# df <- expand.grid(LETTERS[1:3], LETTERS[4:5], LETTERS[6], stringsAsFactors=FALSE)
-# qatd_cpp_split_df(df)
 */
