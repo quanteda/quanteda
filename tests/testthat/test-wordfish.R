51e0079d5d6a94e8b40c2417570fa464927c2c36
HaiyanLW <wangh1651@gmail.com>
2017-03-08 12:59:42 +0000

    docathon: Add test for wordfish

--- tests/testthat/test-wordfish.R
+++ tests/testthat/test-wordfish.R
@@ -13,3 +13,12 @@ test_that("textmodel-wordfish works as expected as austin::wordfish", {
     expect_gt(cc, 0.99)
 })
      
+test_that("textmodel-wordfish works as expected as austin::wordfish", {
+    ie2010dfm <- dfm(data_corpus_irishbudget2010, verbose = FALSE)
+    wfm <- textmodel_wordfish(ie2010dfm, dir = c(6,5))
+    wfmodelAustin <- c(1.7798574, -0.5795493, -1.1451491, -0.1929161,  1.8887539, -0.7086047,
+                       -0.5035685, -0.6343675, -0.9924275, -0.9817932,  1.2315642,  0.2732467,
+                       0.8305827, -0.3419316)
+    cc<-cor(wfm@theta, wfmodelAustin)
+    expect_gt(cc, 0.99)
+})
