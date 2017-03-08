6e4137b48c0231109234623c3b5826c47cf138f3
HaiyanLW <wangh1651@gmail.com>
2017-03-08 21:23:40 +0000

    Docathon: added more tests on another dataset for wordfish

--- tests/testthat/test-wordfish.R
+++ tests/testthat/test-wordfish.R
@@ -1,10 +1,10 @@
 require(quanteda)
 require(testthat)
 context('Testing textmodel-wordfish.R')
-
+data_corpus_inaugural
 # load the austin library
 # library(austin)
-test_that("textmodel-wordfish works as expected as austin::wordfish", {
+test_that("textmodel-wordfish (sparse) works as expected as austin::wordfish", {
     skip_if_not_installed("austin")
     ie2010dfm <- dfm(data_corpus_irishbudget2010, verbose = FALSE)
     wfm <- textmodel_wordfish(ie2010dfm, dir = c(6,5))
@@ -13,7 +13,7 @@ test_that("textmodel-wordfish works as expected as austin::wordfish", {
     expect_gt(cc, 0.99)
 })
      
-test_that("textmodel-wordfish works as expected as austin::wordfish", {
+test_that("textmodel-wordfish (dense) works as expected as austin::wordfish", {
     skip_if_not_installed("austin")
     ie2010dfm <- dfm(data_corpus_irishbudget2010, verbose = FALSE)
     wfm <- textmodel_wordfish(ie2010dfm, dir = c(6,5), sparse = FALSE)
@@ -22,11 +22,29 @@ test_that("textmodel-wordfish works as expected as austin::wordfish", {
     expect_gt(cc, 0.99)
 })
 
-test_that("textmodel-wordfish works as expected as austin::wordfish", {
+test_that("textmodel-wordfish works as expected: dense vd sparse", {
 
     ie2010dfm <- dfm(data_corpus_irishbudget2010, verbose = FALSE)
     wfm_d <- textmodel_wordfish(ie2010dfm, dir = c(6,5), sparse = TRUE)
     wfm_s <- textmodel_wordfish(ie2010dfm, dir = c(6,5), sparse = FALSE)
+    cc<-cor(wfm_d@theta, wfm_s@theta)
+    expect_gt(cc, 0.99)
+})
+
+test_that("textmodel-wordfish (sparse) works as expected on another dataset", {
+    
+    usdfm <- dfm(data_corpus_inaugural, verbose = FALSE)
+    wfm_d <- textmodel_wordfish(usdfm, dir = c(6,5), sparse = TRUE, svd_sparse = TRUE, residual_floor = 0.5)
+    wfm_s <- textmodel_wordfish(usdfm, dir = c(6,5), sparse = FALSE)
+    cc<-cor(wfm_d@theta, wfm_s@theta)
+    expect_gt(cc, 0.99)
+    
+    # with different sparsity of residual matrix
+    wfm_d <- textmodel_wordfish(usdfm, dir = c(6,5), sparse = TRUE, svd_sparse = TRUE, residual_floor = 1)
+    cc<-cor(wfm_d@theta, wfm_s@theta)
+    expect_gt(cc, 0.99)
+    
+    wfm_d <- textmodel_wordfish(usdfm, dir = c(6,5), sparse = TRUE, svd_sparse = TRUE, residual_floor = 2)
     cc<-cor(wfm_d@theta, wfm_s@theta)
     expect_gt(cc, 0.99)
 })
