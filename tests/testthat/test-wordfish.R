08526631527fb5b83fe27803bf79f42628b9531c
HaiyanLW <wangh1651@gmail.com>
2017-03-29 14:21:45 +0100

    Add more tests for wordfish

--- tests/testthat/test-wordfish.R
+++ tests/testthat/test-wordfish.R
@@ -18,33 +18,81 @@ test_that("textmodel-wordfish (dense) works as expected as austin::wordfish", {
     expect_gt(cc, 0.99)
 })
 
-test_that("textmodel-wordfish works as expected: dense vd sparse", {
-    wfm_d <- textmodel_wordfish(ie2010dfm, dir = c(6,5), sparse = TRUE)
-    wfm_s <- textmodel_wordfish(ie2010dfm, dir = c(6,5), sparse = FALSE)
+test_that("textmodel-wordfish works as expected: dense vs sparse vs sparse+mt", {
+
+    ie2010dfm <- dfm(data_corpus_irishbudget2010, verbose = FALSE)
+    wfm_d <- textmodel_wordfish(ie2010dfm, dir = c(6,5), sparse = FALSE)
+
+    options(mt = FALSE)
+    wfm_s <- textmodel_wordfish(ie2010dfm, dir = c(6,5), sparse = TRUE)
     cc<-cor(wfm_d@theta, wfm_s@theta)
+    expect_gt(cc, 0.99)
+    
+    options(mt = TRUE)
+    wfm_mt <- textmodel_wordfish(ie2010dfm, dir = c(6,5), sparse = TRUE)
+    cc<-cor(wfm_d@theta, wfm_mt@theta)
     expect_gt(cc, 0.99)
 })
 
 test_that("textmodel-wordfish (sparse) works as expected on another dataset", {
     usdfm <- dfm(data_corpus_inaugural, verbose = FALSE)
-    wfm_d <- textmodel_wordfish(usdfm, dir = c(6,5), sparse = TRUE, svd_sparse = TRUE, residual_floor = 0.5)
-    wfm_s <- textmodel_wordfish(usdfm, dir = c(6,5), sparse = FALSE)
+    wfm_s <- textmodel_wordfish(usdfm, dir = c(6,5), sparse = TRUE, svd_sparse = TRUE, residual_floor = 0.5)
+    wfm_d <- textmodel_wordfish(usdfm, dir = c(6,5), sparse = FALSE)
     cc<-cor(wfm_d@theta, wfm_s@theta)
     expect_gt(cc, 0.99)
     
     # with different sparsity of residual matrix
-    wfm_d <- textmodel_wordfish(usdfm, dir = c(6,5), sparse = TRUE, svd_sparse = TRUE, residual_floor = 1)
+    wfm_s <- textmodel_wordfish(usdfm, dir = c(6,5), sparse = TRUE, svd_sparse = TRUE, residual_floor = 1)
     cc<-cor(wfm_d@theta, wfm_s@theta)
     expect_gt(cc, 0.99)
     
-    wfm_d <- textmodel_wordfish(usdfm, dir = c(6,5), sparse = TRUE, svd_sparse = TRUE, residual_floor = 2)
+    wfm_s <- textmodel_wordfish(usdfm, dir = c(6,5), sparse = TRUE, svd_sparse = TRUE, residual_floor = 2)
     cc<-cor(wfm_d@theta, wfm_s@theta)
     expect_gt(cc, 0.99)
 })
 
-wfm <- textmodel_wordfish(ie2010dfm, dir = c(6,5))
+test_that("textmodel-wordfish works for quasipoisson as expected: dense vs sparse vs sparse+mt", {
+    ie2010dfm <- dfm(data_corpus_irishbudget2010, verbose = FALSE)
+    wfm_d <- textmodel_wordfish(ie2010dfm, dir = c(6,5), sparse = FALSE,
+                                 dispersion = "quasipoisson", dispersionFloor = 0)
+    options(mt = TRUE)
+    wfm_mt <- textmodel_wordfish(ie2010dfm, dir = c(6,5), 
+                                dispersion = "quasipoisson", dispersionFloor = 0)
+    cc<-cor(wfm_d@theta, wfm_mt@theta)
+    expect_gt(cc, 0.99)
+    
+    options(mt = FALSE)
+    wfm_s <- textmodel_wordfish(ie2010dfm, dir = c(6,5), 
+                                 dispersion = "quasipoisson", dispersionFloor = 0)
+    cc<-cor(wfm_d@theta, wfm_s@theta)
+    expect_gt(cc, 0.99)
+    
+})
+
 
 test_that("print/show/summary method works as expected", {
+    ie2010dfm <- dfm(data_corpus_irishbudget2010, verbose = FALSE)
+    
+    # sparse + mt
+    options(mt = TRUE)
+    wfm <- textmodel_wordfish(ie2010dfm, dir = c(6,5))
+    expect_output(print(wfm), "[ ]*Documents[ ]*theta[ ]*SE[ ]*lower")
+    expect_output(print(wfm), "^Fitted wordfish model:")
+    expect_output(print(wfm), "Estimated feature scores:")
+    
+    # show method
+    expect_output(show(wfm), "[ ]*Documents[ ]*theta[ ]*SE[ ]*lower")
+    expect_output(show(wfm), "^Fitted wordfish model:")
+    expect_output(show(wfm), "Estimated feature scores:")
+    
+    # summary method
+    expect_output(summary(wfm), "[ ]*theta[ ]*SE[ ]*lower")
+    expect_output(summary(wfm), "Call:\n\ttextmodel_wordfish\\(data = ie2010dfm, dir = c\\(6, 5\\)\\)")
+    expect_output(summary(wfm), "Estimated document positions:")
+    
+    # sparse but not mt
+    options(mt = FALSE)
+    wfm <- textmodel_wordfish(ie2010dfm, dir = c(6,5))
     expect_output(print(wfm), "[ ]*Documents[ ]*theta[ ]*SE[ ]*lower")
     expect_output(print(wfm), "^Fitted wordfish model:")
     expect_output(print(wfm), "Estimated feature scores:")
@@ -74,3 +122,7 @@ test_that("for wordfish, coef and coefficients are the same", {
     expect_equal(coef(wfm), coefficients(wfm))
 })
 
+=======
+# reset the global setting
+.onAttach()
+>>>>>>> Add more tests for wordfish
