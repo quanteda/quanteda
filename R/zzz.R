41893e9280190feb366647934d70fcea10b4abe2
HaiyanLW <wangh1651@gmail.com>
2017-03-29 14:20:12 +0100

    Set global option for multi-thread

--- R/zzz.R
+++ R/zzz.R
@@ -4,8 +4,10 @@
     if (threads == 1) {
         packageStartupMessage("Disabling parallel computing")
         RcppParallel::setThreadOptions(1)
+        options(mt = FALSE)
     } else {
         packageStartupMessage("Using ", threads - 1, " of ", threads, " cores for parallel computing")
         RcppParallel::setThreadOptions(threads - 1)
+        options(mt = TRUE)
     }
 }
