44cfe67d56244d372c641c433e175c56f035cb11
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-04-01 08:43:27 +0100

    Move design vignette to subfolder

--- vignettes/design.R
+++
@@ -1,4 +0,0 @@
-## ---- eval=FALSE---------------------------------------------------------
-#  mydfm <- texts(mycorpus, group = "party") %>% char_tolower %>% tokens %>% wordstem %>%
-#                                  tokens_remove(stopwords("english")) %>% dfm
-
