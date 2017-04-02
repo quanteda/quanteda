842ba5eeb771b4103e904b03b80d3d4c07464bed
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-04-02 22:18:35 +0100

    Reimpliment all the dictionary readers

--- R/dictionaries.R
+++ R/dictionaries.R
@@ -99,7 +99,7 @@ setMethod("show", "dictionary",
 #' @param encoding additional optional encoding value for reading in imported 
 #'   dictionaries. This uses the \link{iconv} labels for encoding.  See the 
 #'   "Encoding" section of the help for \link{file}.
-#' @param toLower if \code{TRUE}, convert all dictionary values to lowercase
+#' @param tolower if \code{TRUE}, convert all dictionary values to lowercase
 #' @return A dictionary class object, essentially a specially classed named list
 #'   of characters.
 #' @references Wordstat dictionaries page, from Provalis Research 
@@ -144,49 +144,45 @@ setMethod("show", "dictionary",
 #' @export
 dictionary <- function(..., file = NULL, format = NULL, 
                        concatenator = " ", 
-                       toLower = TRUE, encoding = "") {
+                       tolower = TRUE, encoding = "") {
   # these allow implicit list construction through ...
   x <- list(...)
   if (length(x)==1) 
       x <- as.list(x[[1]])
   if (!is.null(x) & !is.list(x))
     stop("Dictionaries must be named lists or lists of named lists.")
-  # if (any(missingLabels <- which(names(x) == ""))) 
-  #   stop("missing key name for list element", 
-  #        ifelse(length(missingLabels)>1, "s ", " "),
-  #        missingLabels, "\n") 
-  # if (!is.null(x) & !is.list(x))
-  #   stop("Dictionaries must be named lists or lists of named lists.")
 
-  dict_format_mapping <- c(cat="wordstat", dic="LIWC", ykd="yoshikoder", lcd="yoshikoder", 
-                           lc3="lexicoder", yml="YAML")
+  formats <- c(cat = "wordstat", dic = "LIWC", ykd = "yoshikoder", lcd = "yoshikoder", 
+               lc3 = "lexicoder", yml = "YAML")
+  
   if (!is.null(file)) {
 
       if (is.null(format)) {
           ext <- file_ext(file)
-          if (ext %in% names(dict_format_mapping)) {
-              format <- dict_format_mapping[[ext]]
+          if (ext %in% names(formats)) {
+              format <- formats[[ext]]
           }
           else {
               stop(paste("Unknown dictionary file extension", ext))
           }
       }
       else {
-          format <- match.arg(format, dict_format_mapping)
+          format <- match.arg(format, formats)
       }
-      format <- unname(format)
-      
-      if (format=="wordstat") {
-          x <- readWStatDict(file, enc = encoding, toLower = toLower)
-      } else if (format=="LIWC") {
-          x <- readLIWCdict(file, toLower = toLower, encoding = encoding)
-      } else if (format=="yoshikoder") {
-          x <- readYKdict(file)
-      } else if (format=="lexicoder") {
-          x <- readLexicoderDict(file)
-      } else if (format=="YAML") {
+
+      if (format == "wordstat") {
+          x <- read_dict_wordstat(file, encoding)
+      } else if (format == "LIWC") {
+          x <- read_dict_liwc(file, encoding)
+      } else if (format == "yoshikoder") {
+          x <- read_dict_yoshikoder(file)
+      } else if (format == "lexicoder") {
+          x <- read_dict_lexicoder(file)
+      } else if (format == "YAML") {
           x <- yaml::yaml.load_file(file, as.named.list = TRUE)
-      }    
+      }
+      if (tolower)
+          x <- lapply(x, stringi::stri_trans_tolower)
   }
   new("dictionary", x, format = format, file = file, concatenator = concatenator)
 }
@@ -269,241 +265,95 @@ is.dictionary <- function(x) {
 
 
 # Import a Lexicoder dictionary
-# 
-# @param path full pathname of the lexicoder dictionary file (usually ending in .lcd)
-# @param toLower if \code{TRUE} (default), convert the dictionary entries to lower case
-# @return a named list, where each the name of element is a category/key and each element is a list of
-#   the dictionary terms corresponding to that level.
-# @author Adam Obeng
-# @export
-readLexicoderDict <- function(path, toLower=TRUE) {
-    current_key <- NULL
-    current_terms <- c()
-    dict <- list() 
-    #  Lexicoder 3.0 files are always UTF-8
-    for (l in readLines(con <- file(path, encoding = 'utf-8'))) {
-        if (toLower) l <- tolower(l)
-        if (substr(l, 1, 1) == '+') {
-            if (length(current_terms) > 0) {
-                dict[[current_key]] <- current_terms
-            }
-            current_key <- substr(l, 2, nchar(l))
-            current_terms <- c()
-        }
-        else {
-            current_terms <- c(current_terms, l)
-        }
-    }
-    dict[[current_key]] <- stringi::stri_trim_both(current_terms)
+#' dict <- read_dict_lexicoder('/home/kohei/Documents/Dictionary/Lexicoder/LSDaug2015/LSD2015.lc3')
+
+read_dict_lexicoder <- function(path){
+    lines <- stringi::stri_read_lines(path, encoding = 'utf-8')
+    lines <- stringi::stri_enc_toutf8(lines)
+    lines <- stringi::stri_trim_both(lines)
+    lines_yaml <- ifelse(stringi::stri_detect_regex(lines, '^\\+'),
+                         stringi::stri_replace_all_regex(lines, '^+(.+)$', '"$1":'),
+                         stringi::stri_replace_all_regex(lines, '^(.+)$', ' - "$1"'))
+    lines_yaml <- stringi::stri_replace_all_regex(lines_yaml, '[[:control:]]', '') # clean
+    yaml <- paste0(lines_yaml, collapse = '\n')
+    dict <- yaml::yaml.load(yaml, as.named.list = TRUE)
     return(dict)
 }
 
 # Import a Wordstat dictionary
-# 
-# Make a flattened list from a hierarchical wordstat dictionary
-# 
-# @param path full pathname of the wordstat dictionary file (usually ending in .cat)
-# @param enc a valid input encoding for the file to be read, see \link{iconvlist}
-# @param lower if \code{TRUE} (default), convert the dictionary entries to lower case
-# @return a named list, where each the name of element is a bottom level
-#   category in the hierarchical wordstat dictionary. Each element is a list of
-#   the dictionary terms corresponding to that level.
-# @author Kohei Watanabe, Kenneth Benoit
-# @export
-# @examples
-# \dontrun{
-# path <- '~/Dropbox/QUANTESS/corpora/LaverGarry.cat'
-# lgdict <- readWStatDict(path)
-# }
-readWStatDict <- function(path, enc="", toLower=TRUE) {
-    d <- utils::read.delim(path, header=FALSE, fileEncoding=enc, na.string = "__________")
-    d <- data.frame(lapply(d, as.character), stringsAsFactors=FALSE)
-    thismajorcat <- d[1,1]
-    # this loop fills in blank cells in the category|term dataframe
-    for (i in seq_len(nrow(d))) {
-        if (d[i,1] == "") {
-            d[i,1] <- thismajorcat
-        } else {
-            thismajorcat <- d[i,1]
-        }
-        for (j in 1:(ncol(d)-1)) {
-            if(d[i,j] == "" & length(d[i-1,j])!=0) {
-                d[i,j] <- d[i-1,j] 
-            }
-        }
-        if (nchar(d[i,ncol(d)-1]) > 0) {
-            pat <- c("\\(")
-            if (!length(grep(pat, d[i,ncol(d)-1]))==0) {
-                d[i, ncol(d)] <- d[i, ncol(d)-1]
-                d[i, ncol(d)-1] <- "_"
-            }
-        }
-    }
-    flatDict <- list()
-    categ <- list()
+#' dict <- readWStatDict2('/home/kohei/Documents/Dictionary/Wordstat/ROGET.CAT', 'utf-8')
+#' dict <- readWStatDict2('/home/kohei/Documents/Dictionary/Wordstat/WordStat Sentiments.CAT', 'iso-8859-1')
+
+read_dict_wordstat <- function(path, encoding= 'auto') {
     
-    # this loop collapses the category cells together and
-    # makes the list of named lists compatible with dfm
-    for (i in seq_len(nrow(d))) {
-        if (d[i, ncol(d)] ==  "") next
-        categ <- unlist(paste(d[i,(1:(ncol(d)-1))], collapse = "."))
-        w <- d[i, ncol(d)]
-        w <- unlist(strsplit(w, '\\('))[[1]]
-        if (toLower) w <- char_tolower(w)
-        # w <- gsub(" ", "", w)
-        flatDict[[categ]] <- append(flatDict[[categ]], c(w))
-    }
-    # remove any left-over whitespace
-    flatDict <- lapply(flatDict, function(x) gsub("\\s", "", x, perl=TRUE))
-    return(flatDict)
+    lines <- stringi::stri_read_lines(path, encoding = encoding)
+    lines <- stringi::stri_enc_toutf8(lines)
+    lines <- stringi::stri_trim_right(lines)
+    lines_yaml <- ifelse(stringi::stri_detect_regex(lines, ' \\(\\d\\)$'),
+                         stringi::stri_replace_all_regex(lines, '^(\\t*)(.+) \\(\\d\\)$', '$1- "$2"'),
+                         stringi::stri_replace_all_regex(lines, '^(\\t*)(.+)$', '$1"$2": '))
+    
+    lines_yaml <- stringi::stri_replace_all_regex(lines_yaml, '\t', ' ')
+    lines_yaml <- stringi::stri_replace_all_regex(lines_yaml, '[[:control:]]', '') # clean
+    yaml <- paste0(lines_yaml, collapse = '\n')
+    dict <- yaml::yaml.load(yaml, as.named.list = TRUE)
+    return(dict)
 }
 
-
-
 # Import a LIWC-formatted dictionary
-# 
-# Make a flattened dictionary list object from a LIWC dictionary file.
-# @param path full pathname of the LIWC-formatted dictionary file (usually a
-#   file ending in .dic)
-# @param enc a valid input encoding for the file to be read, see 
-#   \link{iconvlist}
-# @param maxcats the maximum number of categories to read in, set by the 
-#   maximum number of dictionary categories that a term could belong to.  For 
-#   non-exclusive schemes such as the LIWC, this can be up to 7.  Set to 10 by 
-#   default, which ought to be more than enough.
-# @return a dictionary class named list, where each the name of element is a
-#   bottom level category in the hierarchical wordstat dictionary. Each element
-#   is a list of the dictionary terms corresponding to that level.
-# @author Kenneth Benoit
-# @export
-readLIWCdict <- function(path, toLower = TRUE, encoding = getOption("encoding")) {
+# read_dict_liwc('/home/kohei/Documents/Dictionary/LIWC/LIWC2007_English.dic')
+read_dict_liwc <- function(path, encoding = 'auto') {
     
-    if (encoding == "") encoding <- getOption("encoding")
-    d <- readLines(con <- file(path, encoding = encoding), warn = FALSE)
-    close(con)
+    lines <- stringi::stri_read_lines(path, encoding = encoding)
+    lines <- stringi::stri_enc_toutf8(lines)
+    lines <- stringi::stri_trim_both(lines)
+    lines <- lines[lines != '']
     
-    # remove any lines with <of>
-    oflines <- grep("<of>", d)
-    if (length(oflines)) {
-        catm("note: ", length(oflines), " term",
-             ifelse(length(oflines)>1, "s", ""), 
-             " ignored because contains unsupported <of> tag\n", sep = "")
-        d <- d[-oflines]
-    }
+    sections <- which(lines == '%')
+    print(sections)
+    lines_key <- lines[(sections[1] + 1):(sections[2] - 1)]
+    lines_value <- lines[(sections[2] + 1):(length(lines))]
     
-    # get the row number that signals the end of the category guide
-    guideRowEnd <- max(grep("^%\\s*$", d))
-    if (guideRowEnd < 1) {
-        stop('Expected a guide (a category legend) delimited by percentage symbols at start of file, none found')
-    }
-    # extract the category guide
-    guide <- d[2:(guideRowEnd-1)]
+    keys_id <- stringi::stri_extract_first_regex(lines_key, '[^\t]+')
+    keys <- stringi::stri_extract_last_regex(lines_key, '[^\t]+')
+    keys <- stringi::stri_replace_all_regex(keys, '[[:control:]]', '') # clean
     
-    guide <- data.frame(do.call(rbind, tokenize(guide)), stringsAsFactors = FALSE)
-    colnames(guide) <- c('catNum', 'catName' )
-    guide$catNum <- as.integer(guide$catNum)
+    values <- stringi::stri_extract_first_regex(lines_value, '[^\t]+')
+    values <- stringi::stri_replace_all_regex(values, '[[:control:]]', '') # clean
+    lines_value <- stringi::stri_replace_all_regex(lines_value, '^(.+)?\t', '') # for safety
+    values_ids <- stringi::stri_extract_all_regex(lines_value, '\\d+')
     
-    # initialize the dictionary as list of NAs
-    dictionary <- list()
-    length(dictionary) <- nrow(guide)
-    # assign category labels as list element names
-    names(dictionary) <- guide[["catName"]]
     
-    # make a list of terms with their category numbers
-    catlist <- d[(guideRowEnd+1):length(d)]
+    dict <- split(rep(values, lengths(values_ids)), as.factor(unlist(values_ids, use.names = FALSE)))
+    dict <- dict[order(as.numeric(names(dict)))]
+    names(dict) <- keys[match(names(dict), keys_id)]
+    return(dict)
     
-    # remove odd parenthetical codes
-    foundParens <- grep("^\\w+\\s+\\(.+\\)", catlist)
-    if (length(foundParens)) {
-        catm("note: ignoring parenthetical expressions in lines:\n")
-        for (i in foundParens)
-            catm("  [line ", foundParens + guideRowEnd, ":] ", catlist[i], "\n", sep = "")
-        catlist <- gsub("\\(.+\\)", "", catlist)
-    }
-    
-    ## clean up irregular dictionary files
-    # remove any repeated \t
-    catlist <- gsub("\t\t+", "\t", catlist)
-    # remove any spaced before a \t
-    catlist <- gsub(" +\t", "\t", catlist)
-    # replace any blanks that should be \t with \t (e.g. in Moral Foundations dictionary)
-    catlist <- gsub("(\\d+) +(\\d+)", "\\1\t\\2", catlist)
-    # remove any \t only lines or empty lines
-    if (length(blanklines <- grep("^\\s*$", catlist))) 
-        catlist <- catlist[-blanklines]
-    
-    catlist <- strsplit(catlist, "\t")
-    catlist <- as.data.frame(do.call(rbind, lapply(catlist, '[', 1:max(sapply(catlist, length)))), stringsAsFactors = FALSE)
-    catlist[, 2:ncol(catlist)] <- sapply(catlist[, 2:ncol(catlist)], as.integer)
-    names(catlist)[1] <- "category"
-    if (toLower) catlist$category <- char_tolower(catlist$category)
-    # remove any blank rows
-    blankRowIndex <- which(is.na(catlist$category))
-    if (length(blankRowIndex)) 
-        catlist <- catlist[-blankRowIndex, ]
-    
-    # remove any parentheses
-    catlist[["category"]] <- gsub("(\\s|\\w|\\b)[()](\\w|\\s)", "\\1\\2", catlist[["category"]])
-    
-    # merge terms that appear on more than one line
-    catlist <- split(catlist[, 2:ncol(catlist)], catlist$category)
-    catlist <- lapply(catlist, function(y) sort(unique(unlist(y))))
-    catnames <- names(catlist)
-    catlist <- as.data.frame(do.call(rbind, lapply(catlist, '[', 1:max(sapply(catlist, length)))), stringsAsFactors = FALSE)
-    rownames(catlist) <- catnames
-    
-    terms <- as.list(rep(NA, nrow(catlist)))
-    names(terms) <- rownames(catlist)
-    for (i in seq_len(nrow(catlist))) {
-        terms[[i]] <- as.numeric(catlist[i, !is.na(catlist[i,])])
-    }
-    
-    for (ind in seq_along(terms)) {
-        for(num in as.numeric(terms[[ind]])){
-            thisCat <- guide$catName[which(guide$catNum==num)]
-            thisTerm <- names(terms[ind])
-            dictionary[[thisCat]] <- append(dictionary[[thisCat]], thisTerm)
-        }
-    }
-    return(dictionary)
 }
 
 # Import a Yoshikoder dictionary
-# 
-# Make a flattened list from a hierarchical Yoshikoder dictionary.  
-# 
-# Parsing Yoshikoder dictionary requires the XML package to be installed.
-# 
-# @param path full pathname of the Yoshikoder dictionary file (ending in \code{.ykd})
-# @return a named list, where each the name of element is a \textit{top} level
-#   category in the hierarchical Yoshikoder dictionary. Each element of the
-#   list is is a vector of the dictionary patterns in that category or any of its
-#   sub-categories.
-# @author Will Lowe
-# @export
-# @examples
-# \dontrun{
-# path <- 'http://dl.conjugateprior.org/laver-garry-ajps.ykd'
-# ykdict <- readYoshikoderDict(path)
-# }
-readYKdict <- function(path){
+# dict <- read_dict_yoshikoder('/home/kohei/Documents/Dictionary/Yoshikoder/laver-garry-ajps.ykd')
+read_dict_yoshikoder <- function(path){
     if (!requireNamespace("XML", quietly = TRUE))
         stop("You must have package XML installed to parse Yoshikoder dictionary files.")
-    
-    xx <- XML::xmlParse(path)
-    catnames <- XML::xpathSApply(xx, "/dictionary/cnode/cnode", 
-                                 XML::xmlGetAttr, name="name")
-    get_patterns_in_subtree <- function(x){
-        XML::xpathSApply(x, ".//pnode", XML::xmlGetAttr, name="name")
+
+    nodesToList <- function(node, dict = list()){
+        nodes <- XML::xpathSApply(node, "cnode")
+        if (length(nodes)) {
+            for (i in seq_along(nodes)) {
+                key <- XML::xmlGetAttr(nodes[[i]], name="name")
+                dict[[key]] <- nodesToList(nodes[[i]], dict[[key]])
+            }
+        } else {
+            dict <- unname(XML::xpathSApply(node, "pnode/@name"))
+        }
+        return(dict)
     }
-    cats <- XML::getNodeSet(xx, "/dictionary/cnode/cnode")
-    stats::setNames(lapply(cats, get_patterns_in_subtree), catnames)
+    
+    xml <- XML::xmlParse(path)
+    root <- XML::xpathSApply(xml, "/dictionary/cnode")
+    dict <- nodesToList(root[[1]])
+    return(dict)
 }
 
-
-    
-
- 
-
-
-
+dict <- read_dict_yoshikoder('/home/kohei/Documents/Dictionary/Yoshikoder/laver-garry-ajps.ykd')
