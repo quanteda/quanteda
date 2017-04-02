fcaa6200175337a6fdef7bbbf6e2af64a75a7bd3
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-04-02 22:17:08 +0100

    Add options for quanteda, set onAttach

---
+++ R/quanteda_options.R
@@ -0,0 +1,91 @@
+# implement default options
+QUANTEDA_OPTION_LIST <- list(quanteda_threads = 1L,
+                             quanteda_verbose = FALSE)
+
+#' get or set package options for quanteda
+#' 
+#' Get or set global options affecting functions across \pkg{quatneda}.
+#' @param ... options to be set, as key-value pair, same as \code{\link{options}}
+#' @param reset logical; if \code{TRUE}, reset all \pkg{quanteda} options to their 
+#'   default values
+#' @details
+#' Currently available options are:
+#' \describe{
+#' \item{\code{verbose}}{logical; if \code{TRUE} then use this as the default
+#'    for all functions with a \code{verbose} argument}
+#' \item{\code{threads}}{integer; specifies the number of threads to use in
+#'    use this as the setting in all functions that uee parallelization}
+#' }
+#' @return 
+#'   When called using a \code{key = value} pair (where \code{key} can be a label or 
+#'   quoted character name)), the option is set and \code{TRUE} is returned invisibly.
+#' 
+#'   When called with no arguments, a named list of the package options is returned.
+#'   
+#'   When called with \code{reset = TRUE} as an argument, all arguments are options 
+#'   are reset to their default values, and \code{TRUE} is returned invisibly.
+#' @export
+#' @importFrom RcppParallel setThreadOptions
+#' @examples
+#' quanteda_options()
+#' quanteda_options(verbose = FALSE)
+#' quanteda_options("verbose" = FALSE)
+#' quanteda_options("threads")
+#' \dontrun{
+#' quanteda_options(reset = TRUE) 
+#' }
+quanteda_options <- function(..., reset = FALSE) {
+    args <- list(...)
+    
+    if ("reset" %in% names(args)) 
+        reset <- args$reset
+    if (reset) {
+        
+        #########################
+        ## HARD-CODED DEFAULTS ##
+        #########################
+        options(quanteda_threads = 1L)
+        options(quanteda_verbose = FALSE)
+        
+        return(invisible(TRUE))
+    }
+        
+    # if no options are specified
+    if (!length(args)) {
+        retlist <- options()[names(QUANTEDA_OPTION_LIST)]
+        names(retlist) <- stringi::stri_replace_all_fixed(names(retlist), "quanteda_", "")
+        return(retlist)
+    }
+    
+    for (i in seq_along(args)) {
+        key <- names(args)[i]
+        value <- args[[i]]
+        
+        # if the name of the argument was specified, without assigning a value
+        if (is.null(key)) {
+            key <- value
+            value <- NA
+        }
+        
+        if (key == "threads") {
+            if (is.na(value)) {
+                return(getOption("quanteda_threads"))
+            } else {
+                options(quanteda_threads = value)
+            }
+        
+        } else if (key == "verbose") {
+            if (is.na(value)) {
+                return(getOption("quanteda_verbose"))
+            } else {
+                options(quanteda_verbose = value)
+            }
+
+        } else {
+            stop(key, " is not a valid quanteda option")
+        }
+    }
+    return(invisible(TRUE))
+}
+
+
--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,21 @@
 .onAttach <- function(...) {
+    # startup message
     packageStartupMessage("quanteda version ", as.character(utils::packageVersion("quanteda")))
+
+    # initialize quanteda options
+    quanteda_options(reset = TRUE)
+    
+    # set verbose
+    quanteda_options(verbose = FALSE)
+
+    # set threads
     threads <- RcppParallel::defaultNumThreads()
     if (threads == 1) {
         packageStartupMessage("Disabling parallel computing")
-        RcppParallel::setThreadOptions(1)
-        options(mt = FALSE)
+        quanteda_options(threads = 1)
     } else {
         packageStartupMessage("Using ", threads - 1, " of ", threads, " cores for parallel computing")
-        RcppParallel::setThreadOptions(threads - 1)
-        options(mt = TRUE)
+        quanteda_options(threads = threads - 1)
     }
+
 }
