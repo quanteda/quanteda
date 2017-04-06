8237223dd46a5e63195da80bb592eba995d6273a
HaiyanLW <wangh1651@gmail.com>
2017-04-06 18:27:18 +0100

    Add more tests for ca

--- tests/testthat/test-textmodel_ca.R
+++ tests/testthat/test-textmodel_ca.R
@@ -2,7 +2,7 @@ context('Testing textmodel-ca_sparse.R')
 
 ie2010dfm <- dfm(data_corpus_irishbudget2010)
 
-test_that("textmodel-ca (sparse) works as expected as ca::ca", {
+test_that("textmodel-ca (rsvd) works as expected as ca::ca", {
     skip_if_not_installed("ca")
     wca <- ca::ca(as.matrix(ie2010dfm))
     wtca <- textmodel_ca_sparse(ie2010dfm)
@@ -12,6 +12,25 @@ test_that("textmodel-ca (sparse) works as expected as ca::ca", {
     expect_equal(wca$sv[1:length(wtca$sv)], wtca$sv, tolerance = 1e-6)
 })
 
+test_that("textmodel-ca (RSpectra) works as expected as ca::ca", {
+    skip_if_not_installed("ca")
+    wca <- ca::ca(as.matrix(ie2010dfm))
+    wtca <- textmodel_ca_sparse(ie2010dfm, method = "RSpectra")
+    expect_equal(wca$rowmass, wtca$rowmass, tolerance = 1e-6)
+    expect_equal(wca$colmass, wtca$colmass, tolerance = 1e-6)
+    
+    expect_equal(wca$sv[1:length(wtca$sv)], wtca$sv, tolerance = 1e-6)
+})
+
+test_that("textmodel-ca works as expected as ca::ca : use mt", {
+    skip_if_not_installed("ca")
+    wca <- ca::ca(as.matrix(ie2010dfm))
+    wtca <- textmodel_ca_sparse(ie2010dfm, mt = TRUE)
+    expect_equal(wca$rowmass, wtca$rowmass, tolerance = 1e-6)
+    expect_equal(wca$colmass, wtca$colmass, tolerance = 1e-6)
+    
+    expect_equal(wca$sv[1:length(wtca$sv)], wtca$sv, tolerance = 1e-6)
+})
 test_that("textmodel-ca (sparse) works as expected as ca::ca: for given number of dimension", {
     skip_if_not_installed("ca")
     wca <- ca::ca(as.matrix(ie2010dfm))
