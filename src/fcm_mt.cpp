098d97fd375238e5e68a4247b10cc93af5f136e0
HaiyanLW <wangh1651@gmail.com>
2017-04-06 15:10:43 +0100

    Moved the typedefs to header file

--- src/fcm_mt.cpp
+++ src/fcm_mt.cpp
@@ -27,13 +27,6 @@ struct equal_pair {
 };
 
 typedef std::unordered_set<std::pair<unsigned int, unsigned int>, hash_pair, equal_pair> SetPair;
-#if QUANTEDA_USE_TBB
-    typedef std::tuple<unsigned int, unsigned int, double> Triplet;
-    typedef tbb::concurrent_vector<Triplet> Triplets;
-#else
-    typedef std::tuple<unsigned int, unsigned int, double> Triplet;
-    typedef std::vector<Triplet> Triplets;
-#endif   
 
 // find out if a pair of token exists in a document
 bool exist(const unsigned int &x, const unsigned int &y,
