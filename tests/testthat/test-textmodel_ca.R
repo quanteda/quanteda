250b7ef1adfc6ab4ea90dac58107ca1182d5fb8c
HaiyanLW <wangh1651@gmail.com>
2017-04-06 19:20:25 +0100

    remove plot test

--- tests/testthat/test-textmodel_ca.R
+++ tests/testthat/test-textmodel_ca.R
@@ -1,4 +1,4 @@
-context('Testing textmodel-ca_sparse.R')
+context('Testing textmodel-ca.R')
 
 ie2010dfm <- dfm(data_corpus_irishbudget2010)
 
@@ -31,7 +31,8 @@ test_that("textmodel-ca works as expected as ca::ca : use mt", {
     
     expect_equal(wca$sv[1:length(wtca$sv)], wtca$sv, tolerance = 1e-6)
 })
-test_that("textmodel-ca (sparse) works as expected as ca::ca: for given number of dimension", {
+
+test_that("textmodel-ca works as expected as ca::ca: for given number of dimension", {
     skip_if_not_installed("ca")
     wca <- ca::ca(as.matrix(ie2010dfm))
     wtca <- textmodel_ca(ie2010dfm, nd = 10)
@@ -41,17 +42,17 @@ test_that("textmodel-ca (sparse) works as expected as ca::ca: for given number o
     expect_equal(wca$sv[1:length(wtca$sv)], wtca$sv, tolerance = 1e-6)
 })
 
-test_that("textmodel-ca (sparse) generates a ca::ca: object, which can call other functions from ca package", {
-    skip_if_not_installed("ca")
-    wca <- ca::ca(smoke)
-    wtca <- textmodel_ca(as.dfm(smoke), nd=5)
-    expect_equal(wca$rowmass, wtca$rowmass, tolerance = 1e-6)
-    
-    # plot method
-    p <- ca::plot.ca(wtca)
-    expect_equal(dimnames(p$rows)[[2]][1], "Dim1")
-    
-    # summary method
-    s <- summary(wtca)
-    expect_equal(attr(s, "class"), "summary.ca")
-})
+# test_that("textmodel-ca generates a ca::ca: object, which can call other functions from ca package", {
+#     skip_if_not_installed("ca")
+#     wca <- ca::ca(smoke)
+#     wtca <- textmodel_ca(as.dfm(smoke), nd=5)
+#     expect_equal(wca$rowmass, wtca$rowmass, tolerance = 1e-6)
+#     
+#     # plot method
+#     #p <- ca::plot.ca(wtca)
+#     #expect_equal(dimnames(p$rows)[[2]][1], "Dim1")
+#     
+#     # summary method
+#     s <- summary(wtca)
+#     expect_equal(attr(s, "class"), "summary.ca")
+# })
