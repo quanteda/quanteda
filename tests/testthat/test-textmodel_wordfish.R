bda29ddf6e97916a7ecd91042580c912d7949fb6
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-04-07 10:24:20 -0500

    change name of test-wordfish.R

--- tests/testthat/test-textmodel_wordfish.R
+++ tests/testthat/test-textmodel_wordfish.R
@@ -46,7 +46,7 @@ test_that("print/show/summary method works as expected", {
     
     # summary method
     expect_output(summary(wfm_mt), "[ ]*theta[ ]*SE[ ]*lower")
-    expect_output(summary(wfm_mt), "Call:\n\ttextmodel_wordfish\\(data = ie2010dfm, dir = c\\(6, 5\\), sparse = TRUE\\)")
+    expect_output(summary(wfm_mt), "Call:\n\ttextmodel_wordfish\\(x = ie2010dfm, dir = c\\(6, 5\\), sparse = TRUE\\)")
     expect_output(summary(wfm_mt), "Estimated document positions:")
     
     # sparse but not mt
@@ -61,7 +61,7 @@ test_that("print/show/summary method works as expected", {
     
     # summary method
     expect_output(summary(wfm_s), "[ ]*theta[ ]*SE[ ]*lower")
-    expect_output(summary(wfm_s), "Call:\n\ttextmodel_wordfish\\(data = ie2010dfm, dir = c\\(6, 5\\), sparse = TRUE\\)")
+    expect_output(summary(wfm_s), "Call:\n\ttextmodel_wordfish\\(x = ie2010dfm, dir = c\\(6, 5\\), sparse = TRUE\\)")
     expect_output(summary(wfm_s), "Estimated document positions:")
 })
 
