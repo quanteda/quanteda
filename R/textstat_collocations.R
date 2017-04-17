8ace11086aaed156c9e5bc2e2a2d6b2995da1b30
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-04-17 13:10:54 -0400

    Update Rd file for textstat_collocations()

--- R/textstat_collocations.R
+++ R/textstat_collocations.R
@@ -9,16 +9,34 @@
 
 #' calculate collocation statistics
 #' 
-#' Identify and score collocations from a corpus, character, or tokens object,
+#' Identify and score collocations from a corpus, character, or tokens object, 
 #' with targeted selection.
-#' @param x a character, \link{corpus}, or \link{tokens} object to be mined 
-#' for collocations
-#' @param method either one of the methods in \code{\link{collocations}} or 
-#'   \code{"bj"} for Blaheta and Johnson's method (called through 
-#'   \code{\link{sequences}})
-#' @param ... additional arguments passed to \code{\link{collocations}}
+#' @param x a character, \link{corpus}, or \link{tokens} object to be mined for
+#'   collocations
+#' @param method association measure for detecting collocations.  Let \eqn{i} 
+#'   index documents, and \eqn{j} index features, \eqn{n_{ij}} refers to 
+#'   observed counts, and \eqn{m_{ij}} the expected counts in a collocations 
+#'   frequency table of dimensions \eqn{(J - size + 1)^2}. Available measures 
+#'   are computed as: \describe{ \item{\code{"lr"}}{The likelihood ratio 
+#'   statistic \eqn{G^2}, computed as: \deqn{2 * \sum_i \sum_j ( n_{ij} * log 
+#'   \frac{n_{ij}}{m_{ij}} )} } \item{\code{"chi2"}}{Pearson's \eqn{\chi^2} 
+#'   statistic, computed as: \deqn{\sum_i \sum_j \frac{(n_{ij} - 
+#'   m_{ij})^2}{m_{ij}}} } \item{\code{"pmi"}}{point-wise mutual information 
+#'   score, computed as log \eqn{n_{11}/m_{11}}} \item{\code{"dice"}}{the Dice 
+#'   coefficient, computed as \eqn{n_{11}/n_{1.} + n_{.1}}} 
+#'   \item{\code{"bj"}}{Blaheta and Johnson's method (called through 
+#'   \code{\link{sequences}})} }
+#' @param ... additional arguments passed to \code{\link{collocations}} for the first four
+#'    methods, or to  \code{\link{sequences}} for \code{method = "bj"}
+#' @references Blaheta, D., & Johnson, M. (2001). 
+#'   \href{http://web.science.mq.edu.au/~mjohnson/papers/2001/dpb-colloc01.pdf}{Unsupervised
+#'    learning of multi-word verbs}. Presented at the ACLEACL Workshop on the 
+#'   Computational Extraction, Analysis and Exploitation of Collocations. 
+#'   
+#'   McInnes, B T. 2004. "Extending the Log Likelihood Measure to Improve
+#'   Collocation Identification."  M.Sc. Thesis, University of Minnesota.
 #' @export
-#' @keywords textstat
+#' @keywords textstat collocations experimental
 #' @examples
 #' \dontrun{
 #' txts <- c("This is software testing: looking for (word) pairs!  
