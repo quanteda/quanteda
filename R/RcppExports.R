ae9770a9587a8b5520a4bf11252180adf65dd160
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-04-18 20:06:21 +0100

    Add test for textmodel_ca() warning

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -85,15 +85,15 @@ qatd_cpp_chars_remove <- function(input_, char_remove) {
     .Call('quanteda_qatd_cpp_chars_remove', PACKAGE = 'quanteda', input_, char_remove)
 }
 
-wordfishcpp <- function(wfm, dirvec, priorvec, tolvec, disptype, dispmin, ABS, svd_on, residual_floor) {
-    .Call('quanteda_wordfishcpp', PACKAGE = 'quanteda', wfm, dirvec, priorvec, tolvec, disptype, dispmin, ABS, svd_on, residual_floor)
-}
-
 wordfishcpp_dense <- function(wfm, dir, priors, tol, disp, dispfloor, abs_err) {
     .Call('quanteda_wordfishcpp_dense', PACKAGE = 'quanteda', wfm, dir, priors, tol, disp, dispfloor, abs_err)
 }
 
 wordfishcpp_mt <- function(wfm, dirvec, priorvec, tolvec, disptype, dispmin, ABS, svd_sparse, residual_floor) {
     .Call('quanteda_wordfishcpp_mt', PACKAGE = 'quanteda', wfm, dirvec, priorvec, tolvec, disptype, dispmin, ABS, svd_sparse, residual_floor)
+}
+
+wordfishcpp <- function(wfm, dirvec, priorvec, tolvec, disptype, dispmin, ABS, svd_on, residual_floor) {
+    .Call('quanteda_wordfishcpp', PACKAGE = 'quanteda', wfm, dirvec, priorvec, tolvec, disptype, dispmin, ABS, svd_on, residual_floor)
 }
 
--- tests/testthat/test-textmodel_ca.R
+++ tests/testthat/test-textmodel_ca.R
@@ -114,3 +114,13 @@ test_that("ca textplot_scale1d method works", {
         "textplot_scale1d for features not implemented for CA models"
     )
 })
+
+test_that("ca sparse = FALSE with threads > 1 issues warning", {
+    camodel <- textmodel_ca(data_dfm_LBGexample)
+    textplot_scale1d(camodel, margin = "document")
+    expect_error(
+        textplot_scale1d(camodel, margin = "features"),
+        "textplot_scale1d for features not implemented for CA models"
+    )
+})
+
