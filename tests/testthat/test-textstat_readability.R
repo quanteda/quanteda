be731c57763214a2b71d34c416160b4d83fe811e
HaiyanLW <wangh1651@gmail.com>
2017-05-04 15:54:11 +0100

    Add more tests for readability for comparison with koRpus

--- tests/testthat/test-textstat_readability.R
+++ tests/testthat/test-textstat_readability.R
@@ -24,3 +24,26 @@ test_that("readability works with sentence length filtering", {
     rdb2 <- textstat_readability(txt, min_sentence_length = 3)
     expect_equal(round(rdb2$meanSentenceLength, 2), c(4, 9))
 })
+
+test_that("readability works as koRpus", {
+    skip_if_not_installed("koRpus")
+    #q_rdb <- textstat_readability("The cat in the hat ate breakfast.")
+    #fileName <- "sample_text2.txt"  
+    #for this a bit longer file, the results differed a bit because the tokenizations are 
+    #inconsistent between two packages. 
+    fileName <- "sample_text.txt"
+    q_rdb <- textstat_readability(readChar(fileName, file.info(fileName)$size))
+
+    # readability analysis from package koRpus
+    k_toks <- koRpus::tokenize(fileName, lang = "en")
+    wordlist_DC <- data_char_wordlists$dalechall
+    k_rdb <- koRpus::readability(k_toks, 
+                                 word.lists = list(Bormuth = wordlist_DC, 
+                                                   Dale.Chall = wordlist_DC, 
+                                                   Harris.Jacobson = wordlist_DC))
+
+    expect_equal(round(q_rdb$ARI, 2), round(k_rdb@ARI$grade, 2))
+    expect_equal(round(q_rdb$Coleman.Liau.grade, 2), round(k_rdb@Coleman.Liau$grade, 2))
+    expect_equal(round(q_rdb$Flesch, 2), round(k_rdb@Flesch$RE, 2))
+    expect_equal(round(q_rdb$SMOG, 2), round(k_rdb@SMOG$grade, 2))
+})
