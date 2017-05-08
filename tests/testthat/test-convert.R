937ea6cf20545ac2f74c175986ca7b4ae289943e
HaiyanLW <wangh1651@gmail.com>
2017-05-08 12:34:31 +0100

    skip stm

--- tests/testthat/test-convert.R
+++ tests/testthat/test-convert.R
@@ -113,7 +113,8 @@ test_that("test stm converter: zero-count feature", {
                              1, 0, 0, 0, 
                              1, 0, 3, 4), byrow = TRUE, nrow = 4))
     expect_warning(convert(mydfm, to = "stm"), "zero-count features: feat2")
-    
+   
+    skip_if_not_installed("stm")
     require(stm)
     stm_model <- stm(documents = stmdfm$documents, vocab = stmdfm$vocab, K=3)
     expect_output(print(stm_model), "A topic model with 3 topics")
