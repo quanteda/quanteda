1a7818e6002808467e3b86180a49a7aebc85540b
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-05-23 13:48:06 +0100

    Add test file

---
+++ tests/misc/test-win-dict.R
@@ -0,0 +1,12 @@
+x <- list(one = c("a", "b"), two = c("d", "e"))
+y <- list(one = c("a", "b"), two = c("d", "e"), three = list(sub1 = "s1", sub2 = c("s2a", "s2b")))
+
+dx <- dictionary(x)
+dy <- dictionary(y)
+
+dx
+dx$one
+
+dy
+dy$one
+dy$three
