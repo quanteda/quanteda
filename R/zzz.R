ffedc1f9f08329beb3b26c3aab5300e919a9f3bc
HaiyanLW <wangh1651@gmail.com>
2017-06-05 19:03:16 +0100

    check .Rprofile

--- R/zzz.R
+++ R/zzz.R
@@ -2,25 +2,25 @@
     # startup message
     packageStartupMessage("quanteda version ", as.character(utils::packageVersion("quanteda")))
 
+    # get options pre-set in .Rprofile
+    #pre_options <- list(getOption("quanteda_verbose"),
+    #                    getOption("quanteda_print_dfm_max_ndoc"),
+    #                    getOption("quanteda_print_dfm_max_nfeature"),
+    #                    getOption("quanteda_threads"))
+    
     # initialize quanteda options
     quanteda_options(reset = TRUE)
     
-    # set verbose
-    quanteda_options(verbose = FALSE)
-
-    # set threads
-    threads <- RcppParallel::defaultNumThreads()
-    if (threads == 1) {
-        packageStartupMessage("Disabling parallel computing")
-        quanteda_options(threads = 1)
-    } else {
-        packageStartupMessage("Using ", threads - 1, " of ", threads, " cores for parallel computing")
-        quanteda_options(threads = threads - 1)
-    }
+    # recover the setting in .Rprofile
+    #if ((length(pre_options)>=1) & !is.null(pre_options[[1]])) options(quanteda_verbose = pre_options[[1]])
+    #if ((length(pre_options)>=2) & !is.null(pre_options[[2]])) options(quanteda_print_dfm_max_ndoc = pre_options[[2]])
+    #if ((length(pre_options)>=3) & !is.null(pre_options[[3]])) options(quanteda_print_dfm_max_nfeature = pre_options[[3]])
+    #if ((length(pre_options)>=4) & !is.null(pre_options[[4]])) options(quanteda_threads = pre_options[[4]])
     
-    # set dfm print arguments
-    quanteda_options(print_dfm_max_ndoc = 20L)
-    quanteda_options(print_dfm_max_nfeature = 20L)
+    if (file.exists("~/.Rprofile")){
+        pre_options<-grep("quanteda_", readLines("~/.Rprofile"), value=TRUE)
+        eval(parse(text = pre_options))
+    }
 }
 
 .onUnload <- function (libpath) {
