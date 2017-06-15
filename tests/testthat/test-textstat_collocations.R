843c0163f3400f93b56ef5d99506be8f491e9b23
HaiyanLW <wangh1651@gmail.com>
2017-06-15 13:10:47 +0100

    Update test for the change of `size`

--- tests/testthat/test-textstat_collocations.R
+++ tests/testthat/test-textstat_collocations.R
@@ -2,8 +2,8 @@ context('test textstat_collocations.R')
 
 test_that("test that collocations do not span texts", {
     toks <- tokens(c('this is a test', 'this also a test'))
-    cols <- rbind(textstat_collocations(toks, max_size = 2, min_count = 1),
-                  textstat_collocations(toks, max_size = 3, min_count = 1))
+    cols <- rbind(textstat_collocations(toks, size = 2, min_count = 1),
+                  textstat_collocations(toks, size = 3, min_count = 1))
     
     expect_false('test this' %in% cols$collocation)
     expect_false('test this also' %in% cols$collocation)
@@ -12,7 +12,7 @@ test_that("test that collocations do not span texts", {
 
 test_that("test that collocations only include selected features", {
     toks <- tokens(c('This is a Twitter post to @someone on #something.'), what = 'fastest')
-    cols <- textstat_collocations(toks, 'lr', features = "^([a-z]+)$", valuetype = 'regex', min_count = 1, max_size = 2)
+    cols <- textstat_collocations(toks, 'lr', features = "^([a-z]+)$", valuetype = 'regex', min_count = 1, size = 2)
     
     expect_true('This is' %in% cols$collocation)
     expect_true('a Twitter' %in% cols$collocation)
@@ -21,20 +21,20 @@ test_that("test that collocations only include selected features", {
     expect_false('on #something' %in% cols$collocation)
 })
 
-# test_that("test that collocations and sequences are counting the same features", {
-#     toks <- tokens(data_corpus_inaugural, remove_punct = TRUE)
-#     toks <- tokens_remove(toks, stopwords(), padding = TRUE)
-#     seqs <- textstat_collocations(toks, method = 'bj_uni', max_size = 2)
-#     cols <- textstat_collocations(toks, method = 'lr', max_size = 2)
-#     both <- merge(seqs, cols, by = 'collocation')
-#     expect_true(all(both$count.x == both$count.x))
-# })
+test_that("test that collocations and sequences are counting the same features", {
+    toks <- tokens(data_corpus_inaugural, remove_punct = TRUE)
+    toks <- tokens_remove(toks, stopwords(), padding = TRUE)
+    seqs <- textstat_collocations(toks, method = 'bj_uni', size = 2)
+    cols <- textstat_collocations(toks, method = 'lr', size = 2)
+    both <- merge(seqs, cols, by = 'collocation')
+    expect_true(all(both$count.x == both$count.x))
+})
 
 test_that("test that extractor works with collocation", {
     
     toks <- tokens(data_corpus_inaugural, remove_punct = TRUE)
     toks <- tokens_remove(toks, stopwords(), padding = TRUE)
-    cols <- textstat_collocations(toks, method = 'lr', max_size = 2)
+    cols <- textstat_collocations(toks, method = 'lr', size = 2)
     cols <- cols[1:5,]
     expect_equal(nrow(cols), length(as.tokens(cols)))
     
@@ -44,7 +44,7 @@ test_that("bigrams and trigrams are all sorted correctly, issue #385", {
     
     toks <- tokens(data_corpus_inaugural, remove_punct = TRUE)
     toks <- tokens_remove(toks, stopwords(), padding = TRUE)
-    cols <- textstat_collocations(toks, method = 'lr', max_size = 3)
+    cols <- textstat_collocations(toks, method = 'lr', size = 2:3)
     expect_equal(order(cols$G2, decreasing = TRUE), seq_len(nrow(cols)))
     
 })
