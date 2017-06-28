3949f1e86f545ca0ab447fc55db8cd4ee5f0e6c2
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-06-28 09:25:58 +0100

    Fix tests in test-features for new phrase() scheme

--- tests/testthat/test-features.R
+++ tests/testthat/test-features.R
@@ -37,23 +37,37 @@ test_that("character vector works consistently on dfm", {
 
 test_that("character vector with whitespace works consistently on tokens", {
       
-    toks <- tokens(c("a b c d e a_b_c d e"))
+    txt <- c("a b c d e a_b_c d e")
+    toks <- tokens(txt)
+    toksch <- as.character(toks)
     feat <- 'a b c'
     expect_equivalent(
         as.list(tokens_compound(toks, features = feat))[[1]],
-        c("a_b_c", "d", "e", "a_b_c", "d", "e"))
-    
+        toksch
+    )
+    expect_equivalent(
+        as.list(tokens_compound(toks, features = phrase(feat)))[[1]],
+        c("a_b_c", "d", "e", "a_b_c", "d", "e")
+    )
+
     expect_equivalent(
         as.list(tokens_select(toks, features = feat))[[1]],
-        c("a", "b", "c"))
+        character(0)
+    )
+    expect_equivalent(
+        as.list(tokens_select(toks, features = phrase(feat)))[[1]],
+        c("a", "b", "c")
+    )
     
     expect_equivalent(
         as.list(tokens_remove(toks, features = feat))[[1]],
-        c("d", "e", "a_b_c", "d", "e"))
+        toksch
+    )
     
-    expect_equivalent(
-        kwic(toks, keywords = feat)[,5],
-        c("a b c"))
+    expect_equal(
+        nrow(kwic(toks, keywords = feat)),
+        0
+    )
 })
 
 test_that("character vector with whitespace works consistently on dfm", {
@@ -72,23 +86,40 @@ test_that("character vector with whitespace works consistently on dfm", {
 test_that("character vector with whitespace and wildcard works consistent on tokens", {
     
     toks <- tokens(c("a b c d e a_b_c d e"))
+    toksch <- as.character(toks)
     feat <- '* d e'
     expect_equivalent(
         as.list(tokens_compound(toks, features = feat))[[1]],
-        c("a", "b", "c_d_e", "a_b_c_d_e"))
-    
+        toksch
+    )
+    expect_equivalent(
+        as.list(tokens_compound(toks, features = phrase(feat)))[[1]],
+        c("a", "b", "c_d_e", "a_b_c_d_e")
+    )
+
     expect_equivalent(
         as.list(tokens_select(toks, features = feat))[[1]],
-        c("c", "d", "e", "a_b_c", "d", "e"))
+        character(0)
+    )
+    expect_equivalent(
+        as.list(tokens_select(toks, features = phrase(feat)))[[1]],
+        c("c", "d", "e", "a_b_c", "d", "e")
+    )
     
     expect_equivalent(
         as.list(tokens_remove(toks, features = feat))[[1]],
-        c("a", "b"))
-    
+        toksch
+    )
     expect_equivalent(
-        kwic(toks, keywords = feat)[,5],
-        c("c d e a_b_c d e"))
+        as.list(tokens_remove(toks, features = phrase(feat)))[[1]],
+        c("a", "b")
+    )
     
+    expect_equal(nrow(kwic(toks, keywords = feat)), 0)
+    
+    ## INCORRECT - TO FIX
+    # kwic(toks, keywords = phrase(feat))
+
 })
 
 test_that("list works consistently on tokens", {
@@ -115,22 +146,45 @@ test_that("list works consistently on tokens", {
 test_that("dictionary works consistently on tokens", {
     
     toks <- tokens(c("a b c d e a_b_c d e"))
+    toksch <- as.character(toks)
     dict <- dictionary(ABC = 'a b c', D = 'd', E = 'e')
-    expect_equivalent(
+
+    expect_equal(
         as.list(tokens_compound(toks, features = dict))[[1]],
-        c("a_b_c", "d", "e", "a_b_c", "d", "e"))
-    
-    expect_equivalent(
+        toksch
+    )
+    expect_equal(
+        as.list(tokens_compound(toks, features = phrase(dict)))[[1]],
+        c("a_b_c", "d", "e", "a_b_c", "d", "e")
+    )
+
+    expect_equal(
         as.list(tokens_select(toks, features = dict))[[1]],
-        c("a", "b", "c", "d", "e", "d", "e"))
+        c("d", "e", "d", "e")
+    )
+    expect_equal(
+        as.list(tokens_select(toks, features = phrase(dict)))[[1]],
+        c("a", "b", "c", "d", "e", "d", "e")
+    )
     
-    expect_equivalent(
+    expect_equal(
         as.list(tokens_remove(toks, features = dict))[[1]],
-        c("a_b_c"))
+        c("a", "b", "c", "a_b_c")
+    )
+    expect_equal(
+        as.list(tokens_remove(toks, features = phrase(dict)))[[1]],
+        c("a_b_c")
+    )
     
-    expect_equivalent(
-        kwic(toks, keywords = dict)[,5],
-        c("a b c d e", "d e"))
+    expect_equal(
+        as.data.frame(kwic(toks, keywords = dict))$keyword,
+        c("d e", "d e")
+    )
+    ## INCORRECT - TO FIX
+    expect_equal(
+        as.data.frame(kwic(toks, keywords = phrase(dict)))$keyword,
+        c("a b c d e", "d e")
+    )
 })
 
 test_that("dictionary works consistently on dfm", {
