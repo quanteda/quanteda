c8909f4fd58c1af1c50bd100ecc9530f065d2614
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-07-01 09:09:11 +0100

    Improve code for text(plot|stat).keyness

--- R/textplot_keyness.R
+++ R/textplot_keyness.R
@@ -1,14 +1,14 @@
 #' plot word keyness
 #' 
-#' Plot the results of textstat_keyness analysis
-#' @param x the keyness object to be plotted
-#' @param sort if \code{TRUE} (the default), order points from low to high 
+#' Plot the results of a keyword analysis using \code{\link{textstat_keyness}}.
+#' @param x a return object from \code{\link{textstat_keyness}}
+#' @param sort logical; if \code{TRUE} (the default), order points from low to high 
 #'   score. 
-#' @param showBothDoc if \code{TRUE} show dual plot for both documents
-#' @param n number of terms to plot.
+#' @param show_reference logical; if \code{TRUE}, show key reference features in addition to key target features
+#' @param n number of terms to plot
 #' @return a \pkg{ggplot2} object
 #' @export
-#' @author Kenneth Benoit and Haiyan Wang
+#' @author Haiyan Wang
 #' @seealso \code{\link{textstat_keyness}}
 #' @keywords textplot
 #' @examples
@@ -19,12 +19,12 @@
 #' head(mydfm) # make sure 'post-war' is in the first row
 #' top20 <- head(result <- textstat_keyness(mydfm), 20)
 #' tail10 <- tail(result, 10)
+#' 
 #' # plot estimated word keyness
 #' textplot_keyness(top20) 
-#' textplot_keyness(result, showBothDoc = TRUE)}
-#' 
-textplot_keyness <-  function(x, sort = TRUE, showBothDoc = FALSE, n = 20) {
-    
+#' textplot_keyness(result, show_reference = TRUE)
+#' }
+textplot_keyness <-  function(x, sort = TRUE, show_reference = FALSE, n = 20) {
     UseMethod("textplot_keyness")
 }
 
@@ -34,10 +34,13 @@ textplot_keyness <-  function(x, sort = TRUE, showBothDoc = FALSE, n = 20) {
 #' @importFrom ggplot2 coord_flip xlab ylab theme_bw geom_text theme geom_point
 #' @importFrom ggplot2 facet_grid element_line
 #' @export
-textplot_keyness <-  function(x, sort = TRUE, showBothDoc = FALSE, n = 20) {
+textplot_keyness.data.frame <- function(x, sort = TRUE, show_reference = FALSE, n = 20) {
+
+    if (!all(c("p", "n_target", "n_reference") %in% names(x)) | ncol(x) != 4) {
+        stop("x must be a return from textstat_keyness")
+    }
     
-    
-    if (!showBothDoc ){
+    if (!show_reference) {
         x <- head(x, n)
         p <- if (sort) {
             ggplot(data = x, aes(x = reorder(rownames(x), x[,1]), y = x[,1]))
@@ -56,7 +59,7 @@ textplot_keyness <-  function(x, sort = TRUE, showBothDoc = FALSE, n = 20) {
             stop (" Better plot for one Doc.")
         topn <- head(x, pos_n)
         tailn <- tail(x, neg_n)
-        if ((pos_n > 0) & (neg_n > 0)){
+        if ((pos_n > 0) & (neg_n > 0)) {
             maxY <- max(max(topn[ ,1]), max(abs(tailn[ ,1])))
         }
         
@@ -111,66 +114,50 @@ apply_theme <- function(p) {
 ggplot_dual_axis = function(plot1, plot2, which.axis = "x") {
     
     # Update plot with transparent panel
-    plot2 = plot2 + theme(panel.background = element_rect(fill = NA))
+    plot2 <- plot2 + theme(panel.background = element_rect(fill = NA))
     
     grid::grid.newpage()
     
     # Increase right margin if which.axis == "y"
-    if(which.axis == "y") plot1 = plot1 + theme(plot.margin = grid::unit(c(0.7, 1.5, 0.4, 0.4), "cm"))
+    if (which.axis == "y") plot1 <- plot1 + theme(plot.margin = grid::unit(c(0.7, 1.5, 0.4, 0.4), "cm"))
     
     # Extract gtable
-    g1 = ggplot_gtable(ggplot_build(plot1))
-    
-    g2 = ggplot_gtable(ggplot_build(plot2))
+    g1 <- ggplot_gtable(ggplot_build(plot1))
+    g2 <- ggplot_gtable(ggplot_build(plot2))
     
     # Overlap the panel of the second plot on that of the first
-    pp = c(subset(g1$layout, g1$layout$name == "panel"))
-    
-    g = gtable_add_grob(g1, g2$grobs[[which(g2$layout$name=="panel")]], pp$t, pp$l, pp$b, pp$l)
+    pp <- c(subset(g1$layout, g1$layout$name == "panel"))
+    g <- gtable_add_grob(g1, g2$grobs[[which(g2$layout$name=="panel")]], pp$t, pp$l, pp$b, pp$l)
     
     # Steal axis from second plot and modify
-    axis.lab = ifelse(which.axis == "x", "axis-b", "axis-l")
-    
-    ia = which(g2$layout$name == axis.lab)
-    
-    ga = g2$grobs[[ia]]
-    
-    ax = ga$children[[2]]
+    axis.lab <- ifelse(which.axis == "x", "axis-b", "axis-l")
+    ia <- which(g2$layout$name == axis.lab)
+    ga <- g2$grobs[[ia]]
+    ax <- ga$children[[2]]
     
     # Switch position of ticks and labels
-    if(which.axis == "x") ax$heights = rev(ax$heights) else ax$widths = rev(ax$widths)
-    
+    if (which.axis == "x") ax$heights = rev(ax$heights) else ax$widths = rev(ax$widths)
     ax$grobs = rev(ax$grobs)
-    
-    if(which.axis == "x") 
-        
-        ax$grobs[[2]]$y = ax$grobs[[2]]$y - grid::unit(1, "npc") + grid::unit(0.15, "cm") else
-            
-            ax$grobs[[1]]$x = ax$grobs[[1]]$x - grid::unit(1, "npc") + grid::unit(0.15, "cm")
+    if (which.axis == "x") {
+        ax$grobs[[2]]$y = ax$grobs[[2]]$y - grid::unit(1, "npc") + grid::unit(0.15, "cm") 
+    } else {
+        ax$grobs[[1]]$x = ax$grobs[[1]]$x - grid::unit(1, "npc") + grid::unit(0.15, "cm")
+    }
     
     # Modify existing row to be tall enough for axis
     if(which.axis == "x") g$heights[[2]] = g$heights[g2$layout[ia,]$t]
     
     # Add new row or column for axis label
-    if(which.axis == "x") {
-        
+    if (which.axis == "x") {
         g = gtable_add_grob(g, ax, 2, 4, 2, 4) 
-        
         g = gtable_add_rows(g, g2$heights[1], 1)
-        
         g = gtable_add_grob(g, g2$grob[[6]], 2, 4, 2, 4)
-        
     } else {
-        
         g = gtable_add_cols(g, g2$widths[g2$layout[ia, ]$l], length(g$widths) - 1)
-        
         g = gtable_add_grob(g, ax, pp$t, length(g$widths) - 1, pp$b) 
-        
         #g = gtable_add_grob(g, g2$grob[[7]], pp$t, length(g$widths), pp$b - 1)
-        
     }
     
     # Draw it
     grid.draw(g)
-    
 }
