4829fc4152d84ee1b93ecd401f7dbb9979290664
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-07-07 08:00:01 +0200

    Remove old test that no longer applies

--- tests/testthat/test-sequences_old.R
+++ tests/testthat/test-sequences_old.R
@@ -33,8 +33,6 @@ test_that("test that nested argument is working", {
 })
 
 test_that("test that sequences_old works with tokens_compound", {
-    skip_on_travis()
-    skip_on_appveyor()
     toks <- tokens('E E a b c E E G G f E E f f G G')
     seqs <- sequences_old(toks, "^[A-Z]$", valuetype="regex", case_insensitive = FALSE,
                       min_count = 1, nested = FALSE, ordered = FALSE)
@@ -43,11 +41,11 @@ test_that("test that sequences_old works with tokens_compound", {
     expect_equivalent(as.list(tokens_compound(toks, seqs, join = FALSE)),
                       list(c("E_E", "a", "b", "c", "E_E_G_G", "E_E", "G_G", "f", "E_E", "f", "f", "G_G")))
     
-    # # seqs have different types
-    attr(seqs, 'types') <- ''
-    expect_equivalent(as.list(tokens_compound(toks, seqs, join = FALSE)),
-                  list(c("E", "E", "a", "b", "c", "E", "E", "G", "G", "f", "E", "E", "f", "f", "G", "G")))
-    
+    # # # seqs have different types
+    # attr(seqs, 'types') <- ''
+    # expect_equivalent(as.list(tokens_compound(toks, seqs, join = FALSE)),
+    #               list(c("E", "E", "a", "b", "c", "E", "E", "G", "G", "f", "E", "E", "f", "f", "G", "G")))
+    # 
 })
 
 
