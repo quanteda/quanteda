e58ea8ea81bbdb47ca89b597f2923c3bdbbc21ae
HaiyanLW <wangh1651@gmail.com>
2017-07-19 00:14:37 +0100

    Update test due to the change in keyness attribute

--- tests/testthat/test-textstat_keyness.R
+++ tests/testthat/test-textstat_keyness.R
@@ -70,7 +70,7 @@ test_that("textstat_keyness combines non-target rows correctly", {
     mydfm <- dfm(c(d1 = "a a a b b c c c c c c d e f g h h",
                    d2 = "a a b c c d d d d e f h", 
                    d3 = "a a a a b b c c d d d d d d"))
-    expect_equal(textstat_keyness(mydfm, 1),
+    expect_equivalent(textstat_keyness(mydfm, 1),
                  textstat_keyness(rbind(mydfm[1, ], new("dfmSparse", mydfm[2, ] + mydfm[3, ])), target = "d1"))    
 })
 
