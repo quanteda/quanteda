27af3bf248c37483a53a4c2398f9685229305168
HaiyanLW <wangh1651@gmail.com>
2017-07-25 19:04:41 +0100

    Update test due to the change of counting paddings Added more test for lambda againt Jouni's implementations

--- tests/testthat/test-sequences.R
+++ tests/testthat/test-sequences.R
@@ -1,7 +1,124 @@
 context('test sequences.R')
 
-toks <- tokens(corpus_segment(data_corpus_inaugural, what = "sentence"))
-toks <- tokens_select(toks, stopwords("english"), "remove", padding = TRUE)
+########################################################################################################
+# Tests of statistics for detecting multiword expressions
+# JK, 18.7.2017
+# 
+# Two functions: One for counting the expressions and one for calculating the statistics
+
+# ************************************************************8
+
+MWEcounts <- function (candidate,text,stopword="xxxx") 
+{
+    # Function for creating the 2^K table of yes/no occurrences 
+    # in text (character vector) 
+    # of words in a K-word candidate expression (character vector) 
+    #
+    K <- length(candidate)
+    J <- length(text)-K+1
+    
+    ##############################################################################
+    # Fixed objects, here up to candidate length of 4 (extend as needed)
+    
+    count.vectors <- list(
+        c("00","01","10","11")
+    )
+    count.vectors[[2]] <- paste(rep(c("0","1"),each=4), rep(count.vectors[[1]],2),sep="")
+    count.vectors[[3]] <- paste(rep(c("0","1"),each=8), rep(count.vectors[[2]],2),sep="")
+    #
+    noyes <- c("no","yes")
+    array.dims <- list(
+        list(W2=noyes,W1=noyes),
+        list(W3=noyes,W2=noyes,W1=noyes),
+        list(W4=noyes,W3=noyes,W2=noyes,W1=noyes)
+    )
+    #
+    data.frames <- list(
+        data.frame(count=NA,W1=gl(2,2,4,labels=noyes),W2=gl(2,1,4,labels=noyes)),
+        data.frame(count=NA,W1=gl(2,4,8,labels=noyes),W2=gl(2,2,8,labels=noyes),W3=gl(2,1,8,labels=noyes)),
+        data.frame(count=NA,W1=gl(2,8,16,labels=noyes),W2=gl(2,4,16,labels=noyes),W3=gl(2,2,16,labels=noyes),W4=gl(2,1,16,labels=noyes))
+    ) 
+    
+    ###############################################################################
+    # Count the words
+    
+    counts <- rep(0,2^K)
+    names(counts) <- count.vectors[[K-1]]
+    #
+    for(j in seq(J)){
+        text.j <- text[j:(j+K-1)]
+        if(all(text.j!=stopword)){
+            agreement <- text.j==candidate
+            tmp <- paste(as.numeric(agreement),collapse="")
+            counts[tmp] <- counts[tmp]+1
+        }
+    }	
+    counts.table <- array(counts,dim=rep(2,K),dimnames=array.dims[[K-1]])
+    counts.data.frame <- data.frames[[K-1]]
+    counts.data.frame$count <- counts
+    #
+    result <- list(expression=paste(candidate,collapse=" "),counts=counts,counts.table=counts.table,counts.data.frame=counts.data.frame)
+    return(result)
+}
+
+# ************************************************************8
+
+MWEstatistics <- function (counts, smooth=0.5) 
+{
+    # Function for calculating some association statistics for a 
+    # K-word candidate expression 
+    # The input is output from the function MWEcounts 
+    #
+    counts.n <- counts$counts
+    counts.table <- counts$counts.table
+    counts.df <- counts$counts.data.frame
+    K <- length(dim(counts.table))
+    
+    results <- matrix(NA,1,9+(2^K))
+    colnames(results) <- c("length","lambda","se.lambda","z.lambda","LRtest","smooth","mInfty","Infty","N",names(counts.n))
+    rownames(results) <- counts$expression
+    results[,"length"] <- K
+    results[,"smooth"] <- smooth
+    results[,-(1:9)] <- counts.n
+    results[,"N"] <- sum(counts.n)
+    results[,"mInfty"] <- as.numeric(counts.n[2^K]==0) # 1 if the expression never appears in the text
+    results[,"Infty"] <- as.numeric(any(counts.n[-(2^K)]==0)) # 1 if the count for any lower-order margin is 0 (i.e. the log-OR is infinity)
+    
+    ##############################################################################
+    # Fixed objects, here up to candidate length of 4 (extend as needed)
+    
+    loglin.margins <- list(
+        list(1,2),
+        list(1:2,2:3,c(1,3)),
+        list(1:3,2:4,c(1,2,4),c(1,3,4))
+    )[[K-1]]
+    formula <- list(
+        count~W1*W2,
+        count~W1*W2*W3,
+        count~W1*W2*W3*W4
+    )[[K-1]]
+    
+    ###############################################################################
+    # Estimated highest-order interaction parameter (lambda), obtained using a Poisson log-linear model
+    
+    counts.df$count <- counts.df$count+smooth
+    options(warn=-1) # Switch of the warning due to the non-integer counts 
+    mod1 <- glm(formula,family=poisson,data=counts.df)
+    options(warn=0)
+    tmp <- length(coef(mod1))
+    results[,"lambda"] <- coef(mod1)[tmp]
+    results[,"se.lambda"] <- sqrt(diag(vcov(mod1)))[tmp]
+    results[,"z.lambda"] <- results[,"lambda"]/results[,"se.lambda"]
+    
+    # Likelihood ratio test of the parameter, obtained from an IPF fit from the loglin function for model without the highest-order interaction
+    # (note: this could also be obtained by fitting this model and the saturated model with glm, and asking for the LR test
+    
+    counts.table <- counts.table+smooth
+    mod2 <- loglin(counts.table,loglin.margins,print=F)	
+    results[,"LRtest"] <- mod2$lrt
+    #
+    return(results)
+}
 
 test_that("test that argument 'size'", {
     
@@ -12,7 +129,7 @@ test_that("test that argument 'size'", {
     expect_equal(seqs$collocation, c('G G', 'E E', 'E G'))
     
     seqs <- sequences(toks, min_count = 1, size=3:4, method = "lambda1")
-    expect_equal(seqs$collocation, c('E G G', 'E G G G', 'E E G G', 'G G G', 'E E G'))
+    expect_equal(seqs$collocation, c('G G G', 'E E G G', 'E G G G', 'E G G', 'E E G'))
 })
 
 test_that("test that argument 'method'", {
@@ -59,20 +176,15 @@ test_that("[ function",{
     seqs <- sequences(toks_capital, min_count = 1)
     a_seq <- seqs[1]
     
-    expect_equal(a_seq$collocation, 'E E')
+    #call Jouni's implementation
+    tt <- as.character(toks_capital)
+    test2 <- MWEcounts(c("G", "F"), tt)
+    test2_stat <- suppressWarnings(MWEstatistics(test2))
+
+    expect_equal(a_seq$collocation, 'G F')
+    expect_equal(a_seq$lambda, test2_stat[2])
     expect_equal(class(a_seq), c("sequences", 'data.frame'))
 })
-
-# test_that("as.tokens.sequences function",{
-#     toks <- tokens('E E a b c E E G G f E E f f G G')
-#     toks <- tokens_select(toks, "^[A-Z]$", valuetype="regex", 
-#                                   case_insensitive = FALSE, padding = TRUE)
-#     seqs <- sequences(toks, min_count = 1)
-#     tokens <- as.tokens(seqs)
-#     
-#     expect_equal(length(tokens), 3)
-#     expect_equal(class(tokens), c("tokens", "tokenizedTexts"))
-# })
 
 test_that("is.sequences function",{
     toks <- tokens('E E a b c E E G G f E E f f G G')
@@ -196,3 +308,39 @@ test_that("test the correctness of significant: against stats package", {
     expect_equal(seqs$chi2[cgt_index], statss$pearson, tolerance = 1e-3)
 })
 
+
+
+test_that("test the correctness of lambda: against Jouni's", {
+    toks <- tokens(inaugTexts, what = "fasterword")
+    toks <- tokens_remove(toks, "^\\p{P}$", valuetype = "regex", padding = TRUE)
+    toks <- tokens_tolower(toks)
+    inaugTexts.vector <- as.character(toks)
+    
+    ### smoothing = 0.5
+    seqs <- textstat_collocations(toks, size=2:4)
+    
+    ## bigram comparison
+    seq2 <- seqs[seqs$collocation == "united states",]
+    test2.tmp <- MWEcounts(c("united","states"),inaugTexts.vector)
+    test2_stat <- suppressWarnings(MWEstatistics(test2.tmp))
+    expect_equal(seq2$lambda, test2_stat[2], tolerance =0.01)
+    expect_equal(seq2$sigma, test2_stat[3], tolerance =0.01)
+    expect_equal(seq2$z, test2_stat[4], tolerance =0.01)
+    
+    ##trigram comparison
+    test3.tmp <- MWEcounts(c("house","of","representatives"),inaugTexts.vector)
+    test3_stat <-suppressWarnings(MWEstatistics(test3.tmp))
+    seq3 <- seqs[seqs$collocation == "house of representatives",]
+    expect_equal(seq3$lambda, test3_stat[2], tolerance =0.01)
+    expect_equal(seq3$sigma, test3_stat[3], tolerance =0.01)
+    expect_equal(seq3$z, test3_stat[4], tolerance =0.01)    
+    
+    # 4- gram comparison
+    test4.tmp <- MWEcounts(c("on","the","part","of"),inaugTexts.vector)
+    test4_stat <- suppressWarnings(MWEstatistics(test4.tmp))
+    seq4 <- seqs[seqs$collocation == "on the part of",]
+    expect_equal(seq4$lambda, test4_stat[2], tolerance =0.01)
+    expect_equal(seq4$sigma, test4_stat[3], tolerance =0.01)
+    expect_equal(seq4$z, test4_stat[4], tolerance =0.01)
+})
+    
