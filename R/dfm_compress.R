008e9b1d07b1376486e1fcf33d4f7cb2531ff0cf
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-07-26 13:31:00 +0100

    Keep always ndoc(x) == nrow(docvars(x))

--- R/dfm_compress.R
+++ R/dfm_compress.R
@@ -96,7 +96,7 @@ dfm_group.dfm <- function(x, groups = NULL, fill = FALSE) {
     x <- group_dfm(x, documents = groups, fill = fill)
     if (!is.null(groups))
         x <- x[as.character(levels(groups)),]
-    x@docvars <- data.frame()
+    x@docvars <- data.frame(row.names = docnames(x))
     return(x)
 }
 
