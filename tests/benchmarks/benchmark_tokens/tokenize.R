a2a969dd9a360077bbc66e51bef66c3f0764551a
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-07-27 17:56:11 +0100

    Update benchmark

--- tests/benchmarks/benchmark_tokens/tokenize.R
+++ tests/benchmarks/benchmark_tokens/tokenize.R
@@ -1,16 +1,25 @@
 library(profvis) # for profiling
 library(tokenizers)
 
+load("/home/kohei/Documents/Brexit/Analysis/data_corpus_guardian.RData")
+
+system.time(
+    tokens(data_corpus_guardian, what='word', hash = TRUE, verbose = TRUE)
+)
+
+
 
 txt <- rep(paste0(letters, collapse=' '), 10000)
 
 microbenchmark::microbenchmark(
-  tokenize_words(txt),
-  tokens(txt, what='fastestword'),
-  tokens(txt, what='fastestword', hash=FALSE),
-  unit='relative'
+    tokenizers::tokenize_words(txt),
+    tokens(txt, what='word', hash = TRUE),
+    tokens(txt, what='word', hash = FALSE),
+    unit='relative'
 )
 
 profvis(
   tokens(txt, what='fastestword')
 )
+
+
