6148f952b81eb7f7844d68a664a04c2b308acef4
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-09-04 12:58:11 +0100

    Keep package version as integer

--- R/dfm-classes.R
+++ R/dfm-classes.R
@@ -34,7 +34,7 @@ setClassUnion("dframe", members = c("data.frame", "NULL")) # TODO should not all
 #' @keywords internal dfm
 setClass("dfm",
          slots = c(settings = "list", weightTf = "list", weightDf = "list", smooth = "numeric", # TODO stop using camel-case
-                   ngrams = "integer", skip = "integer", concatenator = "character", version = "character",
+                   ngrams = "integer", skip = "integer", concatenator = "character", version = "integer",
                    docvars = "dframe"),
          prototype = list(settings = list(NULL),
                           Dim = integer(2), 
