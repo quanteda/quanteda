354e8c82bf8149d3b5a0d725c7ca182192e12652
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-09-19 19:46:47 +0100

    Improve handling of docvars

--- R/tokens_segment.R
+++ R/tokens_segment.R
@@ -1,19 +1,14 @@
 #' segment tokens object by patterns
 #' 
 #' @param x \link{tokens} object whose token elements will be segmented
-#' @param what unit of segmentation.  Current options are  
-#'   \code{"sentences"} (default) and \code{"other"}.
-#'   
-#'   Segmenting on \code{"other"} allows segmentation of a text on any 
-#'   user-defined value, and must be accompanied by the \code{delimiter} 
-#'   argument. 
-#' @param delimiter the same as \code{\link{pattern}}
+#' @inheritParams pattern
 #' @inheritParams valuetype
 #' @param case_insensitive ignore case when matching, if \code{TRUE}
-#' @param remove_delimiter remove deimiter, if \code{TRUE}
+#' @param extract_pattern remove matched patterns from the texts and save in
+#'   \link{docvars}, if \code{TRUE}
 #' @param verbose if \code{TRUE} print messages about how many tokens were 
 #'   selected or removed
-#' @return \code{tokens_segment} returns a \link{tokens} object whose documents
+#' @return \code{tokens_segment} returns a \link{tokens} object whose documents 
 #'   have been redefined according to the segmentation type
 #' @keywords tokens internal
 #' @export
@@ -24,20 +19,17 @@
 #' this distinguished honor."
 #' toks <- tokens(txts)
 #' 
-#' # split into sentences
-#' toks_sent <- tokens_segment(toks, what = "sentences")
-#' 
 #' # split by any punctuation
-#' toks_punc <- tokens_segment(toks, what = "other", delimiter = "[\\p{P}]", valuetype = 'regex',
-#'                             remove_delimiter = FALSE)
-#' toks_punc <- tokens_segment(toks, what = "other", delimiter = "[\\p{P}]", valuetype = 'regex', 
-#'                             remove_delimiter = TRUE)
+#' toks_punc <- tokens_segment(toks, c(".", "?", "!"), valuetype = 'fixed', 
+#'                             pattern_position = 'after')
+#' toks_punc <- tokens_segment(toks, "^\\p{P}$", valuetype = 'regex', extract_pattern = TRUE, 
+#'                             pattern_position = 'after')
 #' 
-tokens_segment <- function(x, what = c("sentences", "other"), 
-                           delimiter = NULL,
+tokens_segment <- function(x, pattern,
                            valuetype = c("glob", "regex", "fixed"),
                            case_insensitive = TRUE, 
-                           remove_delimiter = FALSE,
+                           extract_pattern = FALSE,
+                           pattern_position = c("before", "after"),
                            verbose = quanteda_options("verbose")) {
     UseMethod("tokens_segment")
 }
@@ -45,13 +37,13 @@ tokens_segment <- function(x, what = c("sentences", "other"),
 #' @rdname tokens_segment
 #' @noRd
 #' @export
-tokens_segment.tokenizedTexts <- function(x, what = c("sentences", "other"), 
-                                          delimiter = NULL,
+tokens_segment.tokenizedTexts <- function(x, pattern,
                                           valuetype = c("glob", "regex", "fixed"),
                                           case_insensitive = TRUE, 
-                                          remove_delimiter = FALSE,
+                                          extract_pattern = FALSE,
+                                          pattern_position = c("before", "after"), 
                                           verbose = quanteda_options("verbose")) {
-    x <- tokens_segment(as.tokens(x), delimiter, valuetype, case_insensitive, remove_delimiter, verbose)
+    x <- tokens_segment(as.tokens(x), pattern, valuetype, case_insensitive, extract_pattern, verbose)
     x <- as.tokenizedTexts(x)
     return(x)
 }
@@ -60,41 +52,49 @@ tokens_segment.tokenizedTexts <- function(x, what = c("sentences", "other"),
 #' @noRd
 #' @importFrom RcppParallel RcppParallelLibs
 #' @export
-tokens_segment.tokens <- function(x, what = c("sentences", "other"), 
-                                  delimiter = NULL,
+tokens_segment.tokens <- function(x, pattern,
                                   valuetype = c("glob", "regex", "fixed"),
                                   case_insensitive = TRUE, 
-                                  remove_delimiter = FALSE,
+                                  extract_pattern = FALSE,
+                                  pattern_position = c("before", "after"),
                                   verbose = quanteda_options("verbose"), ...) {
     
-    what <- match.arg(what)
     valuetype <- match.arg(valuetype)
+    pattern_position <- match.arg(pattern_position)
+    
     attrs <- attributes(x)
     types <- types(x)
-    vars <- docvars(x)
+    vars <- get_docvars2(x, c( 'system', 'user'))
 
-    if (what == "sentences") {
-        if (!is.null(delimiter)) warning("delimiter is only used for 'other'")
-        pattern <- "^[\\p{Sterm}]$"
-        valuetype <- "regex"
-    } else {
-        pattern <- delimiter
-    }
-    
     patt_id <- features2id(pattern, types, valuetype, case_insensitive, attr(x, 'concatenator'))
+    
     if ("" %in% pattern) patt_id <- c(patt_id, list(0)) # append padding index
 
-    x <- qatd_cpp_tokens_segment(x, types, patt_id, remove_delimiter)
+    if (pattern_position == "before") {
+        x <- qatd_cpp_tokens_segment(x, types, patt_id, extract_pattern, 1)
+    } else {
+        x <- qatd_cpp_tokens_segment(x, types, patt_id, extract_pattern, 2)
+    }
     
-    # create repeated docvars with document and segment IDs
-    rownames(vars) <- NULL # faster to repeat rows without rownames
-    vars <- cbind(vars[attr(x, 'docid'),,drop = FALSE], 
-                  '_docid' = attr(x, 'docid'), 
-                  '_segid' = attr(x, 'segid'))
-    rownames(vars) <- stri_c(attr(x, 'document'), '.', attr(x, 'segid'), sep = '')
+    docname <- paste(attr(x, 'document'), as.character(attr(x, 'segid')), sep = '.')
     
-    create(x, what = 'tokens', 
-           names = rownames(vars), 
-           docvars = vars, 
-           attrs = attrs)
+    if (!is.null(vars)) {
+        # create repeated docvars with document and segment IDs
+        rownames(vars) <- NULL # faster to repeat rows without rownames
+        vars <- vars[attr(x, 'docid'),,drop = FALSE]
+        vars[,'_document'] <- attr(x, 'document')
+        vars[,'_docid'] <- attr(x, 'docid')
+        vars[,'_segid'] <- attr(x, 'segid')
+        rownames(vars) <- docname
+    } else {
+        vars <- data.frame('_document' = attr(x, 'document'),
+                           '_docid' = attr(x, 'docid'),
+                           '_segid' = attr(x, 'segid'),
+                           row.names = docname,
+                           stringsAsFactors = FALSE)
+    }
+    if (extract_pattern) 
+        vars[,'pattern'] = attr(x, 'pattern')
+    
+    create(x, what = 'tokens', names = docname, docvars = vars,  attrs = attrs)
 }
