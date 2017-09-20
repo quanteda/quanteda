c5f14447a3e77a6346d0c404d94caaf0741bdf66
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-09-20 22:41:02 +0100

    Update man

--- tests/testthat/test-stopwords.R
+++
@@ -1,9 +0,0 @@
-context('test stopwords.R')
-
-test_that("test that there are some stopwords", {
-
-      expect_true( length(stopwords()) > 0 )
-      expect_true( length(stopwords('english')) > 0 )
-      expect_true( length(stopwords('SMART')) > 0 )
-      
-})
