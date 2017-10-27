63c392d453cfba71f3edbcd25bba2cde28d038e6
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-10-27 11:50:42 +0100

    Add textmodel_affinity()

---
+++ R/textplot_influence.R
@@ -0,0 +1,52 @@
+#' influence plot for text scaling models
+#' 
+#' Plot the results of a fitted scaling model, from (e.g.) a predicted 
+#' \link{textmodel_affinity} model.
+#' @param x the fitted or predicted scaling model object to be plotted
+#' @param n the number of features whose influence will be plotted
+#' @param ... additional arguments passed to \code{\link{plot}}
+#' @seealso \code{\link{textmodel_affinity}}
+#' @importFrom graphics plot
+#' @export
+#' @author Patrick Perry and Kenneth Benoit
+#' @keywords textplot
+textplot_influence <- function(x, n = 30, ...) {
+    UseMethod("textplot_influence")
+}
+
+
+#' @rdname textplot_influence
+#' @noRd
+textplot_influence.affinity_influence <- function(x, n = 30, ...) {
+    ans <- summary(x, ...)
+    plot(ans, n, ...)
+}
+
+#' @rdname textmodel_influence
+#' @importFrom graphics legend text points
+#' @export
+textplot_influence.summary_affinity_influence <- function(x, n = 30, ...) {
+    word <- x$word[x$support]
+    rate <- x$rate[x$support]
+    influence <- x$median[x$support]
+    direction <- x$direction[x$support]
+    imbalance <- influence / rate
+    
+    x <- log10(rate)
+    y <- 100 * influence
+    col <- as.integer(direction)
+    plot(x, y, type="n", xlab=expression(Log[10]("Median Rate")),
+         ylab=expression("Median Influence" %*% 100))
+    
+    if (!is.null(n) && !is.na(n)) {
+        n <- min(n, nrow(x))
+        subset <- rank(-influence, ties.method="first")  <= n
+    } else {
+        subset <- rep(TRUE, length(word))
+    }
+    points(x[!subset], y[!subset], cex=0.5, col=col[!subset])
+    text(x[subset], y[subset], word[subset], cex=0.75, col=col[subset])
+    
+    levels <- levels(direction)
+    legend("topleft", legend = levels, fill = seq_along(levels), inset=0.05)
+}
