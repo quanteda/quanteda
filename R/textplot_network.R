f2d56436a125bbfaba2c7eaf86af43d1cdbe7f72
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-10-31 17:53:13 +0000

    Update example

--- R/textplot_network.R
+++ R/textplot_network.R
@@ -11,8 +11,8 @@
 #' @author Kohei Watanabe and Stefan Müller
 #' @examples
 #' \dontrun{
-#' toks <- tokens(corpus_subset(data_corpus_inaugural, President=="Obama"), remove_punct = TRUE)
-#' toks <- tokens_remove(toks, feature = stopwords("english"), padding = FALSE)
+#' toks <- tokens(corpus_subset(data_corpus_inaugural, President == "Obama"), remove_punct = TRUE)
+#' toks <- tokens_remove(toks, stopwords("english"), padding = FALSE)
 #' myfcm <- fcm(toks, context = 'window', tri = FALSE)
 #' feats <- names(topfeatures(myfcm, 20))
 #' textplot_network(myfcm[feats, feats], ignore = 0.5)
@@ -23,23 +23,23 @@
 #'   \code{\link{coef.textmodel}}
 #' @keywords textplot
 textplot_network <- function(x, width = 10, size = 15, shape = "circle", ignore = 0.5, ...) {
-  
-  if (!is.fcm(x))
-    stop("x must be a fcm object")
-  
-  diag(x) <- 0
-  suppressMessages(
-    x[x < quantile(as.vector(x), ignore)] <- 0
-  )
-  temp <- igraph::graph_from_adjacency_matrix(x, weighted = TRUE, diag = FALSE, mode = 'undirected')
-  igraph::igraph_options(plot.layout = igraph::layout_with_fr, vertex.label.family = 'sans', 
-                         vertex.label.color = 'black',
-                         vertex.size = size,
-                         vertex.shape = shape,
-                         edge.color = adjustcolor('sky blue', 0.5),
-                         vertex.color = adjustcolor('white', 1.0),
-                         vertex.frame.color = adjustcolor('sky blue', 1.0),
-                         edge.curved = 0.3)
-  plot.igraph(temp, edge.width = igraph::E(temp)$weight / max(igraph::E(temp)$weight) * width, ...)
+    
+    if (!is.fcm(x))
+        stop("x must be a fcm object")
+    
+    diag(x) <- 0
+    suppressMessages(
+        x[x < quantile(as.vector(x), ignore)] <- 0
+    )
+    temp <- igraph::graph_from_adjacency_matrix(x, weighted = TRUE, diag = FALSE, mode = 'undirected')
+    igraph::igraph_options(plot.layout = igraph::layout_with_fr, vertex.label.family = 'sans', 
+                           vertex.label.color = 'black',
+                           vertex.size = size,
+                           vertex.shape = shape,
+                           edge.color = adjustcolor('sky blue', 0.5),
+                           vertex.color = adjustcolor('white', 1.0),
+                           vertex.frame.color = adjustcolor('sky blue', 1.0),
+                           edge.curved = 0.3)
+    igraph::plot.igraph(temp, edge.width = igraph::E(temp)$weight / max(igraph::E(temp)$weight) * width, ...)
 }
 
