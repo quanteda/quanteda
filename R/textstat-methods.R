a966776578fffb9ab07490ca0c8ce7310b41ec1d
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-12-27 12:17:14 +0000

    Replace textstat_select with filter.textstat

--- R/textstat-methods.R
+++ R/textstat-methods.R
@@ -1,71 +1,49 @@
-# base methods --------------
+# filter for textstat data.frames ------------
 
-#' @method "[" textstat
+#' @importFrom dplyr filter
 #' @export
-#' @noRd
-"[.textstat" <- function(x, i, j, ...) {
-    if (missing(i)) i <- seq_len(nrow(x))
-    if (missing(j)) j <- seq_len(ncol(x))
-    l <- class(x)
-    x <- as.data.frame(x)[i, j, drop = FALSE]
-    class(x) <- l
-    return(x)
-}
+dplyr::filter
 
-# textstat_select ------------
-
-#' Select rows of textstat objects by glob, regex or fixed patterns
+#' Return rows with matching conditions including feature matches
 #'
-#' Users can subset output object of \code{textstat_collocations},
-#' \code{textstat_keyness} or \code{textstat_frequency} based on
-#' \code{"glob"}, \code{"regex"} or \code{"fixed"} patterns using this method.
-#' @param x a \code{textstat} object
+#' Use filters to find rows of the return objects from many \code{textstat_*()}
+#' functions where conditions are true, including matches to features using
+#' \code{"glob"}, \code{"regex"} or \code{"fixed"} patterns.
+#' @param .data a \code{textstat} object returned from one of the applicable
+#' \code{textstat_*} functions in \pkg{quanteda}
 #' @inheritParams pattern
-#' @param selection whether to \code{"keep"} or \code{"remove"} the rows that
-#'   match the pattern
 #' @inheritParams valuetype
 #' @param case_insensitive ignore case when matching, if \code{TRUE}
+#' @seealso  \code{\link{textstat_collocations}},
+#' \code{\link{textstat_keyness}}, \code{\link{textstat_frequency}}
 #' @keywords textstat internal
 #' @export
 #' @examples
 #' period <- ifelse(docvars(data_corpus_inaugural, "Year") < 1945, "pre-war", "post-war")
 #' mydfm <- dfm(data_corpus_inaugural, groups = period)
 #' keyness <- textstat_keyness(mydfm)
-#' textstat_select(keyness, 'america*')
-#' 
-textstat_select <- function(x, 
+#' filter(keyness, pattern = "america*")
+#' filter(keyness, p < .00001) %>% head()
+#' filter(keyness, p < .00001, pattern = "america*") 
+filter.textstat <- function(.data, ...,
                             pattern = NULL, 
-                            selection = c("keep", "remove"), 
+                            # selection = c("keep", "remove"), 
                             valuetype = c("glob", "regex", "fixed"),
                             case_insensitive = TRUE) {
-    UseMethod("textstat_select")
-}
-
-#' @export
-textstat_select.default <- function(x, pattern = NULL, 
-                                  selection = c("keep", "remove"), 
-                                  valuetype = c("glob", "regex", "fixed"),
-                                  case_insensitive = TRUE) {
-    stop(friendly_class_undefined_message(class(x), "textstat_select"))
-}
-
-#' @export
-textstat_select.textstat <- function(x, pattern = NULL,
-                                     selection = c("keep", "remove"), 
-                                     valuetype = c("glob", "regex", "fixed"), 
-                                     case_insensitive = TRUE) {
     
-    if (is.null(pattern)) return(x)
-    selection <- match.arg(selection)
-    valuetype <- match.arg(valuetype)
-    attrs <- attributes(x)
+    attrs <- attributes(.data)
     
-    id <- unlist(regex2id(pattern, x[[1]], valuetype, case_insensitive))
-    if (selection == 'keep') {
-        x <- x[id,]
-    } else {
-        x <- x[id * -1,]
+    # call dplyr filter
+    .data <- as.data.frame(dplyr::filter(dplyr:::tbl_df(.data), ...))
+    
+    # select on features if specified
+    if (!missing(pattern)) {
+        valuetype <- match.arg(valuetype)
+        id <- unlist(regex2id(pattern, .data[[1]], valuetype, case_insensitive))
+        .data <- .data[id, , drop = FALSE]
     }
-    class(x) <- attrs$class
-    return(x)
+    
+    # reclass the object as textstat etc.
+    class(.data) <- attrs$class
+    .data
 }
