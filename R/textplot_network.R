43906b2093bb163626c8ba715e5d301d9ed4185f
Kohei Watanabe <watanabe.kohei@gmail.com>
2018-01-04 11:24:30 +0000

    Add mono for non-Windows platforms

--- R/textplot_network.R
+++ R/textplot_network.R
@@ -91,7 +91,7 @@ textplot_network.fcm <- function(x, min_freq = 0.5, omit_isolated = TRUE,
             if (!vertex_labelfont %in% names(windowsFonts()))
                 stop(msg, 'Run extrafont::import_font() and extrafont::loadfonts(device = "win") to use custom fonts.')
         } else {
-            if (!vertex_labelfont %in% c('sans', 'serif', extrafont::fonts()))
+            if (!vertex_labelfont %in% c('sans', 'serif', 'mono', extrafont::fonts()))
                 stop(msg, 'Run extrafont::import_font() to use custom fonts.')
         }
     }
