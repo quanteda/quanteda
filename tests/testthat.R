b5637956fc4bb5283d843957dedeffd818dd6e70
Kenneth Benoit <kbenoit@lse.ac.uk>
2018-02-21 14:52:42 +0000

    Change threads for tests on CRAN to two

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,6 +5,11 @@ library(quanteda)
 
 ops <- quanteda_options()
 quanteda_options(reset = TRUE)
+# change threads to 2 for CRAN
+if (!identical(Sys.getenv("NOT_CRAN"), "true")) {
+    quanteda_options(threads = 2)
+}
+
 test_check("quanteda")
 quanteda_options(ops)
 
