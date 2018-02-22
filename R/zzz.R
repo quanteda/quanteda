eeff4c718102ada45f06c2c6fcf999788922b543
Kohei Watanabe <watanabe.kohei@gmail.com>
2018-02-22 07:53:36 +0000

    Add link to package website in startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,21 @@
 .onAttach <- function(...) {
     # startup message
-    packageStartupMessage("quanteda version ", as.character(utils::packageVersion("quanteda")))
+    packageStartupMessage("Initializing package: ‘quanteda‘")
+    packageStartupMessage("Package version: ", as.character(utils::packageVersion("quanteda")))
     
     # initialize options
     quanteda_options(initialize = TRUE)
     
     # threads message
     if (qatd_cpp_tbb_enabled()) {
-        packageStartupMessage("Using ", quanteda_options("threads"), " of ", 
-                              RcppParallel::defaultNumThreads(), " threads for parallel computing")
+        packageStartupMessage("Parallel computing: ", quanteda_options("threads"), " of ", 
+                              RcppParallel::defaultNumThreads(), " threads")
     } else {
-        packageStartupMessage("Parallel computing is disabled") 
+        packageStartupMessage("Parallel computing: disabled") 
     }
+    
+    # wesite link
+    packageStartupMessage('To learn how to use ‘quanteda‘ through examples and tutorials, go to https://quanteda.io')
 }
 
 .onUnload <- function (libpath) {
