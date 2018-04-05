7672d439867d3a2b0676eaab72d86c405843e957
Kohei Watanabe <watanabe.kohei@gmail.com>
2018-04-05 09:29:44 +0100

    Skip tests for wordcloud on Linux

--- tests/testthat/test-textplot.R
+++ tests/testthat/test-textplot.R
@@ -95,6 +95,7 @@ test_that("test textplot_wordcloud works for dfm objects", {
 test_that("test textplot_wordcloud comparison works", {
     skip_on_travis()
     skip_on_cran()
+    skip_on_os('linux')
     testcorp <- corpus_reshape(corpus(data_char_sampletext))
     set.seed(1)
     docvars(testcorp, "label") <- sample(c("A", "B"), size = ndoc(testcorp), replace = TRUE)
