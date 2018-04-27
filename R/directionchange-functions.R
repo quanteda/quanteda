1f5257ff79e26775439c5460b49233909dbef469
Kohei Watanabe <watanabe.kohei@gmail.com>
2018-04-27 09:56:49 +0100

    Correct S3 methods

--- R/directionchange-functions.R
+++ R/directionchange-functions.R
@@ -1,21 +1,30 @@
+#' [Experimental] Change direction of words in tokens
+#' 
+#' This function adds Unicode direction mark to tokens types for punctuations
+#' and symbols to correct how right-to-left languages (e.g. Arabic, Hebrew,
+#' Urdu) are printed in HTML-based consoles (e.g. R Studio). This is an
+#' experimental function subjects to future changes.
+#' @param x the input object whose punctuation marks will be modified by direction mark
+#' @export
+tokens_tortl <- function(x) {
+    UseMethod("tokens_tortl")
+}
 
 #' @export
 tokens_tortl.default <- function(x) {
     stop(friendly_class_undefined_message(class(x), "tokens_tortl"))
 }
 
-#' [Experimental] Change direction of words in tokens
-#' 
-#' This function adds unicode direction mark to tokens types for punctuations
-#' and symbols to correct how right-to-left languages (e.g. Arabic, Hebrew,
-#' Urdu) are printed in HTML-based consoles (e.g. R Studio). This is an
-#' experimental function subjects to future changes.
-#' @rdname tokens_tortl
-#' @noRd
 #' @export
 tokens_tortl.tokens <- function(x) {
     attr(x, "types") <- char_tortl(types(x))
     return(x)
+}
+
+#' @rdname tokens_tortl
+#' @export
+char_tortl <- function(x) {
+    UseMethod("char_tortl")
 }
 
 #' @export
@@ -23,9 +32,7 @@ char_tortl.default <- function(x) {
     stop(friendly_class_undefined_message(class(x), "char_tortl"))
 }
 
-#' @rdname tokens_tortl
-#' @noRd
 #' @export
-char_tortl <- function(x) {
+char_tortl.character <- function(x) {
     stri_replace_all_regex(x, "([\\p{P}\\p{S}])", "$1\\u200F")
 }
--- tests/testthat/test-directionchange.R
+++ tests/testthat/test-directionchange.R
@@ -2,7 +2,12 @@ context("test direction change functions")
 
 # Note: the line below does not appear correctly in R Studio
 txt <- "מדינת ישראל נוסדה בשנת 1948."
+toks <- tokens(txt)
 
 test_that("char_tortl works.", {
     expect_equal(char_tortl(txt), "מדינת ישראל נוסדה בשנת 1948.\u200F")
+})
+
+test_that("tokens_tortl works.", {
+    expect_equal(types(tokens_tortl(toks)), types(tokens(char_tortl(txt))))
 })
