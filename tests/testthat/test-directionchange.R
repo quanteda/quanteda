32d638eafcd466bfebd9f1eed235ecae411c3dbe
Kenneth Benoit <kbenoit@lse.ac.uk>
2018-05-05 14:52:31 +0100

    Add a test mixing rtl and ltr

--- tests/testthat/test-directionchange.R
+++ tests/testthat/test-directionchange.R
@@ -1,15 +1,20 @@
+
 context("test direction change functions")
 
 # Note: the line below does not appear correctly in R Studio
-txt <- "מדינת ישראל נוסדה בשנת 1948."
-toks <- tokens(txt)
 
 test_that("char_tortl works.", {
     skip_on_os("windows")
+    txt <- "מדינת ישראל נוסדה בשנת 1948."
     expect_equal(char_tortl(txt), "מדינת ישראל נוסדה בשנת 1948.\u200F")
+    
+    txt <- "!מדינת ישר computer וסדה בשנת 1948."
+    # expect_equal(char_tortl(txt), "!מדינת ישר computer וסדה בשנת 1948.")
 })
 
 test_that("tokens_tortl works.", {
     skip_on_os("windows")
+    txt <- "מדינת ישראל נוסדה בשנת 1948."
+    toks <- tokens(txt)
     expect_equal(types(tokens_tortl(toks)), types(tokens(char_tortl(txt))))
 })
