118142f60b3cde497e7a12d3e6da77650115089f
kbenoit <kbenoit@lse.ac.uk>
2018-11-25 12:24:33 +0000

    Fix failing tests
    - the testthat() call should have been an expect_something() call
    - I think the behaviour of repeating the list names is correct, for the original tests, having no list element name ought to be repeated in the output.  I added a version of the first test where the second element is named "B" since this is what I think would match the (original) expectation.

--- tests/testthat/test-dictionaries.R
+++ tests/testthat/test-dictionaries.R
@@ -407,18 +407,22 @@ test_that("pattern2list() preserves the order of keys and values", {
 
 
 test_that("split_values() handle concatenators correctly", {
-
-  test_that(
-    quanteda:::split_values(list(A = "a_a", "b_b"), "_", " "),
-    list(A = c("a", "a"), A = "a a", B = c("b", "b"), A = "b b")
-  )
-  test_that(
-    quanteda:::split_values(list(A = "a_a", "A_A"), "_", "-"),
-    list(A = c("a", "a"), A = "a-a", A = c("A", "A"), A = "A-A")
-  )
-  test_that(
-    quanteda:::split_values(list(A = "a_a", "A_A"), " ", " "),
-    list(A = "a_a", A = "A_A")
-  )
-  
+    
+    expect_identical(
+        quanteda:::split_values(list(A = "a_a", "b_b"), "_", " "),
+        list(A = c("a", "a"), A = "a a", c("b", "b"), "b b")
+    )
+    expect_identical(
+        quanteda:::split_values(list(A = "a_a", B = "b_b"), "_", " "),
+        list(A = c("a", "a"), A = "a a", B = c("b", "b"), B = "b b")
+    )
+    expect_identical(
+        quanteda:::split_values(list(A = "a_a", "A_A"), "_", "-"),
+        list(A = c("a", "a"), A = "a-a", c("A", "A"), "A-A")
+    )
+    expect_identical(
+        quanteda:::split_values(list(A = "a_a", "A_A"), " ", " "),
+        list(A = "a_a", "A_A")
+    )
+    
 })
