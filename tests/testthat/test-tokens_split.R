2e017b7629ae1d5c566f75f09e907efac73481f2
kbenoit <kbenoit@lse.ac.uk>
2018-11-25 14:01:44 +0000

    Add a breaking test to compel fix of edge case

--- tests/testthat/test-tokens_split.R
+++ tests/testthat/test-tokens_split.R
@@ -33,5 +33,44 @@ test_that("tokens_split works in the same way as tokens", {
     )
 })
 
+test_that("tokens_split works fully when matching entire tokens to separator pattern", {
+    toks <- tokens("five-star rating", remove_hyphen = TRUE)
+    expect_identical(
+        as.character(tokens_split(toks, separator = "-", valuetype = "regex", remove_separator = FALSE)),
+        c("five", "-", "star", "rating")
+    )
+    expect_identical(
+        as.character(tokens_split(toks, separator = "-", valuetype = "regex", remove_separator = TRUE)),
+        c("five", "star", "rating")
+    )
+    expect_identical(
+        as.character(tokens_split(toks, separator = "-", valuetype = "fixed", remove_separator = TRUE)),
+        c("five", "star", "rating")
+    )
+})
 
+test_that("tokens_split works with multi-character separator", {
+    skip("Pending whether @koheiw thinks this should be a test")
+    toks <- tokens("five--star rating", remove_hyphen = TRUE)
+    expect_identical(
+        as.character(tokens_split(toks, separator = "--", remove_separator = FALSE)),
+        c("five", "--", "star", "rating")
+    )
+    expect_identical(
+        as.character(tokens_split(toks, separator = "--", remove_separator = TRUE)),
+        c("five", "star", "rating")
+    )
+})
 
+test_that("tokens_split works with regex character separator of indeterminate pattern", {
+    skip("Pending whether @koheiw thinks this should be a test")
+    toks <- tokens("abcdef")
+    expect_identical(
+        as.character(tokens_split(toks, separator = "[be]", valuetype = "regex", remove_separator = FALSE)),
+        c("a", "b", "cd", "e", "f")
+    )
+    expect_identical(
+        as.character(tokens_split(toks, separator = "[be]", valuetype = "regex", remove_separator = TRUE)),
+        c("a", "cd", "f")
+    )
+})
