5ae95876a398cc25739957b15605761b655cd33b
kbenoit <kbenoit@lse.ac.uk>
2018-12-11 22:47:29 +1100

    Update man and tests, add discard_remainder argument
    and: Add (mostly) R version for comparison

--- R/tokens_chunk.R
+++ R/tokens_chunk.R
@@ -1,21 +1,28 @@
-#' [Experimental] Chunk documents in equal sizes
+#' Segment tokens object by chunks of a given size
 #' 
-#' Segment tokens by splitting documents in equal sizes.
-#' @param x \link{tokens} object whose token elements will be segmented
-#' @param size size of each chunk
-#' @param overlap if \code{TRUE}, chunks overlaps with next chunks
-#' @return returns a \link{tokens} object whose documents are in the same length
+#' Segment tokens by splitting into equally sized chunks.
+#' @param x \link{tokens} object whose token elements will be segmented into
+#'   chunks
+#' @param size integer; the size of the chunks in tokens
+#' @param overlap logical; if \code{TRUE}, the last token of a chunk will be the
+#'   first token of the next chunk
+#' @param discard_remainder logical; if \code{TRUE}, discard any ending chunk
+#'   whose length is less than \code{size}
+#' @return A \link{tokens} object whose documents have been split into chunks of
+#'   length \code{size}, similar to \code{\link{tokens_segment}}.
 #' @keywords tokens
 #' @export
 #' @examples 
-#' txt <- "Fellow citizens, I am again called upon by the voice of my country to
-#' execute the functions of its Chief Magistrate. When the occasion proper for
-#' it shall arrive, I shall endeavor to express the high sense I entertain of
-#' this distinguished honor."
-#' toks <- tokens(txt)
-#' tokens_chunk(toks, 5)
-#' tokens_chunk(toks, 5, TRUE)
-tokens_chunk <- function(x, size, overlap = FALSE) {
+#' txts <- c(doc1 = "Fellow citizens, I am again called upon by the voice of 
+#'                   my country to execute the functions of its Chief Magistrate.",
+#'           doc2 = "When the occasion proper for it shall arrive, I shall 
+#'                   endeavor to express the high sense I entertain of this
+#'                   distinguished honor.")
+#' toks <- tokens(txts)
+#' tokens_chunk(toks, size = 5)
+#' tokens_chunk(toks, size = 5, overlap = TRUE)
+#' tokens_chunk(toks, size = 5, discard_remainder = FALSE)
+tokens_chunk <- function(x, size, overlap = FALSE, discard_remainder = TRUE) {
     UseMethod("tokens_chunk")
 }
 
@@ -23,19 +30,19 @@ tokens_chunk <- function(x, size, overlap = FALSE) {
 #' @noRd
 #' @importFrom RcppParallel RcppParallelLibs
 #' @export
-tokens_chunk.tokens <- function(x, size, overlap = FALSE) {
-    
-    if (length(size) > 1L) 
+tokens_chunk.tokens <- function(x, size, overlap = FALSE, discard_remainder = TRUE) {
+
+    if (length(size) > 1L)
         stop("Size must be a single integer")
-    if (size < 1L) 
+    if (size < 1L)
         stop("Chunk size must be larger than or equal to 1")
 
     attrs <- attributes(x)
-    type <- types(x)    
+    type <- types(x)
     result <- qatd_cpp_tokens_chunk(x, type, size, overlap)
-    
+
     # add repeated versions of remaining docvars
-    attrs$docvars <- attrs$docvars[attr(result, "docnum"),,drop = FALSE] # repeat rows
+    attrs$docvars <- attrs$docvars[attr(result, "docnum"),, drop = FALSE] # repeat rows
     if (any(duplicated(attr(result, "docnum")))) {
         docid <- paste0(names(x)[attr(result, "docnum")], ".", attr(result, "segnum"))
     } else {
@@ -44,11 +51,80 @@ tokens_chunk.tokens <- function(x, size, overlap = FALSE) {
     attrs$docvars["_document"] <- names(x)[attr(result, "docnum")]
     attrs$docvars["_docid"] <- attr(result, "docnum")
     attrs$docvars["_segid"] <- attr(result, "segnum")
-    
+
     attrs$names <- rownames(attrs$docvars) <- docid
     attr(result, "docnum") <- attr(result, "segnum") <- NULL
-    
     attributes(result) <- attrs
-    return(result)
 
+    if (discard_remainder && length(result[[ndoc(result)]]) < size) {
+        result <- result[-ndoc(result)]
+    }
+
+    result
+}
+
+#' Segment tokens object by chunks of a given size (alternative)
+#' 
+#' Segment tokens by splitting into equally sized chunks.
+#' @param x \link{tokens} object whose token elements will be segmented into
+#'   chunks
+#' @param size integer; the size of the chunks in tokens
+#' @param discard_remainder logical; if \code{TRUE}, discard any ending chunk
+#'   whose length is less than \code{size}
+#' @return A \link{tokens} object whose documents have been split into chunks,
+#'   similar to \code{\link{tokens_segment}}.
+#' @keywords tokens internal
+#' @examples 
+#' txts <- c(doc1 = "Fellow citizens, I am again called upon by the voice of 
+#'                   my country to execute the functions of its Chief Magistrate.",
+#'           doc2 = "When the occasion proper for it shall arrive, I shall 
+#'                   endeavor to express the high sense I entertain of this
+#'                   distinguished honor.")
+#' toks <- tokens(txts)
+#' tokens_chunk(toks, size = 5)
+tokens_chunk2 <- function(x, size, discard_remainder = TRUE) {
+    UseMethod("tokens_chunk")
+}
+
+#' @rdname tokens_chunk2
+tokens_chunk2.tokens <- function(x, size, discard_remainder = TRUE) {
+    attrs <- attributes(x)
+
+    # choose something very unlikely to be found
+    septoken <- "\u058E"
+    # just to be sure
+    if (septoken %in% types(x)) stop("need a different septoken!")
+
+    septoken_index <- max(unlist(unclass(x))) + 1
+    attrs$types <- c(types(x), septoken)
+
+    x <- lapply(unclass(x), insert_values,
+                n = size, val = septoken_index, truncate = discard_remainder)
+    attributes(x) <- attrs
+
+    # segment on the inserted pattern
+    ret <- tokens_segment(x, pattern = septoken, extract_pattern = TRUE)
+    # return without the extracted pattern in the docvars
+    attr(ret, "docvars")$pattern <- NULL
+    ret
+}
+
+#' Insert integers into a vector at defined intervals
+#' 
+#' Inserts \code{val} into a numeric vector \code{x} every \code{n} values.
+#' @param x a numeric vector
+#' @param n the length of the partitions of \code{x} to be divided by \code{val}
+#' @param val the numeric value to be inserted
+#' @param truncate logical; if \code{TRUE}, discard any remaining sequence less
+#'   than \code{n} at the end of the chunked tokens.
+#' max(unlist(unclass(x)))
+insert_values <- function(x, n, val, truncate = TRUE) {
+    ret <- unlist(Map(c, split(x, (seq_along(x) - 1) %/% n), list(val)), use.names = FALSE)
+    # trim the value that is always added to the end
+    ret <- head(ret, -1)
+    # truncate any short part at the end plus the last val
+    if (truncate && length(x) > n && (shortend <- length(x) %% n)) {
+        ret <- head(ret, -(shortend + 1))
+    }
+    ret
 }
--- tests/testthat/test-tokens_chunk.R
+++ tests/testthat/test-tokens_chunk.R
@@ -2,12 +2,12 @@ context("tokens_chunk works")
 
 test_that("tokens_chunk works", {
     txt <- c(d1 = "a b c d", d2 = "e f g")
-    
+
     corp <- corpus(txt, docvars = data.frame(title = c("title1", "title2"),
                                              stringsAsFactors = FALSE))
     toks <- tokens(corp)
-    
-    toks_chunk1 <- tokens_chunk(toks, 5)
+
+    toks_chunk1 <- tokens_chunk(toks, size = 5, discard_remainder = FALSE)
     expect_identical(as.list(toks_chunk1),
                      list(d1 = c("a", "b", "c", "d"),
                           d2 = c("e", "f", "g")))
@@ -19,8 +19,8 @@ test_that("tokens_chunk works", {
                                 row.names = c("d1", "d2"),
                                 stringsAsFactors = FALSE,
                                 check.names = FALSE))
-    
-    toks_chunk2 <- tokens_chunk(toks, 2)
+
+    toks_chunk2 <- tokens_chunk(toks, size = 2, discard_remainder = FALSE)
     expect_identical(as.list(toks_chunk2),
                      list(d1.1 = c("a", "b"),
                           d1.2 = c("c", "d"),
@@ -34,8 +34,8 @@ test_that("tokens_chunk works", {
                                 row.names = c("d1.1", "d1.2", "d2.1", "d2.2"),
                                 stringsAsFactors = FALSE,
                                 check.names = FALSE))
-    
-    toks_chunk3 <- tokens_chunk(toks, 2, overlap = TRUE)
+
+    toks_chunk3 <- tokens_chunk(toks, 2, overlap = TRUE, discard_remainder = FALSE)
     expect_identical(as.list(toks_chunk3),
                      list(d1.1 = c("a", "b"),
                           d1.2 = c("b", "c"),
@@ -45,17 +45,18 @@ test_that("tokens_chunk works", {
                           d2.2 = c("f", "g"),
                           d2.3 = c("g")))
     expect_identical(attr(toks_chunk3, "docvars"),
-                     data.frame("title" = c("title1", "title1", "title1", "title1", "title2", "title2", "title2"),
+                     data.frame("title" = c("title1", "title1", "title1", "title1", "title2",
+                                            "title2", "title2"),
                                 "_document" = c("d1", "d1", "d1", "d1", "d2", "d2", "d2"),
                                 "_docid" = c(1L, 1L, 1L, 1L, 2L, 2L, 2L),
                                 "_segid" = c(1L, 2L, 3L, 4L, 1L, 2L, 3L),
-                                row.names = c("d1.1", "d1.2", "d1.3", "d1.4", "d2.1", "d2.2", "d2.3"),
+                                row.names = c("d1.1", "d1.2", "d1.3", "d1.4", "d2.1", "d2.2",
+                                              "d2.3"),
                                 stringsAsFactors = FALSE,
                                 check.names = FALSE))
-    
 })
 
-test_that("tokens_chunk raies error for invalide size", {
+test_that("tokens_chunk raises error for invalid size", {
     toks <- tokens(c(d1 = "a b c d", d2 = "e f g"))
     expect_error(tokens_chunk(toks, -1),
                  "Chunk size must be larger than or equal to 1")
@@ -63,6 +64,54 @@ test_that("tokens_chunk raies error for invalide size", {
                  "Chunk size must be larger than or equal to 1")
     expect_error(tokens_chunk(toks, c(1, 3)),
                  "Size must be a single integer")
-    
-    
+})
+
+test_that("tokens_chunk works", {
+    toks <- tokens(c("a b c d e f", "a a b d c"))
+    expect_is(tokens_chunk(toks, size = 3), "tokens")
+    expect_equivalent(
+        as.list(tokens_chunk(toks, 3, discard_remainder = TRUE)),
+        list(c("a", "b", "c"), c("d", "e", "f"), c("a", "a", "b"))
+    )
+    expect_equivalent(
+        as.list(tokens_chunk(toks, 3, discard_remainder = FALSE)),
+        list(c("a", "b", "c"), c("d", "e", "f"), c("a", "a", "b"), c("d", "c"))
+    )
+    expect_identical(
+        metadoc(tokens_chunk(toks, 3)),
+        data.frame("_document" = paste0("text", c(1, 1, 2)),
+                   "_docid" = c(1L, 1L, 2L),
+                   "_segid" = c(1L, 2L, 1L),
+                   stringsAsFactors = FALSE,
+                   row.names = c("text1.1", "text1.2", "text2.1"),
+                   check.names = FALSE)
+    )
+})
+
+context("Ken's tokens_chunk() comparisons - can remove later")
+
+test_that("tokens_chunk and tokens_chunk2 are the same", {
+    toks <- tokens(data_corpus_inaugural)
+    expect_identical(
+        tokens_chunk(toks, size = 100, discard_remainder = FALSE),
+        quanteda:::tokens_chunk2(toks, size = 100, discard_remainder = FALSE)
+    )
+    expect_identical(
+        tokens_chunk(toks, size = 100, discard_remainder = TRUE),
+        quanteda:::tokens_chunk2(toks, size = 100, discard_remainder = TRUE)
+    )
+})
+
+test_that("insert_values works", {
+    expect_identical(quanteda:::insert_values(1, 3, 99), 1)
+    expect_identical(
+        quanteda:::insert_values(1:3, 3, 99, truncate = TRUE),
+        c(1, 2, 3)
+    )
+    expect_identical(quanteda:::insert_values(1:5, 3, 99, truncate = TRUE), c(1, 2, 3))
+    expect_identical(
+        quanteda:::insert_values(1:5, 3, 99, truncate = FALSE),
+        c(1:3, 99, 4:5)
+    )
+    expect_identical(quanteda:::insert_values(1:6, 3, 99), c(1:3, 99, 4:6))
 })
