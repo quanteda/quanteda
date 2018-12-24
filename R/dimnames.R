da0c80128ddd949714739ffefd8a3e3bdc0f28e8
Kohei Watanabe <watanabe.kohei@gmail.com>
2018-12-25 07:21:20 +0900

    Update man

--- R/dimnames.R
+++ R/dimnames.R
@@ -1,5 +1,13 @@
-#' Internal functions to set dimnames. Default dimnames() converts empty character
-#' vectors to NULL, leading to disfunctioning of subsetting functions
+#' Internal functions to set dimnames
+#'
+#' Default dimnames() converts a zero-length character vector to NULL, leading
+#' to disfunctioning of subsetting functions. These are safer methods to set
+#' dimnames of a dfm or fcm.
+#' @param x a dfm or fcm
+#' @param value character a vector for docnames or featmanes or a list of them
+#'   for dimnames
+#' @keywords internal
+#' @rdname set_dfm_dimnames
 "set_dfm_dimnames<-" <- function(x, value) {
     if (is.null(value[[1]])) value[[1]] <- character()
     if (is.null(value[[2]])) value[[2]] <- character()
@@ -9,12 +17,14 @@
     return(x)
 }
 
+#' @rdname set_dfm_dimnames
 "set_dfm_docnames<-" <- function(x, value) {
     stopifnot(nrow(x) == length(value))
     x@Dimnames[[1]] <- value
     return(x)
 }
 
+#' @rdname set_dfm_dimnames
 "set_dfm_featnames<-" <- function(x, value) {
     if (is.null(value)) value <- character()
     stopifnot(ncol(x) == length(value))
@@ -22,6 +32,7 @@
     return(x)
 }
 
+#' @rdname set_dfm_dimnames
 "set_fcm_dimnames<-" <- function(x, value) {
     if (is.null(value[[1]])) value[[1]] <- character()
     if (is.null(value[[2]])) value[[2]] <- character()
@@ -31,6 +42,7 @@
     return(x)
 }
 
+#' @rdname set_dfm_dimnames
 "set_fcm_featnames<-" <- function(x, value) {
     if (is.null(value)) value <- character()
     stopifnot(nrow(x) == length(value))
