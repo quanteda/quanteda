c0efeb50beb4f76658bce8c1a1b20fc199ccd37c
Kohei Watanabe <watanabe.kohei@gmail.com>
2018-12-27 15:40:56 +0900

    Update benchmarks

--- tests/benchmarks/benchmark_tokens/tokenize.R
+++ tests/benchmarks/benchmark_tokens/tokenize.R
@@ -4,26 +4,23 @@ library(tokenizers)
 corp <- readRDS("/home/kohei/Documents/Brexit/Data/data_corpus_guardian.RDS")
 
 system.time(
-    tokens(corp, what='word', verbose = TRUE)
+    tokens(corp, what = 'word', verbose = TRUE)
 )
 
 system.time(
-    tokens(corp, what='fastestword', verbose = TRUE)
+    tokens(corp, what = 'fastestword', verbose = TRUE)
 )
-
-
 
 txt <- rep(paste0(letters, collapse=' '), 10000)
 
 microbenchmark::microbenchmark(
     tokenizers::tokenize_words(txt),
-    tokens(txt, what='word', hash = TRUE),
-    tokens(txt, what='word', hash = FALSE),
-    unit='relative'
+    tokens(txt, what = 'word'),
+    unit = 'relative'
 )
 
 profvis(
-  tokens(txt, what='fastestword')
+  tokens(txt, what = 'fastestword')
 )
 
 
