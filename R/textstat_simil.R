44c8bd0cf9aaaacaa99dc0a9339ea1a9be8f4f7c
Kohei Watanabe <watanabe.kohei@gmail.com>
2019-01-03 11:49:15 +0900

    Add utility functions to check uniform or zero columns

--- R/textstat_simil.R
+++ R/textstat_simil.R
@@ -292,9 +292,16 @@ textstat_proxy <- function(x, y = NULL,
     }
     dimnames(result) <- list(colnames(x), colnames(y))
     if (use_na) {
-        l <- (colSums(x != 0) == 0) | (colSums(y != 0) == 0)
-        result[l,,drop = FALSE] <- NA
-        result[,l,drop = FALSE] <- NA
+        na1 <- na2 <- logical()
+        if (method == "cosine") {
+            na1 <- qatd_cpp_nz(x) == 0
+            na2 <- qatd_cpp_nz(y) == 0
+        } else if (method == "correlation") {
+            na1 <- quanteda:::qatd_cpp_stddev(x) == 0
+            na2 <- quanteda:::qatd_cpp_stddev(y) == 0
+        }
+        result[na1,,drop = FALSE] <- rep(NA, length(na1))
+        result[,na2,drop = FALSE] <- rep(NA, length(na2))
     }
     return(result)
     # return(as(result, "CsparseMatrix"))
--- src/simil_mt.cpp
+++ src/simil_mt.cpp
@@ -23,6 +23,15 @@ double get_limit(std::vector<double> simils, const unsigned int rank, double lim
     return limit;
 }
 
+rowvec nz(const sp_mat& mt) {
+    rowvec v(mt.n_cols, fill::zeros);
+    if (mt.is_empty()) return(v);
+    for (uword i = 0; i < mt.n_cols; i++) {
+        v[i] = sum(colvec(mt.col(i)) != 0);
+    }
+    return(v);
+}
+
 rowvec stddev(const sp_mat& mt, const int norm_type) {
     rowvec v(mt.n_cols, fill::zeros);
     if (mt.is_empty()) return(v);
@@ -332,3 +341,16 @@ S4 qatd_cpp_similarity(arma::sp_mat& mt1,
     return to_matrix(simil_tri, ncol1, ncol2, symm); 
 
 }
+
+// [[Rcpp::export]]
+NumericVector qatd_cpp_stddev(arma::sp_mat& mt) {
+    std::vector<double> sds = to_vector(stddev(mt, 1));
+    return wrap(sds);
+}
+
+// [[Rcpp::export]]
+NumericVector qatd_cpp_nz(arma::sp_mat& mt) {
+    std::vector<double> nzs = to_vector(nz(mt));
+    return wrap(nzs);
+}
+
