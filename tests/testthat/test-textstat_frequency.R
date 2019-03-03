3adbb3c5927e07c7dfa4582e62b9aa1e1b19c197
kbenoit <kbenoit@lse.ac.uk>
2019-03-03 12:40:25 +1100

    Set seed or specify ties method for tests

--- tests/testthat/test-textstat_frequency.R
+++ tests/testthat/test-textstat_frequency.R
@@ -3,7 +3,7 @@ context("test textstat_frequency")
 test_that("test textstat_frequency without groups", {
     dfm1 <- dfm(c("a a b b c d", "a d d d", "a a a"))
     expect_equivalent(
-        textstat_frequency(dfm1),
+        textstat_frequency(dfm1, ties.method = "random"),
         data.frame(feature = c("a", "d", "b", "c"),
                    frequency = c(6,4,2,1),
                    rank = 1:4,
@@ -12,7 +12,7 @@ test_that("test textstat_frequency without groups", {
                    stringsAsFactors = FALSE)
     )
     expect_equivalent(
-      textstat_frequency(dfm1, n = 2),
+      textstat_frequency(dfm1, n = 2, ties.method = "random"),
       data.frame(feature = c("a", "d", "b", "c"),
                  frequency = c(6,4,2,1),
                  rank = 1:4,
@@ -28,13 +28,14 @@ test_that("test textstat_frequency without groups", {
     grp1 <- c("one", "two", "one")
     corp1 <- corpus(txt, docvars = data.frame(grp2 = grp1))
     
-    expect_equivalent(
-        textstat_frequency(dfm(corp1), groups = grp1),
-        textstat_frequency(dfm(corp1), groups = "grp2")
+    expect_identical(
+        textstat_frequency(dfm(corp1), groups = grp1, ties.method = "dense"),
+        textstat_frequency(dfm(corp1), groups = "grp2", ties.method = "dense")
     )
 
+    set.seed(10)
     expect_equivalent(
-        textstat_frequency(dfm(corp1), groups = grp1),
+        textstat_frequency(dfm(corp1), groups = grp1, ties.method = "random"),
         data.frame(feature = c("a", "b", "c", "d", "d", "a"),
                    frequency = c(5,2,1,1,3,1),
                    rank = c(1:4, 1:2),
@@ -44,11 +45,11 @@ test_that("test textstat_frequency without groups", {
     )
     
     expect_equivalent(
-      textstat_frequency(dfm(corp1), groups = grp1, n = 2),
+      textstat_frequency(dfm(corp1), groups = grp1, n = 2, ties.method = "random"),
       data.frame(feature = c("a", "b", "d", "a"),
-                 frequency = c(5,2,3,1),
+                 frequency = c(5, 2, 3, 1),
                  rank = c(1:2, 1:2),
-                 docfreq = c(2,1,1,1),
+                 docfreq = c(2, 1, 1, 1),
                  group = c("one", "one", "two", "two"),
                  stringsAsFactors = FALSE)
     )
@@ -63,8 +64,9 @@ test_that("test textstat_frequency works with weights", {
     dfm1 <- dfm(corp1)
     dfm1weighted <- dfm_weight(dfm1, "prop")
     
+    set.seed(10)
     expect_equivalent(
-        textstat_frequency(dfm1weighted),
+        textstat_frequency(dfm1weighted, ties.method = "random"),
         data.frame(feature = c("a", "d", "b", "c"),
                    frequency = c(1.58, .916, .333, .1666),
                    rank = 1:4,
