e23aaaee95096c7501de1ea7817bf5e3d79c328e
Kohei Watanabe <watanabe.kohei@gmail.com>
2019-06-30 11:13:22 +0200

    Disable deprecation message as it fails too many tests

--- R/dfm_select.R
+++ R/dfm_select.R
@@ -103,9 +103,9 @@ dfm_select.dfm <-  function(x, pattern = NULL,
             id_pat <- integer()
         }
     } else {
-        .Deprecated(msg = "pattern = dfm is deprecated; use dfm_match() instead")
         # special handling if pattern is a dfm
         if (is.dfm(pattern)) {
+            #.Deprecated(msg = "pattern = dfm is deprecated; use dfm_match() instead")
             pattern <- featnames(pattern)
             valuetype <- "fixed"
             case_insensitive <- FALSE
