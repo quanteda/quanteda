51c39501488a4742ac1bcceea6ae436caf22d84b
kbenoit <kbenoit@lse.ac.uk>
2019-12-03 09:27:49 +0000

    Set up tests for v2 core index operators

---
+++ tests/testthat/test-operators-for-core-objects.R
@@ -0,0 +1,97 @@
+context("test v2 core object operators")
+
+test_that("corpus core operators work for v2", {
+    corp <- corpus_subset(data_corpus_inaugural, Year <= 1805)
+    
+    expect_is(corp["1793-Washington"], "corpus")
+    expect_identical(corp[2], 
+                     corp["1793-Washington"])
+    expect_identical(corp[c(FALSE, TRUE, FALSE, FALSE, FALSE)], 
+                     corp["1793-Washington"])
+    
+    expect_identical(corp[[2]], 
+                     corp[["1793-Washington"]])
+    expect_identical(corp[c(FALSE, TRUE, FALSE, FALSE, FALSE)][[1]], 
+                     corp[["1793-Washington"]])
+    
+    expect_is(corp$Year, "numeric")
+    expect_identical(
+        corp$Year,
+        docvars(corp, "Year")
+    )
+})
+
+test_that("tokens core operators work for v2", {
+    toks <- tokens(corp)[1:5]
+    
+    expect_is(toks["1793-Washington"], "tokens")
+    expect_identical(toks[2], toks["1793-Washington"])
+    expect_identical(toks[c(FALSE, TRUE, FALSE, FALSE, FALSE)], 
+                     toks["1793-Washington"])
+
+    expect_is(toks[["1793-Washington"]], "character")
+    expect_identical(toks[[2]], toks[["1793-Washington"]])
+    expect_identical(toks[c(FALSE, TRUE, FALSE, FALSE, FALSE)][[1]], 
+                     toks[["1793-Washington"]])
+    
+    expect_is(toks$Year, "numeric")
+    expect_identical(
+        toks$Year,
+        docvars(toks, "Year")
+    )
+})
+
+test_that("dfm core operators work for v2", {
+    dfmat <- dfm(toks)
+    expect_is(dfmat["1793-Washington"], NA)
+    expect_identical(
+        dfmat["1793-Washington"],
+        as.matrix(dfmat)["1793-Washington"]
+    )
+    expect_error(
+        dfmat[["1793-Washington"]], 
+        "[[ undefined for dfm objects"
+    )
+    expect_is(dfmat$Year, "numeric")
+    expect_identical(
+        dfmat$Year,
+        docvars(dfmat, "Year")
+    )
+})
+
+test_that("fcm core operators work for v2", {
+    fcmat <- fcm(dfm_trim(dfmat, min_termfreq = 20))
+    expect_is(fcmat[c("the", "and")], c(NA, NA))
+    expect_identical(
+        fcmat[c("the", "and")],
+        as.matrix(fcmat)[c("the", "and")]
+    )
+    expect_error(
+        fcmat[["the"]], 
+        "[[ undefined for fcm objects"
+    )
+    expect_error(
+        fcmat$Year,
+        "$ undefined for fcm objects"
+    )
+})
+
+test_that("dictionary core operators work for v2", {
+    dict <- dictionary(list(christmas = c("Christmas", "Santa", "holiday"),
+                            opposition = c("Opposition", "reject", "notincorpus")),
+                       tolower = FALSE)
+    
+    expect_identical(
+        dict$opposition,
+        c("Opposition", "reject", "notincorpus")
+    )
+    expect_identical(
+        dict[["opposition"]],
+        dict$opposition
+    )
+    expect_identical(
+        dict["opposition"],
+        dictionary(list(opposition = c("Opposition", "reject", "notincorpus")),
+                       tolower = FALSE)
+    )
+})
