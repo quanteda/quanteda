23046888fbc957e2501e1fb3d9ad5e3edebd1035
Kohei Watanabe <watanabe.kohei@gmail.com>
2020-01-08 14:49:27 +0100

    Fix test for summary = FALSE and short documents

--- tests/testthat/test-corpus.R
+++ tests/testthat/test-corpus.R
@@ -493,35 +493,34 @@ test_that("corpus printing works with new textual summary", {
     )
     expect_output(
         print(data_corpus_irishbudget2010, ndoc = 0, nchar = 0, show.summary = TRUE),
-        "Corpus consisting of 14 documents and 6 docvars.", fixed = TRUE
+        "Corpus consisting of 14 documents and 6 docvars.", 
+        fixed = TRUE
     )
     expect_output(
         print(data_corpus_irishbudget2010, ndoc = 2, nchar = 10, show.summary = TRUE),
-        "Corpus consisting of 14 documents and 6 docvars.
-  [Lenihan, Brian (FF)] When I pre ...
- [Bruton, Richard (FG)] This draco ...
-and 12 more documents.", fixed = TRUE
+        "Corpus consisting of 14 documents and 6 docvars.\n [Lenihan, Brian (FF)] When I pre ...\n[Bruton, Richard (FG)] This draco ...\nand 12 more documents.", 
+        fixed = TRUE
     )
     expect_output(
         print(data_corpus_irishbudget2010, ndoc = 2, nchar = 10, show.summary = FALSE),
-        "  [Lenihan, Brian (FF)] When I pre ...
- [Bruton, Richard (FG)] This draco ...
-and 12 more documents.", fixed = TRUE
+        " [Lenihan, Brian (FF)] When I pre ...\n[Bruton, Richard (FG)] This draco ...", 
+        fixed = TRUE
     )
     expect_output(
         print(data_corpus_irishbudget2010[1:2], ndoc = 3, nchar = 10, show.summary = TRUE),
-        "Corpus consisting of 2 documents and 6 docvars.
-  [Lenihan, Brian (FF)] When I pre ...
- [Bruton, Richard (FG)] This draco ...", fixed = TRUE
+        "Corpus consisting of 2 documents and 6 docvars.\n [Lenihan, Brian (FF)] When I pre ...\n[Bruton, Richard (FG)] This draco ...", 
+        fixed = TRUE
     )
     expect_output(
         print(corpus("a b c d"), ndoc = -1, nchar = 2),
-        "Corpus consisting of 1 document.
- [text1] a  ...", fixed = TRUE
+        "Corpus consisting of 1 document.\n[text1] a  ...", fixed = TRUE
+    )
+    expect_output(
+      print(corpus("a b c d"), ndoc = -1, nchar = 10),
+      "Corpus consisting of 1 document.\n[text1] a b c d", fixed = TRUE
     )
     expect_output(
         print(corpus("a b c d"), ndoc = -1, nchar = -1),
-        "Corpus consisting of 1 document.
- [text1] a b c d ...", fixed = TRUE
+        "Corpus consisting of 1 document.\n[text1] a b c d", fixed = TRUE
     )
 })
