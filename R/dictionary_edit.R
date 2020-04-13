0529bb2883a0ff01c234718ff5f0729433623006
Kenneth Benoit <kbenoit@lse.ac.uk>
2020-04-13 16:09:59 +0100

    Add choice of editor (via ... to edit())

--- R/dictionary_edit.R
+++ R/dictionary_edit.R
@@ -2,27 +2,29 @@
 #'
 #' Provides convenient editing of dictionaries, using an interactive editor.
 #' @param x a [dictionary] or (list of) character elements
+#' @param ... (optional) arguments passed to [utils::edit()] (such as the choice
+#'   of editor)
 #' @return an edited version of the input object
 #' @export
 #' @examples
+#' # edit the positive and negative entries from the LSD2015
 #' \dontrun{
-#' # edit some supplied stopwords
-#' my_posneg_dict <- dictionary_edit(data_dictionary_LSD2015[1:2])
-#' }
-dictionary_edit <- function(x) {
+#' my_posneg_dict <- dictionary_edit(data_dictionary_LSD2015[1:2])}
+#' 
+dictionary_edit <- function(x, ...) {
     UseMethod("dictionary_edit")
 }
 
 #' @export
-dictionary_edit.default <- function(x) {
+dictionary_edit.default <- function(x, ...) {
     stop(friendly_class_undefined_message(class(x), "dictionary_edit"))
 }
 
 #' @export
-dictionary_edit.dictionary2 <- function(x) {
+dictionary_edit.dictionary2 <- function(x, ...) {
     x <- as.dictionary(x)
     attrs <- attributes(x)
-    x <- build_dictionary2(list_edit(as.list(x)))
+    x <- build_dictionary2(list_edit(as.list(x), ...))
     x <- rebuild_dictionary2(x, attrs)
     title <- meta(x, "title")
     if (!is.null(title)) meta(x, "title") <- paste(title, "(edited)")
@@ -37,29 +39,27 @@ dictionary_edit.dictionary2 <- function(x) {
 #' @importFrom yaml write_yaml read_yaml
 #' @importFrom utils edit
 #' @export
-list_edit <- function(x) {
+list_edit <- function(x, ...) {
     tfile <- tempfile(fileext = ".yml")
     write_yaml(x, file = tfile)
-    try(edited <- utils::edit(file = tfile), silent = TRUE)
+    try(edited <- utils::edit(file = tfile, ...), silent = TRUE)
     read_yaml(tfile)
 }
 
 #' @rdname dictionary_edit
-#' @param sep a character vector of strings to append after each element,
-#'   defaults to `"\n"` so that each word will be on its own line in the editor
 #' @export
 #' @importFrom stringi stri_write_lines
 #' @examples
+#' # edit the system stopwords
 #' \dontrun{
 #' my_stopwords <- stopwords("en", source = "snowball") %>%
-#'     char_edit()
-#' }
-char_edit <- function(x, sep = "\n") {
+#'     char_edit()}
+char_edit <- function(x, ...) {
     if (!is.vector(x) || !is.character(x))
         stop("x must be a character vector")
     tfile <- tempfile(fileext = ".txt")
     stri_write_lines(x, fname = tfile)
-    try(edited <- utils::edit(file = tfile), silent = TRUE)
+    try(edited <- utils::edit(file = tfile, ...), silent = TRUE)
     result <- readLines(tfile, encoding = "UTF-8")
     if (result[length(result)] == "")
         result <- result[-length(result)]
