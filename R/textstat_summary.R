c5ea195c6ce1cc047a565fdbb8dbfa213dc83165
Kohei Watanabe <watanabe.kohei@gmail.com>
2020-05-19 16:49:08 +0200

    Add documentation and tests

--- R/textstat_summary.R
+++ R/textstat_summary.R
@@ -1,9 +1,21 @@
-#' Summarize a corpus
+#' Summarize documents
 #'
-#' Displays information about a corpus, including attributes and metadata such
-#' as date of number of texts, creation and source.
+#' Count the total number of number tokens and sentences.
 #'
-#' @param object corpus to be summarized
+#' Count the total number tokens and sentences as well as
+#' tokens with numbers, punctuation marks or symbols.
+#' \itemize{
+#'   \item n_token = number of tokens; equal to [ntoken()]
+#'   \item n_type = number of unique tokens; equal to [ntype()]
+#'   \item n_sent = number of sentences; equal `ntoken(tokens(x), what = "sentence")`
+#'   \item number = number of tokens with numbers (`\p{N}`)
+#'   \item punct = number of tokens with numbers (`\p{P}`)
+#'   \item symbol = number of tokens with numbers (`\p{S}`)
+#'   \item any = number of tokens with numbers (`[\p{N}\p{P}\p{S}]`)
+#'   \item noise = \eqn{any / n_token}
+#'   \item is_dup = existence of identical documents
+#' }
+#' @param x corpus to be summarized
 #' @param cache if `TRUE`, use internal cache from the second time
 #' @param ... additional arguments passed through to [dfm()]
 #' @export
@@ -16,25 +28,30 @@
 #' dfmat <- dfm(toks)
 #' textstat_summary(dfmat, cache = TRUE)
 textstat_summary <- function(x, cache = TRUE, ...) {
-    UseMethod("textstat_readability")
+    UseMethod("textstat_summary")
+}
+
+#' @export
+textstat_summary.default <- function(x, cache = TRUE, ...) {
+    stop(friendly_class_undefined_message(class(x), "textstat_summary"))
 }
 
 #' @method textstat_summary corpus
 #' @export
-textstat_summary.corpus <- function(object, cache = TRUE, ...) {
-    summarize(as.corpus(object), cache, ...)
+textstat_summary.corpus <- function(x, cache = TRUE, ...) {
+    summarize(as.corpus(x), cache, ...)
 }
 
 #' @method textstat_summary tokens
 #' @export
-textstat_summary.tokens <- function(object, cache = TRUE, ...) {
-    summarize(as.tokens(object), cache, ...)
+textstat_summary.tokens <- function(x, cache = TRUE, ...) {
+    summarize(as.tokens(x), cache, ...)
 }
 
 #' @method textstat_summary dfm
 #' @export
-textstat_summary.dfm <- function(object, cache = TRUE, ...) {
-    summarize(as.dfm(object), cache, ...)
+textstat_summary.dfm <- function(x, cache = TRUE, ...) {
+    summarize(as.dfm(x), cache, ...)
 }
 
 summarize <- function(x, cache = TRUE, ...) {
@@ -52,13 +69,15 @@ summarize <- function(x, cache = TRUE, ...) {
         "symbol" = "\\p{S}",
         "any" = "[\\p{N}\\p{P}\\p{S}]"
     ))
+    
     temp <- dfm(x, ...)
     result <- convert(
         dfm_lookup(temp, dictionary = dict, valuetype = "regex"),
-        "data.frame"
+        "data.frame",
+        docid_field = "document"
     )
     result$n_token <- ntoken(temp)
-    result$n_type <- nfeat(temp) 
+    result$n_type <- ntype(temp)
     result$noise <- result$any / result$n_token
     result$n_sent <- NA
     result$is_dup <- NA
@@ -67,7 +86,8 @@ summarize <- function(x, cache = TRUE, ...) {
         result$n_sent <- ntoken(tokens(x, what = "sentence"))
     if (is.corpus(x) || is.tokens(x))
         result$is_dup <- duplicated(x)
-    
+    result <- result[c("document", "n_token", "n_type", "n_sent",
+                       names(dict), "noise", "is_dup")]
     if (cache)
         set_cache(x, "summary", result, ...)
     return(result)
--- tests/testthat/test-textstat_summary.R
+++ tests/testthat/test-textstat_summary.R
@@ -1,19 +1,11 @@
 test_that("textstat_summary method works", {
     
-    corp <- data_corpus_inaugural
+    corp <- data_corpus_inaugural[10:15]
     toks <- tokens(corp)
     dfmt <- dfm(toks)
     
     # corpus
     summ_corp <- textstat_summary(corp, cache = FALSE)
-    expect_equal(
-        summ_corp$punct,
-        unname(ntoken(dfm_select(dfmt, "[\\p{P}]", valuetype = "regex")))
-    )
-    expect_equal(
-        summ_corp$number,
-        unname(ntoken(dfm_select(dfmt, "[\\p{N}]", valuetype = "regex")))
-    )
     expect_equal(
         summ_corp$n_sent,
         unname(ntoken(tokens(corp, what = "sentence")))
@@ -22,16 +14,29 @@ test_that("textstat_summary method works", {
         summ_corp$is_dup,
         rep(FALSE, ndoc(corp))
     )
+    expect_equal(
+        names(summ_corp),
+        c("document", "n_token", "n_type", "n_sent", "number", 
+          "punct", "symbol", "any", "noise", "is_dup")
+    )
     
     # tokens
     summ_toks <- textstat_summary(toks, cache = FALSE)
     expect_equal(
         summ_toks$punct,
-        unname(ntoken(dfm_select(dfmt, "[\\p{P}]", valuetype = "regex")))
+        unname(ntoken(tokens_select(toks, "[\\p{P}]", valuetype = "regex")))
     )
     expect_equal(
         summ_toks$number,
-        unname(ntoken(dfm_select(dfmt, "[\\p{N}]", valuetype = "regex")))
+        unname(ntoken(tokens_select(toks, "[\\p{N}]", valuetype = "regex")))
+    )
+    expect_equal(
+        summ_toks$n_token,
+        unname(ntoken(toks))
+    )
+    expect_equal(
+        summ_toks$n_type,
+        unname(ntype(toks))
     )
     expect_equal(
         summ_toks$n_sent,
@@ -40,6 +45,11 @@ test_that("textstat_summary method works", {
     expect_equal(
         summ_toks$is_dup,
         rep(FALSE, ndoc(toks))
+    )
+    expect_equal(
+        names(summ_toks),
+        c("document", "n_token", "n_type", "n_sent", "number", 
+          "punct", "symbol", "any", "noise", "is_dup")
     )
     
     # dfm
@@ -53,6 +63,14 @@ test_that("textstat_summary method works", {
         unname(ntoken(dfm_select(dfmt, "[\\p{N}]", valuetype = "regex")))
     )
     expect_equal(
+        summ_dfm$n_token,
+        unname(ntoken(dfmt))
+    )
+    expect_equal(
+        summ_dfm$n_type,
+        unname(ntype(dfmt))
+    )
+    expect_equal(
         summ_dfm$n_sent,
         rep(NA, ndoc(dfmt))
     )
@@ -60,39 +78,43 @@ test_that("textstat_summary method works", {
         summ_dfm$is_dup,
         rep(NA, ndoc(dfmt))
     )
-    
+    expect_equal(
+        names(summ_dfm),
+        c("document", "n_token", "n_type", "n_sent", "number", 
+          "punct", "symbol", "any", "noise", "is_dup")
+    )
 })
 
 
 test_that("textstat_summary chaching is working", {
     
-    corp <- data_corpus_inaugural
+    corp <- data_corpus_inaugural[10:15]
     summ_corp <- textstat_summary(corp, cache = TRUE)
     expect_identical(meta(corp, type = "object")$summary$data, summ_corp)
     summ_corp <- textstat_summary(corp, cache = FALSE)
     quanteda:::clear_cache(corp, "summary")
     expect_identical(meta(corp, type = "object")$summary, list())
-    expect_equal(nrow(textstat_summary(head(corp, 10), cache = TRUE)), 10)
+    expect_equal(nrow(textstat_summary(head(corp, 3), cache = TRUE)), 3)
     
     toks <- tokens(corp)
     summ_toks <- textstat_summary(toks, cache = TRUE)
     expect_identical(meta(toks, type = "object")$summary$data, summ_toks)
     summ_toks <- textstat_summary(toks, cache = FALSE)
     expect_identical(meta(toks, type = "object")$summary, list())
-    expect_equal(nrow(textstat_summary(head(toks, 10), cache = TRUE)), 10)
+    expect_equal(nrow(textstat_summary(head(toks, 3), cache = TRUE)), 3)
     
     dfmt <- dfm(toks)
     summ_dfm <- textstat_summary(dfmt, cache = TRUE)
     expect_identical(meta(dfmt, type = "object")$summary$data, summ_dfm)
     summ_dfm <- textstat_summary(dfmt, cache = FALSE)
     expect_identical(meta(dfmt, type = "object")$summary, list())
-    expect_equal(nrow(textstat_summary(head(dfmt, 10), cache = TRUE)), 10)
+    expect_equal(nrow(textstat_summary(head(dfmt, 3), cache = TRUE)), 3)
 })
 
 
 test_that("summary chache is updated", {
     
-    corp <- data_corpus_inaugural[1:10]
+    corp <- data_corpus_inaugural[10:15]
     summ_corp1 <- textstat_summary(corp, cache = TRUE, tolower = FALSE, remove_punct = FALSE)
     summ_corp2 <- textstat_summary(corp, cache = TRUE, tolower = TRUE, remove_punct = FALSE)
     summ_corp3 <- textstat_summary(corp, cache = TRUE, tolower = TRUE, remove_punct = TRUE)
