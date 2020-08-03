3eabd78ec9307c1ee30c8476e9f69eb2aba6f38d
Kohei Watanabe <watanabe.kohei@gmail.com>
2020-08-03 17:18:40 +0200

    Restore stopwords

---
+++ R/stopwords.R
@@ -0,0 +1,7 @@
+#' @section Stopwords: 
+#' 
+#' Stopword lists were formerly built into \pkg{quanteda}, but have been moved to the 
+#' \pkg{stopwords} package. See [stopwords::stopwords()].
+#' @importFrom stopwords stopwords
+#' @export 
+stopwords::stopwords
