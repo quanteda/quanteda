203260cf7249aada45ccfa595708a94cbf44fb73
Kenneth Benoit <kbenoit@lse.ac.uk>
2020-11-28 17:41:01 +0000

    Retain class definitions for textstat_proxy
    This is a temporary measure that allows quanteda.textstats to work with both the modularised package that contains none of the textstat functions, as well as quanteda v2.1.2.  This way, we can get quanteda.textstats on CRAN and update the stripped out quanteda package later.

--- R/textstat.R
+++ R/textstat.R
@@ -16,3 +16,68 @@ NULL
 is.collocations <- function(x) {
     "collocations" %in% class(x)
 }
+
+# class definitions and class functions for textstat_proxy --------
+
+# transitional only, to get quanteda.textstats passing on CRAN
+
+#' @title textstat_simil/dist classes
+#' @description Sparse classes for similarity and distance matrices created by
+#'   [textstat_simil()] and [textstat_dist()].
+#' @rdname textstat_proxy-class
+#' @export
+#' @keywords internal textstat
+#' @slot .Data a sparse \pkg{Matrix} object, symmetric if selection is
+#'   `NULL`
+#' @slot method the method used for computing similarity or distance
+#' @slot min_simil numeric; a threshold for the similarity values below which similarity
+#'   values are not computed
+#' @slot margin identifies the margin of the dfm on which similarity or
+#'   difference was computed:  `"documents"` for documents or
+#'   `"features"` for word/term features.
+#' @slot type either `"textstat_simil"` or `"textstat_dist"`
+#' @seealso [textstat_simil()]
+setClass("textstat_proxy", contains = "Matrix",
+         slots = c(method = "character",
+                   margin = "character",
+                   type = "character"))
+
+#' @rdname textstat_proxy-class
+#' @slot selection target units, if any
+setClass("textstat_dist", contains = c("textstat_proxy", "dgeMatrix"))
+
+#' @rdname textstat_proxy-class
+setClass("textstat_dist_symm", contains = c("textstat_proxy", "dspMatrix"))
+
+#' @rdname textstat_proxy-class
+setClass("textstat_simil", contains = c("textstat_proxy", "dgeMatrix"))
+
+#' @rdname textstat_proxy-class
+#' @export
+setClass("textstat_simil_symm", contains = c("textstat_proxy", "dspMatrix"))
+
+#' @rdname textstat_proxy-class
+#' @export
+setClass("textstat_simil_sparse", contains = c("textstat_proxy", "dgTMatrix"),
+         slots = c(min_simil = "numeric"))
+
+#' @rdname textstat_proxy-class
+setClass("textstat_simil_symm_sparse", contains = c("textstat_proxy", "dsTMatrix"),
+         slots = c(min_simil = "numeric"))
+
+#' @rdname textstat_proxy-class
+validate_min_simil <- function(object) {
+    if (object@min_simil < -1.0 || object@min_simil > 1.0) {
+        paste("min_simil must range from -1.0 to 1.0")
+    } else {
+        return(TRUE)
+    }
+}
+
+setValidity("textstat_simil_sparse", function(object) {
+    validate_min_simil(object)
+})
+
+setValidity("textstat_simil_symm_sparse", function(object) {
+    validate_min_simil(object)
+})
