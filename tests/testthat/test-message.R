94e5594840069bcc7ffd8af7a09d9eb99c1091d8
Kohei Watanabe <watanabe.kohei@gmail.com>
2020-12-24 19:39:07 +0900

    Add tests

---
+++ tests/testthat/test-message.R
@@ -0,0 +1,67 @@
+context("test messaging functions")
+
+test_that("info works", {
+    
+    # logical indices
+    expect_message(
+        quanteda:::info("there is %s", c("a document", "documents"), indices = FALSE),
+        "there is a document"
+    )
+    expect_message(
+        quanteda:::info("there are %s", c("a document", "documents"), indices = TRUE),
+        "there are documents"
+    )
+    
+    # numeric indices
+    expect_message(
+        quanteda:::info("there is %s", c("a document", "documents"), indices = 1),
+        "there is a document"
+    )
+    expect_message(
+        quanteda:::info("there are %s", c("a document", "documents"), indices = 2),
+        "there are documents"
+    )
+    
+    # multiple numeric indices
+    expect_message(
+        quanteda:::info("there %s %s", 
+                        list(c("isn't", "is", "are"), c("a document", "documents")), 
+                        indices = list(2, 1)),
+        "there is a document"
+    )
+    expect_message(
+        quanteda:::info("there %s %s", 
+                        list(c("isn't", "is", "are"), c("a document", "documents")), 
+                        indices = list(3, 2)),
+        "there are documents"
+    )
+    expect_message(
+        quanteda:::info("there %s %s", 
+                        list(c("isn't", "is", "are"), c("a document", "documents")), 
+                        indices = c(3, 2)),
+        "there are documents"
+    )
+    
+    # mixed indices types
+    expect_message(
+        quanteda:::info("there %s %s", 
+                        list(c("isn't", "is", "are"), c("a document", "documents")), 
+                        indices = list(2, FALSE)),
+        "there is a document"
+    )
+    expect_message(
+        quanteda:::info("there %s %s", 
+                        list(c("isn't", "is", "are"), c("a document", "documents")), 
+                        indices = list(3, TRUE)),
+        "there are documents"
+    )
+    
+    # warning
+    expect_warning(
+        quanteda:::info("there %s %s", 
+                        list(c("isn't", "is", "are"), c("a document", "documents")), 
+                        indices = list(1, 1), warn = TRUE),
+        "there isn't a document"
+    )
+    
+})
