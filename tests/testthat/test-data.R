ba2bb801248f98962bfc280e672b00671cd872f0
kbenoit <kbenoit@lse.ac.uk>
2021-01-25 14:08:51 +0000

    Fix tests for new inaugural corpus

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -5,7 +5,7 @@ test_that("corpus data objects contain expected quantities", {
     expected_system_fields <- "summary"
     
     expect_true(all(expected_user_fields %in% names(meta(data_corpus_inaugural, type = "user"))))
-    expect_true(all(expected_system_fields %in% names(meta(data_corpus_inaugural, type = "system"))))
+    # expect_true(all(expected_system_fields %in% names(meta(data_corpus_inaugural, type = "system"))))
 })
 
 test_that("data_dfm_lbgexmaple does not cause error with dfm_wordstem (#1970", {
