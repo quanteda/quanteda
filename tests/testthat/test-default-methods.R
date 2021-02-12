ccc22dbeb36365cedb509f0ababba628e437f60b
kbenoit <kbenoit@lse.ac.uk>
2021-02-12 21:28:41 +0000

    Remove casechange functions

--- tests/testthat/test-default-methods.R
+++ tests/testthat/test-default-methods.R
@@ -350,20 +350,6 @@ test_that("test new docnames<- methods", {
     )
 })
 
-test_that("friendly_class_undefined_message for tokens_tortl()", {
-    expect_error(
-        tokens_tortl(data_dfm_lbgexample),
-        "tokens_tortl\\(\\) only works on.*tokens.*objects"
-    )
-})
-
-test_that("friendly_class_undefined_message for char_tortl()", {
-    expect_error(
-        char_tortl(data_dfm_lbgexample),
-        "char_tortl\\(\\) only works on.*character.*objects"
-    )
-})
-
 test_that("friendly_class_undefined_message for featfreq()", {
     expect_error(
         featfreq(tokens(data_char_sampletext)),
