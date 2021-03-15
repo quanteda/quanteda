329e915aea2c1a80b60bf0cf71ab586201092488
Kohei Watanabe <watanabe.kohei@gmail.com>
2021-03-15 13:31:57 +0900

    Export tokens_group()

--- R/tokens_group.R
+++ R/tokens_group.R
@@ -5,14 +5,24 @@
 #' @param x [tokens] object
 #' @inheritParams groups
 #' @keywords tokens internal
+#' @export
 #' @examples
-#' # dfm_group examples
 #' corp <- corpus(c("a a b", "a b c c", "a c d d", "a c c d"),
 #'                    docvars = data.frame(grp = c("grp1", "grp1", "grp2", "grp2")))
 #' toks <- tokens(corp)
-#' quanteda:::tokens_group(toks, groups = "grp")
-#' quanteda:::tokens_group(toks, groups = c(1, 1, 2, 2))
-#' quanteda:::tokens_group(toks, groups = factor(c(1, 1, 2, 2), levels = 1:3))
+#' tokens_group(toks, groups = "grp")
+#' tokens_group(toks, groups = c(1, 1, 2, 2))
+#' tokens_group(toks, groups = factor(c(1, 1, 2, 2), levels = 1:3))
+tokens_group <- function(x, groups = NULL, fill = FALSE) {
+    UseMethod("dfm_group")
+}
+
+#' @export
+tokens_group.default <- function(x, groups = NULL, fill = FALSE) {
+    check_class(class(x), "tokens_group")
+}
+
+#' @export
 tokens_group <- function(x, groups = NULL, fill = FALSE) {
     x <- as.tokens(x)
     if (!is.factor(groups))
@@ -29,7 +39,7 @@ tokens_group <- function(x, groups = NULL, fill = FALSE) {
 }
 
 group_tokens <- function(x, groups) {
-    result <- base::split(unlist(unclass(x), use.names = FALSE), rep(groups, lengths(x)))
+    result <- split(unlist(unclass(x), use.names = FALSE), rep(groups, lengths(x)))
     attr(result, "class") <- "tokens"
     attr(result, "types") <- attr(x, "types")
     attr(result, "padding") <- attr(x, "padding")
