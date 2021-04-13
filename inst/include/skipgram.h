3fc831f7a509e3d4bf1bfcbe8942d5f1104da0c6
Kohei Watanabe <watanabe.kohei@gmail.com>
2021-04-13 11:36:10 +0900

    Add debug code

--- inst/include/skipgram.h
+++ inst/include/skipgram.h
@@ -28,6 +28,9 @@ inline void skip(const Text &tokens,
                  MapNgrams &map_ngram,
                  IdNgram &id_ngram) {
     
+    Rcout << "Ngram Size " << ngram.size() << ", ";
+    Rcout << "Position " << start << "\n";
+    
     ngram.push_back(tokens[start]);
     
     // Rcout << "Size " << tokens.size() << ", ";
@@ -39,7 +42,7 @@ inline void skip(const Text &tokens,
             unsigned int next = start + 1 + skips[j];
             if(tokens.size() - 1 < next) break;
             if(tokens[next] == 0) break; // skip padding
-            Rcout << "Compound " << tokens[start] << " at " << start << " with " << next << "\n";
+            // Rcout << "Compound " << tokens[start] <<  " and " << tokens[next] << " at " << start << "\n";
             skip(tokens, tokens_ng, set_words, next, n, skips, ngram, map_ngram, id_ngram);
         }
     } else {
@@ -48,8 +51,11 @@ inline void skip(const Text &tokens,
         if (set_words.size() > 0) { // for compounding
             auto it = set_words.find(ngram);
             if (it != set_words.end()) {
+                Rcout << "Save ngram ";
+                dev::print_ngram(ngram);
                 tokens_ng.push_back(ngram_id(ngram, map_ngram, id_ngram));
             } else {
+                Rcout << "Save unigram " << tokens[start] << "\n";
                 //Ngram unigram(1, tokens[start]);
                 //tokens_ng.push_back(ngram_id(unigram, map_ngram, id_ngram));
                 tokens_ng.push_back(tokens[start]);
