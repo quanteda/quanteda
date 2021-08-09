7185ac0e95b551fb1df872cf7cb1130e4cd27d29
Stefan Müller <mullers@tcd.ie>
2021-08-09 12:55:30 +0100

    Assign metadata correctly to data_corpus_inaugural

--- tests/data_creation/inaugural/update-inaugural.R
+++ tests/data_creation/inaugural/update-inaugural.R
@@ -2,16 +2,14 @@
 
 library("quanteda")
 
-data_corpus_inaugural <- corpus_subset(data_corpus_inaugural,
-                                       President != "Biden" & Year != "2021")
+data_corpus_inaugural_pre_2021 <- corpus_subset(data_corpus_inaugural, Year != "2021")
 
 metadata <- meta(data_corpus_inaugural)
 
-data_corpus_biden_2021 <- corpus(readtext::readtext("tests/data_creation/inaugural/2021-Biden.txt"))
-docnames(data_corpus_biden_2021) <- "2021-Biden"
+data_corpus_inaugural_2021 <- corpus(readtext::readtext("tests/data_creation/inaugural/2021-Biden.txt"))
+docnames(data_corpus_inaugural_2021) <- "2021-Biden"
 
-data_corpus_inaugural <- data_corpus_inaugural +
-    data_corpus_biden_2021
+data_corpus_inaugural <- data_corpus_inaugural_pre_2021 + data_corpus_inaugural_2021
     
 docvars(data_corpus_inaugural) <- read.csv("tests/data_creation/inaugural/inaugural_docvars.csv")
 data_corpus_inaugural$Party <- factor(data_corpus_inaugural$Party)
