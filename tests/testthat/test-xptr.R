c2ea592a3128841077c8c0b94faa1e1b45a06c7d
Kohei Watanabe <watanabe.kohei@gmail.com>
2023-02-05 19:13:57 +0900

    Covert to CI tests

--- tests/testthat/test-xptr.R
+++ tests/testthat/test-xptr.R
@@ -1,27 +1,31 @@
 require(quanteda)
-require(testthat)
-data(data_corpus_sotu, package = "quanteda.corpora")
-corp <- as.corpus(data_corpus_sotu)
-toks <- tokens(corp)
+
+toks <- tokens(data_corpus_inaugural)
 xtoks <- as.externalptr(toks)
-
-class(toks)
-class(xtoks)
-expect_identical(types(toks), types(xtoks))
-
-# deep copy the xtokens
 xtoks_copy <- as.externalptr(xtoks)
-expect_identical(as.tokens(xtoks_copy), as.tokens(xtoks))
 
-# operations on copied xtokens do not affect the original xtokens
-xtoks_copy <- tokens_remove(xtoks_copy, stopwords(), padding = TRUE)
-expect_false(identical(as.list(xtoks_copy), as.list(as.tokens(xtoks))))
-expect_identical(as.list(toks), as.list(xtoks))
+test_that("attributes are the same", {
+    expect_identical(types(toks), types(xtoks))
+    expect_identical(attr(toks, "docvars"), attr(xtoks, "docvars"))
+    expect_identical(attr(toks, "meta"), attr(xtoks, "meta"))
+})
 
-# tokens_select modify tokens and xtokens in the same way
-toks2 <- tokens_remove(toks, stopwords(), padding = TRUE) %>% 
-    tokens_select(data_dictionary_LSD2015, padding = TRUE)
-xtoks2 <- tokens_remove(xtoks, stopwords(), padding = TRUE) %>% 
-    tokens_select(data_dictionary_LSD2015, padding = TRUE)
-expect_identical(as.list(toks2), as.list(as.tokens(xtoks2)))
+test_that("deep copy xtokens", {
+    expect_identical(as.tokens(xtoks_copy), as.tokens(xtoks))
+})
 
+test_that("operations on copied xtokens do not affect the original xtokens", {
+    xtoks_copy <- tokens_remove(xtoks_copy, stopwords(), padding = TRUE)
+    expect_false(identical(as.list(xtoks_copy), as.list(as.tokens(xtoks))))
+    expect_identical(as.list(toks), as.list(xtoks))
+})
+
+test_that("tokens_select modify tokens and xtokens in the same way", {
+    
+    toks2 <- tokens_remove(toks, stopwords(), padding = TRUE) %>% 
+        tokens_select(data_dictionary_LSD2015, padding = TRUE)
+    xtoks2 <- tokens_remove(xtoks, stopwords(), padding = TRUE) %>% 
+        tokens_select(data_dictionary_LSD2015, padding = TRUE)
+    expect_identical(as.list(toks2), as.list(xtoks2))
+    
+})
