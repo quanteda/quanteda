c95ebaa11a433c5b9bbbc8a2036e04fbace3150d
Kohei Watanabe <watanabe.kohei@gmail.com>
2023-02-05 19:18:08 +0900

    Deep copy original object

--- tests/testthat/test-xptr.R
+++ tests/testthat/test-xptr.R
@@ -11,20 +11,24 @@ test_that("attributes are the same", {
 })
 
 test_that("deep copy xtokens", {
-    expect_identical(as.tokens(xtoks_copy), as.tokens(xtoks))
+    expect_identical(as.tokens(xtoks_copy), 
+                     as.tokens(xtoks))
 })
 
 test_that("operations on copied xtokens do not affect the original xtokens", {
     xtoks_copy <- tokens_remove(xtoks_copy, stopwords(), padding = TRUE)
-    expect_false(identical(as.list(xtoks_copy), as.list(as.tokens(xtoks))))
-    expect_identical(as.list(toks), as.list(xtoks))
+    expect_false(identical(as.list(xtoks_copy), 
+                           as.list(xtoks)))
+    expect_identical(as.list(toks), 
+                     as.list(xtoks))
 })
 
 test_that("tokens_select modify tokens and xtokens in the same way", {
     
     toks2 <- tokens_remove(toks, stopwords(), padding = TRUE) %>% 
         tokens_select(data_dictionary_LSD2015, padding = TRUE)
-    xtoks2 <- tokens_remove(xtoks, stopwords(), padding = TRUE) %>% 
+    xtoks2 <- as.externalptr(xtoks) %>% 
+        tokens_remove(stopwords(), padding = TRUE) %>% 
         tokens_select(data_dictionary_LSD2015, padding = TRUE)
     expect_identical(as.list(toks2), as.list(xtoks2))
     
