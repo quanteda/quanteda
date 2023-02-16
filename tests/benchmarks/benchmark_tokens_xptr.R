28c5ba73ad9abeda1146301ea9d3052f2ec73dfe
Kohei Watanabe <watanabe.kohei@gmail.com>
2023-02-16 09:13:13 +0900

    Use quanteda3 for more accurate comparison

--- tests/benchmarks/benchmark_tokens_xptr.R
+++ tests/benchmarks/benchmark_tokens_xptr.R
@@ -1,3 +1,4 @@
+#devtools::install_github("quanteda/quanteda3")
 require(quanteda)
 corp <- readRDS('/home/kohei/Dropbox/Public/data_corpus_guardian2016-10k.rds') %>% 
     corpus_reshape()
@@ -9,23 +10,31 @@ class(toks)
 class(xtoks)
 
 microbenchmark::microbenchmark(
-    old = tokens(toks, remove_punct = TRUE, remove_numbers = TRUE, 
+    old = quanteda3::tokens(corp, remove_punct = TRUE, remove_numbers = TRUE, 
+                            remove_symbols = TRUE),
+    new = tokens(corp, remove_punct = TRUE, remove_numbers = TRUE, 
                  remove_symbols = TRUE),
+    times = 1
+)
+
+microbenchmark::microbenchmark(
+    old = quanteda3::tokens(toks, remove_punct = TRUE, remove_numbers = TRUE, 
+                           remove_symbols = TRUE),
     new = tokens(xtoks, remove_punct = TRUE, remove_numbers = TRUE, 
-       remove_symbols = TRUE),
+                 remove_symbols = TRUE),
     times = 10
 )
 
-toks2 <- tokens_select(toks, data_dictionary_LSD2015)
+toks2 <- quanteda3::tokens_select(toks, data_dictionary_LSD2015)
 xtoks2 <- tokens_select(as.tokens_xptr(xtoks), data_dictionary_LSD2015)
-identical(toks2, as.tokens(xtoks2))
+identical(as.list(toks2), as.list(xtoks2))
 
 microbenchmark::microbenchmark(
-    old = tokens_remove(toks, stopwords("en"), padding = TRUE) %>% 
-        tokens_remove("[\\p{N}]", valuetype = "regex", padding = TRUE) %>% 
-        tokens_remove("[\\p{P}]", valuetype = "regex", padding = TRUE) %>% 
-        tokens_remove("[\\p{S}]", valuetype = "regex", padding = TRUE) %>% 
-        dfm(remove_padding = TRUE),
+    old = quanteda3::tokens_remove(toks, stopwords("en"), padding = TRUE) %>% 
+        quanteda3::tokens_remove("[\\p{N}]", valuetype = "regex", padding = TRUE) %>% 
+        quanteda3::tokens_remove("[\\p{P}]", valuetype = "regex", padding = TRUE) %>% 
+        quanteda3::tokens_remove("[\\p{S}]", valuetype = "regex", padding = TRUE) %>% 
+        quanteda3::dfm(remove_padding = TRUE),
     new = as.tokens_xptr(xtoks) %>% 
         tokens_remove(stopwords("en"), padding = TRUE) %>% 
         tokens_remove("[\\p{N}]", valuetype = "regex", padding = TRUE) %>% 
@@ -36,32 +45,32 @@ microbenchmark::microbenchmark(
 )
 
 microbenchmark::microbenchmark(
-    old = tokens_compound(toks, data_dictionary_LSD2015),
+    old = quanteda3::tokens_compound(toks, data_dictionary_LSD2015),
     new = as.tokens_xptr(xtoks) %>% 
         tokens_compound(data_dictionary_LSD2015),
     times = 10
 )
 
 microbenchmark::microbenchmark(
-    old = tokens_compound(toks, "&", window = 1),
+    old = quanteda3::tokens_compound(toks, "&", window = 1),
     new = as.tokens_xptr(xtoks) %>% 
         tokens_compound("&", window = 1),
     times = 10
 )
 
 microbenchmark::microbenchmark(
-    dfm(toks),
-    dfm(xtoks),
+    old = quanteda3::dfm(toks),
+    news = dfm(xtoks),
     times = 10
 )
-identical(dfm(xtoks), dfm(toks))
+identical(dfm(xtoks), quanteda3::dfm(toks))
 
 microbenchmark::microbenchmark(
-    dfm(tokens_group(xtoks)),
-    dfm_group(dfm(xtoks)),
+    old = quanteda3::tokens_group(toks),
+    new = tokens_group(xtoks),
     times = 10
 )
-identical(dfm_group(dfmt), dfm(tokens_group(xtoks)))
+identical(as.tokens(tokens_group(xtoks)), quanteda3::tokens_group(toks))
 
 profvis::profvis(
     dfm(xtoks)
