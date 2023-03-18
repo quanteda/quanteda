0be8551898e48766bad1ff1abd56c1b69a32751d
Kohei Watanabe <watanabe.kohei@gmail.com>
2023-03-19 07:09:41 +0900

    Save rules as data object

--- rules/download.R
+++
@@ -1,5 +0,0 @@
-word <- readLines("https://raw.githubusercontent.com/unicode-org/icu/main/icu4c/source/data/brkitr/rules/word.txt") 
-writeLines(word, "rules/word.txt")
-
-sent <- readLines("https://raw.githubusercontent.com/unicode-org/icu/main/icu4c/source/data/brkitr/rules/sent.txt")
-writeLines(sent, "rules/sent.txt")
---
+++ rules/update.R
@@ -0,0 +1,7 @@
+word <- readLines("https://raw.githubusercontent.com/unicode-org/icu/main/icu4c/source/data/brkitr/rules/word.txt") 
+writeLines(word, "rules/word.txt")
+
+data_breakrules <- list(word = paste0(readLines("rules/word.txt"), collapse = "\n"))
+data_breakrules <- c(data_breakrules, yaml::read_yaml("rules/cusotm.yml"))
+
+save("data_breakrules", file = "data/data_breakrules.rda")
