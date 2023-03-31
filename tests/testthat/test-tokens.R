aa0524d9718d106bc2b721b30b50a16be20c3855
Kenneth Benoit <kbenoit@KB-MacBook-Pro-14.local>
2023-03-31 17:06:43 +0930

    Skip failing hiragana test

--- tests/testthat/test-tokens.R
+++ tests/testthat/test-tokens.R
@@ -126,10 +126,9 @@ test_that("tokens works as expected for what = character", {
 
 test_that("tokens works with unusual hiragana #554", {
     skip("Behaviour changed - consider removing test")
-    skip_on_travis()
     skip_on_cran()
-    skip_on_appveyor()
     skip_on_os("windows")
+    skip_on_os("mac")
     txts <- c("づいﾞ", "゛んﾞ", "たーﾟ")
     expect_equivalent(as.list(tokens(txts)),
                       list(c("づ", "いﾞ"), c("゛", "んﾞ"), c("た", "ーﾟ")))
