ade0737a7255dc39885c39fa39132f125dd5d139
Kohei Watanabe <watanabe.kohei@gmail.com>
2023-04-10 14:39:05 +0900

    Tidy up

--- src/subset.cpp
+++ src/subset.cpp
@@ -1,36 +1,20 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
-
 // [[Rcpp::export]]
 void cpp_sub(std::string text) {
-    for(size_t i = 0; i < text.length();)
-    {
+    for (size_t i = 0; i < text.length(); i++) {
         int cplen = 1;
-        if((text[i] & 0xf8) == 0xf0) cplen = 4;
+        if ((text[i] & 0xf8) == 0xf0) cplen = 4;
         else if((text[i] & 0xf0) == 0xe0) cplen = 3;
         else if((text[i] & 0xe0) == 0xc0) cplen = 2;
-        if((i + cplen) > text.length()) cplen = 1;
+        if ((i + cplen) > text.length()) cplen = 1;
         
         Rcout << text.substr(i, cplen) << "\n";
         i += cplen;
     }
 }
 
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
-// run after the compilation.
-//
 
 /*** R
 cpp_sub("今天周五123")
