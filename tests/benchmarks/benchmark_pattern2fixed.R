9da10e6c52795484cbeb2db4b93b7c73fbb8489b
Kohei Watanabe <watanabe.kohei@gmail.com>
2024-01-18 16:21:36 +0900

    Update

--- tests/benchmarks/benchmark_pattern2fixed.R
+++ tests/benchmarks/benchmark_pattern2fixed.R
@@ -1,8 +1,13 @@
 require(stringi)
 require(quanteda)
-type <- stri_rand_strings(1000000, 1:200)
+type <- stri_rand_strings(100000, 1:200)
 microbenchmark::microbenchmark(
-    quanteda3::object2fixed(data_dictionary_LSD2015[1:2], type),
-    quanteda::object2fixed(data_dictionary_LSD2015[1:2], type),
-    times = 10
+    v3 = quanteda3::object2fixed(data_dictionary_LSD2015[1:2], type),
+    v4 = quanteda::object2fixed(data_dictionary_LSD2015[1:2], type),
+    times = 1
+)
+
+microbenchmark::microbenchmark(
+    v3 = quanteda3:::index_types(type, "glob", TRUE),
+    v4 = quanteda:::index_types(unlist(data_dictionary_LSD2015[1:2]), type, "glob", TRUE)
 )
