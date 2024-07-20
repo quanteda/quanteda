8809226940c3a990c560dd8c9978dcd55f93cdc7
Kohei Watanabe <watanabe.kohei@gmail.com>
2024-07-21 07:00:43 +0900

    Do not recompile in ntoken

--- src/tokens_xptr.cpp
+++ src/tokens_xptr.cpp
@@ -67,7 +67,6 @@ int cpp_ndoc(TokensPtr xptr) {
 
 // [[Rcpp::export]]
 IntegerVector cpp_ntoken(TokensPtr xptr, bool padding = true) {
-    xptr->recompile();
     std::size_t H = xptr->texts.size();
     IntegerVector ls_(H, 0);
     if (padding) { 
