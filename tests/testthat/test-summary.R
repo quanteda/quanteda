f42eb0f58d2514dde6aff42f6789760e81bfe8ba
Kohei Watanabe <watanabe.kohei@gmail.com>
2026-02-08 08:15:43 +0900

    Use tokenize_sentence() to make result consistent

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -1,5 +1,5 @@
 test_that("summary and print.summary work", {
-    summary(data_corpus_inaugural[1:2])
+    
     # a second time for the cache
     summ <- summary(data_corpus_inaugural[1:2])
     expect_is(summ, "data.frame")
@@ -9,10 +9,36 @@ test_that("summary and print.summary work", {
     )
     expect_output(
         print(summ),
-        "Corpus consisting of 2 documents, showing 2 documents:
+        paste(c(
+        "Corpus consisting of 2 documents, showing 2 documents:",
+        "",
+        "            Text Types Tokens Sentences Year  President FirstName Party",
+        " 1789-Washington   625   1537        24 1789 Washington    George  none",
+        " 1793-Washington    96    147         5 1793 Washington    George  none"
+    ), collapse = "\n"), fixed = TRUE)
+    
+})
 
-            Text Types Tokens Sentences Year  President FirstName Party
- 1789-Washington   625   1537        24 1789 Washington    George  none
- 1793-Washington    96    147         4 1793 Washington    George  none"
+test_that("summary is consistent with other functions", {
+    
+    # a second time for the cache
+    summ <- summary(data_corpus_inaugural)
+    toks <- tokens(data_corpus_inaugural)
+    corp <- corpus_reshape(data_corpus_inaugural)
+    
+    expect_equivalent(
+        summ$Types,
+        as.integer(ntype(toks))
+    )
+    
+    expect_equivalent(
+        summ$Tokens,
+        as.integer(ntoken(toks))
+    )
+    
+    expect_equivalent(
+        summ$Sentences,
+        as.integer(table(docid(corp)))
     )
 })
+
