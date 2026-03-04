847bbc20da8dae91ba163ffc885f5e345ee99360
Kohei Watanabe <watanabe.kohei@gmail.com>
2026-03-04 19:34:48 +0900

    Return ngram matrix

--- src/ngrams.cpp
+++ src/ngrams.cpp
@@ -1,119 +1,63 @@
 #include "lib.h"
-#include "dev.h"
-#include <bitset>
+//#include "dev.h"
 using namespace quanteda;
 
-// #ifdef QUANTEDA
-// float GLOBAL_PATTERN_MAX_LOAD_FACTOR = 0.05;
-// float GLOBAL_NGRAMS_MAX_LOAD_FACTOR = 0.25;
-// #endif
-
-// #if QUANTEDA_USE_TBB
-// #if TBB_VERSION_MAJOR >= 2021
-// typedef std::atomic<unsigned int> UintAtomic; //oneAPI TBB
-// #else
-// typedef tbb::atomic<unsigned int> UintAtomic; //old TBB
-// #endif
-// typedef tbb::concurrent_unordered_map<Ngram, UintAtomic, hash_ngram, equal_ngram> MapNgrams0;
-// #else
-// typedef std::atomic<unsigned int> UintAtomic;
-// typedef std::unordered_map<Ngram, UintAtomic, hash_ngram, equal_ngram> MapNgrams0;
-// #endif
-typedef std::vector<std::pair<Ngram, unsigned int>> VecNgramsPair;
-
-
-void counts2(Text text,
-             MapNgrams &map_seqs,
-             const std::vector<unsigned int> &sizes){
+void counts(Text text,
+            MapNgrams &map_seqs,
+            const unsigned int &size){
     
-    for (auto size : sizes) { // start from the largest size
-        if (text.size() < size) continue;
-        for (std::size_t i = 0; i < text.size() - size + 1; i++) {
-            Text text_sub(text.begin() + i, text.begin() + i + size);
-            //Rcout << "@" << i << " " <<  nested << ": ";
-            //dev::print_ngram(text_sub);
-            auto &count = map_seqs[text_sub];
-            count++;
-        }
+    if (text.size() < size) return;
+    for (std::size_t i = 0; i < text.size() - size + 1; i++) {
+        Text text_sub(text.begin() + i, text.begin() + i + size);
+        auto &count = map_seqs[text_sub];
+        count++;
     }
 }
 
 
 /*
- * This function estimates the strength of association between specified words
- * that appear in sequences.
+ * This function create a matrix that lists all the n-grams
  * @used sequences()
  * @param texts_ tokens object
- * @param count_min sequences appear less than this are ignored
- * @param method method to estimate collocation association
- * @param smoothing this number is added to collocation counts
- * @param nested estimate parameters for nested collocations
+ * @param size the size of the n-grams
  */
 // [[Rcpp::export]]
-DataFrame cpp_ngrams(const List &texts_,
-                     const IntegerVector sizes_,
-                     const int thread = -1){
+IntegerVector cpp_ngrams(const List &texts_,
+                         unsigned int size,
+                         const int thread = -1){
     
     Texts texts = as<Texts>(texts_);
-    std::vector<unsigned int> sizes = as< std::vector<unsigned int> >(sizes_);
-    std::sort(sizes.begin(), sizes.end(), std::greater<unsigned int>()); // sort in descending order
 
     MapNgrams map_seqs;
     map_seqs.max_load_factor(GLOBAL_PATTERN_MAX_LOAD_FACTOR);
     
-    //dev::Timer timer;
-    //dev::start_timer("Count", timer);
     std::size_t H = texts.size();
 #if QUANTEDA_USE_TBB
     arena.execute([&]{
         tbb::parallel_for(tbb::blocked_range<int>(0, H), [&](tbb::blocked_range<int> r) {
             for (int h = r.begin(); h < r.end(); ++h) {
-                counts2(texts[h], map_seqs, sizes);
+                counts(texts[h], map_seqs, size);
             }
         });
     });
 #else
     for (std::size_t h = 0; h < H; h++) {
-        counts2(texts[h], map_seqs, sizes);
+        counts(texts[h], map_seqs, size);
     }
 #endif
     
     std::size_t N = map_seqs.size();
-    
-    // for estimation
-    VecNgramsPair seqs_count; // all the collocation
-    seqs_count.reserve(N);
-    VecNgramsPair seqs; // only eligible collocation 
-    seqs.reserve(N);
-    
-    // for output
-    IntParams counts, lengths;
-    counts.reserve(N);
-    lengths.reserve(N);
+    std::vector<int> vec;
+    vec.reserve(N * size);
     
     for (auto it = map_seqs.begin(); it != map_seqs.end(); ++it) {
-        // convert to a vector for faster iteration
-        seqs_count.push_back(std::make_pair(it->first, (unsigned int)it->second));
-        
-        // NOTE: join all ngrams with max lengths to make matrix
-        
-        // estimate only sequences without padding
-        if (std::none_of(it->first.begin(), it->first.end(), [](unsigned int v){ return v == 0; })) {
-            //seqs.push_back(it->first);
-            lengths.push_back(it->first.size());
-            counts.push_back(it->second);
-        }
+        vec.insert(vec.end(), it->first.begin(), it->first.end());
     }
     
-    DataFrame output_ = DataFrame::create(
-        //_["collocation"] = seqs_,
-        _["count"] = as<IntegerVector>(wrap(counts)),
-        _["length"] = as<NumericVector>(wrap(lengths)),
-        //_[method] = as<NumericVector>(wrap(lmda)),
-        //_["sigma"] = as<NumericVector>(wrap(sgma)),
-        _["stringsAsFactors"] = false);
-    
-    return output_;
+    IntegerVector vec_ = wrap(vec);
+    vec_.attr("dim") = Dimension(size, N);
+    IntegerMatrix mat_ = transpose(as<IntegerMatrix>(vec_));
+    return mat_;
     
 }
 
@@ -123,10 +67,9 @@ require(quanteda)
 toks <- tokens(data_corpus_inaugural)
 toks <- tokens_select(toks, stopwords("english"), "remove", padding = TRUE)
 i <- seq_along(types(toks))
-i <- seq(1000)
 microbenchmark::microbenchmark(
-    col = cpp_ngrams(toks, 2, -1),
-    exp = expand.grid(i, i),
+    col = cpp_ngrams(toks, 3, -1),
+    exp = expand.grid(1:2, i, 1:2),
     times = 10
 )
 */
