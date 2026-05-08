6cf94f7411880dc4561f77e46a5b18d8df8fbc80
Kohei Watanabe <watanabe.kohei@gmail.com>
2026-05-08 19:24:16 +0900

    Update tests

--- tests/testthat/test-message.R
+++ tests/testthat/test-message.R
@@ -7,6 +7,6 @@ test_that("msg works", {
     expect_equal(
         quanteda:::msg("there are %s features", 10000, 
                        prepend = "[ ", append = " ]"),
-        "[ there are 10000 features ]"
+        "[ there are 10,000 features ]"
     )
 })
