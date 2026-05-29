13e57725c09c652aafea9c04836cef473ef7e134
Kohei Watanabe <watanabe.kohei@gmail.com>
2026-05-29 09:14:06 +0900

    Update test

--- tests/testthat/test-as.tensor.R
+++ tests/testthat/test-as.tensor.R
@@ -12,9 +12,8 @@ test_that("as.tensor.tokens works correctly", {
     # Test basic conversion
     tens1 <- as.tensor(toks)
     expect_true(inherits(tens1, "torch_tensor"))
-    expect_true(tens1$is_sparse())
     expect_equal(
-        as.matrix(tens1$to_dense()),
+        as.matrix(tens1),
         rbind(c(1, 2, 3, 4, 5, 6, 7),
               c(7, 4, 5, 5, 0, 0, 0),
               c(0, 0, 0, 0, 0, 0, 0))
@@ -23,9 +22,8 @@ test_that("as.tensor.tokens works correctly", {
     # Test with length parameter
     tens2 <- as.tensor(toks, length = 8)
     expect_true(inherits(tens2, "torch_tensor"))
-    expect_true(tens2$is_sparse())
     expect_equal(
-        as.matrix(tens2$to_dense()),
+        as.matrix(tens2),
         rbind(c(1, 2, 3, 4, 5, 6, 7, 0),
               c(7, 4, 5, 5, 0, 0, 0, 0),
               c(0, 0, 0, 0, 0, 0, 0, 0))
@@ -33,38 +31,34 @@ test_that("as.tensor.tokens works correctly", {
     
     tens3 <- as.tensor(toks, length = 5)
     expect_true(inherits(tens3, "torch_tensor"))
-    expect_true(tens3$is_sparse())
     expect_equal(
-        as.matrix(tens3$to_dense()),
+        as.matrix(tens3),
         rbind(c(1, 2, 3, 4, 5),
-              c(6, 4, 5, 5, 0),
+              c(7, 4, 5, 5, 0),
               c(0, 0, 0, 0, 0))
     )
     
     # Test empty tokens
     tens4 <- as.tensor(tokens(c(doc1 = "", doc2 = "")))
     expect_true(inherits(tens4, "torch_tensor"))
-    expect_true(tens4$is_sparse())
     expect_equal(
-        as.matrix(tens4$to_dense()),
+        as.matrix(tens4),
         matrix(integer(), nrow = 2, ncol = 0)
     )
     
     # Test single document
     tens5 <- as.tensor(tokens(c(doc1 = "a b c")))
     expect_true(inherits(tens5, "torch_tensor"))
-    expect_true(tens5$is_sparse())
     expect_equal(
-        as.matrix(tens5$to_dense()),
+        as.matrix(tens5),
         rbind(c(1, 2, 3))
     )
     
     # Test empty tokens
     tens6 <- as.tensor(as.tokens(list()))
     expect_true(inherits(tens6, "torch_tensor"))
-    expect_true(tens6$is_sparse())
     expect_equal(
-        as.matrix(tens6$to_dense()),
+        as.matrix(tens6),
         matrix(integer(), nrow = 0, ncol = 0)
     )
     
@@ -72,10 +66,25 @@ test_that("as.tensor.tokens works correctly", {
     toks_inau <- tokens(data_corpus_inaugural)
     tens7 <- as.tensor(toks_inau)
     expect_true(inherits(tens7, "torch_tensor"))
-    expect_true(tens7$is_sparse())
     expect_equal(
         tens7$size(), 
         c(ndoc(toks_inau), max(ntoken(toks_inau)))
+    )
+    
+    # Test index
+    tens8 <- as.tensor(toks, length = 5, index = c(1, 3))
+    expect_true(inherits(tens8, "torch_tensor"))
+    expect_equal(
+        as.matrix(tens8),
+        rbind(c(1, 2, 3, 4, 5),
+              c(0, 0, 0, 0, 0))
+    )
+    
+    tens9 <- as.tensor(toks, length = 5, index = 2)
+    expect_true(inherits(tens9, "torch_tensor"))
+    expect_equal(
+        as.matrix(tens9),
+        rbind(c(7, 4, 5, 5, 0))
     )
 })
 
