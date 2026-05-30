165d4b2c497fe4054b1276eac30136fa9bb85cb4
Kohei Watanabe <watanabe.kohei@gmail.com>
2026-05-30 16:22:12 +0900

    Avoid calling as.tokens(x) which trigers recompilation

--- R/docvars.R
+++ R/docvars.R
@@ -247,6 +247,12 @@ docvars.tokens <- function(x, field = NULL) {
 
 #' @noRd
 #' @export
+docvars.tokens_xptr <- function(x, field = NULL) {
+    select_docvars(attr(x, "docvars"), field, user = TRUE, system = FALSE, drop = TRUE)
+}
+
+#' @noRd
+#' @export
 docvars.dfm <- function(x, field = NULL) {
     x <- as.dfm(x)
     select_docvars(x@docvars, field, user = TRUE, system = FALSE, drop = TRUE)
